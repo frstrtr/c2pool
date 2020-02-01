e1017f339fa523b94fd6182b6f736e314c8ebb0c
Alec <frstrtr@gmail.com>
2020-02-01 17:17:27 +0300

    Update design.cpp

--- design.cpp
+++ design.cpp
@@ -1,6 +1,9 @@
-ASIO
-LMDB
+# ASIO
+# LMDB or LevelDB database?
+#   https://en.wikipedia.org/wiki/LevelDB
+#   https://en.wikipedia.org/wiki/Lightning_Memory-Mapped_Database
 
-Web-server?
-Deferred requests?
-Bitcoin Core based design solutions
+# Web-server?
+# Deferred requests?
+# Bitcoin Core based design solutions
+# ...
