193d60da1b560dbc30bf9883d872bd0fe39a34fa
Neels99 <vasile.danil1@gmail.com>
2020-02-08 08:24:53 +0300

    raw pack, without pack.cpp

--- test/util_tests/pack_test.cpp
+++ test/util_tests/pack_test.cpp
@@ -6,15 +6,13 @@ public:
     virtual ~PackTypeTest(){
 
     }
-    Type First;
-    Type Second;
+
 protected:
     virtual void SetUp(){
-        First = Type(1,2);
-        Second = Type(1,2);
+
     }
 };
 
 TEST_F(PackTypeTest, equal_type){
-    ASSERT_EQ(First, Second);
+
 }
