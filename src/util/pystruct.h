69ca63f170c2bca191108ce435eca9a437a8bcd0
Neels99 <vasile.danil1@gmail.com>
2020-02-10 11:34:47 +0300

    added python + tests

---
+++ src/util/pystruct.h
@@ -0,0 +1,23 @@
+//
+// Created by vasil on 09.02.2020.
+//
+
+#ifndef CPOOL_PYSTRUCT_H
+#define CPOOL_PYSTRUCT_H
+
+#include <string>
+#include "Python.h"
+
+
+class pystruct{
+public:
+
+    void read();
+
+    void unpack(std::string types, std::string vars);
+
+    void pack(std::string, std::string vars);
+};
+
+
+#endif //CPOOL_PYSTRUCT_H
