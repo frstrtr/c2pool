87801957654945c7f2336cc8fe7ff9400e13cec2
Neels99 <vasile.danil1@gmail.com>
2020-02-16 00:56:17 +0300

    Added MemoryFile; reworked pack

--- src/util/filesys.cpp
+++ src/util/filesys.cpp
@@ -1,20 +1,32 @@
-//
-// Created by vasil on 14.02.2020.
-//
-
 #include "filesys.h"
 #include <cstring>
 #include <string>
 
-file::file(string nameFile) {
+File::File(string nameFile, string buffer) {
     f.open(nameFile); //TODO: Open only while read/write?
+    if (buffer != ""){
+        f << buffer;
+    }
 }
 
-string file::read(int length) {
+File::~File() {
+    f.close();
+}
+
+string File::getvalue() {
+    string buff;
+    while (f >> buff){
+        //TODO: output in DEBUG_MODE
+    }
+    return buff;
+}
+
+string File::read(int length) {
     string res;
     if (f.is_open()) {
         while (getline (f,res))
         {
+            //? res += buff;
             //TODO: output in DEBUG_MODE
         }
         if (length != -1) {
@@ -26,12 +38,16 @@ string file::read(int length) {
     return "";
 }
 
-int file::write(string text) {
+int File::write(string text) {
     f << text;
+    return text.length();
 }
 
-file::~file() {
-    f.close();
+int File::write(int num) {
+    stringstream buff;
+    buff << num;
+    f << buff.str();
+    return buff.str().length();
 }
 
 string FileSystem::getProjectDir() {
@@ -51,4 +67,46 @@ const char *FileSystem::getSubDir_c(string path) {
     char* cstr = new char [str.length()+1];
     std::strcpy (cstr, str.c_str());
     return cstr;
+}
+
+MemoryFile::~MemoryFile() {
+
+}
+
+string MemoryFile::read(int length) { //TODO: rework
+    string res;
+
+    while (getline(f, res)) {
+        //TODO: output in DEBUG_MODE
+    }
+    if (length != -1) {
+        res = res.substr(0, length);
+    }
+    return res;
+
+    //TODO:raise
+    return "";
+}
+
+string MemoryFile::getvalue() {
+    return f.str();
+}
+
+int MemoryFile::write(string text) {
+    f << text;
+    return text.length();
+}
+
+int MemoryFile::write(int num) {
+    stringstream buff;
+    buff << num;
+    f << buff.str();
+    return buff.str().length();
+}
+
+MemoryFile::MemoryFile(string buffer) {
+    if (buffer != "") {
+        f = stringstream();
+        f << buffer;
+    }
 }
