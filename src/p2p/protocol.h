97ea5fc9399d383522f96d4e89ac2eb8ddaccac0
Neels99 <vasile.danil1@gmail.com>
2020-04-08 07:45:40 +0300

    protocol vars, start of transfer methods

--- src/p2p/protocol.h
+++ src/p2p/protocol.h
@@ -9,6 +9,8 @@
 #include "pystruct.h"
 #include <sstream>
 #include "config.cpp"
+#include <map>
+#include <iostream>
 
 namespace c2pool::messages{
     class message;
@@ -91,11 +93,8 @@ namespace c2pool::p2p {
         P2PNode*_node;
         Factory* factory;
         tcp::socket socket;
-        const unsigned int version;
         const unsigned long max_remembered_txs_size = 25000000;
         unsigned long max_payload_length;
-
-        boost::asio::steady_timer timeout_delayed; //Таймер для автодисконнекта, если нет никакого ответа в течении работы таймера. Сбрасывается каждый раз, как получает какие-то пакеты.
 
         friend class Factory;
     };
@@ -155,19 +154,139 @@ namespace c2pool::p2p {
 
         //todo: connect_timeout
 
-        
 
 
         void send_version(){
             //TODO: init struct Version
         }
 
-        void handle_version(){
+        void handle_version(int ver, int serv, address_type to, address_type from, long _nonce, string sub_ver, int _mode, long best_hash) {
 
+            cout << "Peer " << from.address << ":" << from.port << " says protocol version is " << ver << ", client version " << sub_ver;
+
+            /*
+             
+        if self.other_version is not None:
+            raise PeerMisbehavingError('more than one version message')
+        if version < getattr(self.node.net, 'MINIMUM_PROTOCOL_VERSION', 1400):
+            raise PeerMisbehavingError('peer too old')
+
+        self.other_version = version
+        self.other_sub_version = sub_version[:512]
+        self.other_services = services
+
+        if nonce == self.node.nonce:
+            raise PeerMisbehavingError('was connected to self')
+        if nonce in self.node.peers:
+            if p2pool.DEBUG:
+                print 'Detected duplicate connection, disconnecting from %s:%i' % self.addr
+            self.disconnect()
+            return
+
+        self.nonce = nonce
+        self.connected2 = True
+
+        self.timeout_delayed.cancel()
+        self.timeout_delayed = reactor.callLater(100, self._timeout)
+
+        old_dataReceived = self.dataReceived
+        def new_dataReceived(data):
+            if self.timeout_delayed is not None:
+                self.timeout_delayed.reset(100)
+            old_dataReceived(data)
+        self.dataReceived = new_dataReceived
+
+        self.factory.proto_connected(self)
+
+        self._stop_thread = deferral.run_repeatedly(lambda: [
+            self.send_ping(),
+        random.expovariate(1/100)][-1])
+
+        if self.node.advertise_ip:
+            self._stop_thread2 = deferral.run_repeatedly(lambda: [
+                self.sendAdvertisement(),
+            random.expovariate(1/(100*len(self.node.peers) + 1))][-1])
+
+        if best_share_hash is not None:
+            self.node.handle_share_hashes([best_share_hash], self)
+
+        def add_to_remote_view_of_my_known_txs(added):
+            if added:
+                self.send_have_tx(tx_hashes=list(added.keys()))
+
+        watch_id0 = self.node.known_txs_var.added.watch(add_to_remote_view_of_my_known_txs)
+        self.connection_lost_event.watch(lambda: self.node.known_txs_var.added.unwatch(watch_id0))
+
+        def remove_from_remote_view_of_my_known_txs(removed):
+            if removed:
+                self.send_losing_tx(tx_hashes=list(removed.keys()))
+
+                # cache forgotten txs here for a little while so latency of "losing_tx" packets doesn't cause problems
+                key = max(self.known_txs_cache) + 1 if self.known_txs_cache else 0
+                self.known_txs_cache[key] = removed #dict((h, before[h]) for h in removed)
+                reactor.callLater(20, self.known_txs_cache.pop, key)
+        watch_id1 = self.node.known_txs_var.removed.watch(remove_from_remote_view_of_my_known_txs)
+        self.connection_lost_event.watch(lambda: self.node.known_txs_var.removed.unwatch(watch_id1))
+
+        def update_remote_view_of_my_known_txs(before, after):
+            t0 = time.time()
+            added = set(after) - set(before)
+            removed = set(before) - set(after)
+            if added:
+                self.send_have_tx(tx_hashes=list(added))
+            if removed:
+                self.send_losing_tx(tx_hashes=list(removed))
+
+                # cache forgotten txs here for a little while so latency of "losing_tx" packets doesn't cause problems
+                key = max(self.known_txs_cache) + 1 if self.known_txs_cache else 0
+                self.known_txs_cache[key] = dict((h, before[h]) for h in removed)
+                reactor.callLater(20, self.known_txs_cache.pop, key)
+            t1 = time.time()
+            if p2pool.BENCH and (t1-t0) > .01: print "%8.3f ms for update_remote_view_of_my_known_txs" % ((t1-t0)*1000.)
+        watch_id2 = self.node.known_txs_var.transitioned.watch(update_remote_view_of_my_known_txs)
+        self.connection_lost_event.watch(lambda: self.node.known_txs_var.transitioned.unwatch(watch_id2))
+
+        self.send_have_tx(tx_hashes=self.node.known_txs_var.value.keys())
+
+        def update_remote_view_of_my_mining_txs(before, after):
+            t0 = time.time()
+            added = set(after) - set(before)
+            removed = set(before) - set(after)
+            if removed:
+                self.send_forget_tx(tx_hashes=list(removed))
+                self.remote_remembered_txs_size -= sum(100 + bitcoin_data.tx_type.packed_size(before[x]) for x in removed)
+            if added:
+                self.remote_remembered_txs_size += sum(100 + bitcoin_data.tx_type.packed_size(after[x]) for x in added)
+                assert self.remote_remembered_txs_size <= self.max_remembered_txs_size
+                fragment(self.send_remember_tx, tx_hashes=[x for x in added if x in self.remote_tx_hashes], txs=[after[x] for x in added if x not in self.remote_tx_hashes])
+            t1 = time.time()
+            if p2pool.BENCH and (t1-t0) > .01: print "%8.3f ms for update_remote_view_of_my_mining_txs" % ((t1-t0)*1000.)
+
+        watch_id2 = self.node.mining_txs_var.transitioned.watch(update_remote_view_of_my_mining_txs)
+        self.connection_lost_event.watch(lambda: self.node.mining_txs_var.transitioned.unwatch(watch_id2))
+
+        self.remote_remembered_txs_size += sum(100 + bitcoin_data.tx_type.packed_size(x) for x in self.node.mining_txs_var.value.values())
+        assert self.remote_remembered_txs_size <= self.max_remembered_txs_size
+        fragment(self.send_remember_tx, tx_hashes=[], txs=self.node.mining_txs_var.value.values())
+             */
         }
     private:
-        bool connected2 = false;
+        const unsigned int version;
+        unsigned int other_version;
+        string other_sub_version;
+        int other_services; //TODO: int64? IntType(64)
 
+        bool connected2 = false;
+        string addr[2] = {self.transport.getPeer().host, self.transport.getPeer().port}; //TODO
+        boost::asio::steady_timer timeout_delayed; //Таймер для автодисконнекта, если нет никакого ответа в течении работы таймера. Сбрасывается каждый раз, как получает какие-то пакеты.
+        //TODO???: remote_tx_hashes = set() # view of peer's known_txs # not actually initially empty, but sending txs instead of tx hashes won't hurt
+        int remote_remembered_txs_size = 0; //todo: remove?
+
+        map<string, /*TODO*/> remembered_txs = null; ////TODO: type of key+vakue;  view of peer's mining_txs [78]
+        int = remembered_txs_size = 0 [79]
+        map<string, /*TODO*/> known_txs_cache = null; //TODO: type of key+vakue
+
+        int nonce; //TODO: int64? IntType(64)
 
     };
 }
