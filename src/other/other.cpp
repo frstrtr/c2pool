b14dd950512af59bd95401279546aebabf0a69db
Neels99 <vasile.danil1@gmail.com>
2020-04-13 10:53:24 +0300

    added random methods; upd message, pack, protocol.

---
+++ src/other/other.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <boost/random.hpp>
+#include <vector>
+
+namespace c2pool::random{
+    using namespace boost::random;
+
+    boost::random::mt19937 generator(std::time(0));
+
+    ///[min, max)
+    int RandomInt(int min, int max){
+        boost::random::uniform_int_distribution<> rnd(min, max-1);
+        return rnd(generator);
+    }
+
+    ///[min, max]
+    float RandomFloat(float min, float max){
+        float Min = float(min), Max = float(max);
+        boost::random::uniform_int_distribution<> rnd(Min,Max);
+        float res = ((float) rnd(generator) / Max);
+        float range = Max - Min;
+        res = (res*range) + Min;
+        return res;
+    }
+
+    template <typename T>
+    T RandomChoice(std::vector<T> list){
+        int pos = RandomInt(0, list.size());
+        return list[pos];
+    }
+
+}
--- src/util/pack.h
+++ src/util/pack.h
@@ -252,6 +252,12 @@ class ComposedType: public Type<string>{
     //auto record_type;
     stringstream fields;
 
+private:
+    void Space(){ // проверка и разделение переменных в потоке.
+        if (fields.rdbuf()->in_avail() != 0){ //проверка на то, что в fields уже есть какие-то данные.
+            fields << ";";
+        }
+    }
 
 public:
     ComposedType(){
@@ -266,13 +272,32 @@ public:
 
     template <typename T>
     ComposedType& add(const T& value){
-        if (fields.rdbuf()->in_avail() != 0){ //проверка на то, что в fields уже есть какие-то данные.
-            fields << ";";
-        }
+        Space();
         fields << value;
         return *this;
     }
 
+    template <typename T>
+    ComposedType& add(const vector<T> value){
+        Space();
+        fields << "[";
+        for (int i = 0; i < value.size(); i++){
+            if (i != 0) {
+                fields << ","; //элементы массива разделяются запятой!// s
+            }
+            fields << value[i];
+        }
+        fields << "]";
+        return *this;
+    }
+
+    string read(){//TODO: this is real read, remove other
+        string buff;
+        fields >> buff;
+        string res = "|" + buff + "|";
+        return res;
+    }
+    //___________________________________
     template <typename T>
     ComposedType& add(string field_name, PackTypes packType, const T& value){
         fields << "(" << field_name << "," << packType << "," << value << ")";
@@ -290,13 +315,6 @@ public:
         value.fields >> _v;
         fields << "(" << field_name << "," << PackTypes::ComposedType << "," << _v << ")";
         return *this;
-    }
-
-    string read(){//TODO: this is real read, remove other
-        string buff;
-        fields >> buff;
-        string res = "|" + buff + "|";
-        return res;
     }
 
     string read(MemoryFile &f){
