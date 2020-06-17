5e12586b8917c6cf09bfdb946fba668d5f2b03b4
Neels99 <vasile.danil1@gmail.com>
2020-06-18 00:04:14 +0300

    rework config,  unfinished node.h/.cpp

---
+++ src/networks/config.h
@@ -0,0 +1,29 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <vector>
+#include <string>
+
+namespace c2pool::config
+{
+    class Network
+    {
+        std::vector<std::string> BOOTSTRAP_ADDRS{"127.0.0.1"};
+        std::string PREFIX = "1234567890";
+        int MINIMUM_PROTOCOL_VERSION = 1600;
+    };
+
+    /*
+    template for test config:
+    class TestNetwork : Network{
+    public:
+        TestNetwork(){
+            ...
+            PREFIX = "TESTNETWORK";
+            ...
+        }
+    }
+    */
+} // namespace c2pool::config
+
+#endif //CONFIG_H
