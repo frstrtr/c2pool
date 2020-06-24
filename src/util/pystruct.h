3aada2283c9a869aea698380326d03224cd0cded
Neels99 <vasile.danil1@gmail.com>
2020-06-25 02:02:39 +0300

    first test

--- src/util/pystruct.h
+++ src/util/pystruct.h
@@ -22,10 +22,10 @@ namespace c2pool::messages::python
     class pymessage
     {
     public:
-        static int receive_length(char *length_data); //TODO remove comment: rdy
+        static int receive_length(char *length_data);
 
         //called, when get message from p2pool [unpacked]
-        static std::stringstream receive(char *command, char *checksum, char *payload);  //TODO remove comment: rdy
+        static std::stringstream receive(char *command, char *checksum, char *payload);
 
         //called, when send message to p2pool [packed]
         static char *send(char *comamnd, char *payload2); 
@@ -37,7 +37,7 @@ namespace c2pool::messages::python::for_test
     class pymessage
     {
     public:
-        static char *get_packed_int(int num); //todo: end this
+        static char *get_packed_int(int num);
     };
 } // namespace c2pool::messages::python::for_test
 
--- test/util_tests/pystruct_test.cpp
+++ test/util_tests/pystruct_test.cpp
@@ -2,50 +2,12 @@
 #include <pystruct.h>
 using namespace std;
 
-TEST(PyCode, PyStructPack){
-    Py::Initialize();
+TEST(PyCode, PyReceiveLength){
+    int first_num = 10;
 
-    stringstream ss;
+    char* first_num_chr = c2pool::messages::python::for_test::pymessage::get_packed_int(first_num);
 
-    ss << 321 << ", " << 1337;
-    string res = pystruct::pack("<II", ss);
-    ASSERT_EQ(res, "b\'A\\x01\\x00\\x009\\x05\\x00\\x00\'");
-    ss.clear();
-    ss << "bzz" << ", " << 1337 << ", " << 321;
-    res = pystruct::pack("<3sII", ss);
-    ASSERT_EQ(res,"b\'bzz9\\x05\\x00\\x00A\\x01\\x00\\x00\'");
+    int second_num = c2pool::messages::python::pymessage::receive_length(first_num_chr);
 
-    Py::Finalize();
-}
-
-TEST(PyCode, PyStructUnpack){
-    string r;
-    int a, b;
-    stringstream s = pystruct::unpack("<3sII", "b\'bzz9\\x05\\x00\\x00A\\x01\\x00\\x00\'");
-
-    s >> r >> a >> b;
-    ASSERT_EQ(r, "bzz");
-    ASSERT_EQ(a, 1337);
-    ASSERT_EQ(b, 321);
-}
-
-TEST(PyCode, PyStructPackUnpack){
-    char* types = "<I2I10s";
-    int i11 = 1, i12 = 12, i13 = 31;
-    string s1 = "zerone.bit";
-
-    stringstream _pack;
-
-    _pack << i11 << ", " << i12 << ", " << i13 << ", " << s1;
-    char* res = pystruct::pack(types, _pack);
-
-    stringstream _unpack = pystruct::unpack(types, res);
-
-    int i21, i22, i23;
-    string s2;
-    _unpack >> i21 >> i22 >> i23 >> s2;
-    ASSERT_EQ(i21, i11);
-    ASSERT_EQ(i22, i12);
-    ASSERT_EQ(i23, i13);
-    ASSERT_EQ(s2, s1);
+    ASSERT_EQ(first_num, second_num);
 }
