ea33f6a5e199b03462628625a94491ac05848b38
Neels99 <vasile.danil1@gmail.com>
2020-07-14 18:17:14 +0300

    Update messages_test.cpp

--- test/util_tests/messages_test.cpp
+++ test/util_tests/messages_test.cpp
@@ -22,15 +22,18 @@ TEST(TestMessages, IMessage)
 
 TEST(TestMessages, message_version)
 {
+    //SEND
     c2pool::messages::address_type addrs1(3, "4.5.6.7", 8);
     c2pool::messages::address_type addrs2(9, "10.11.12.13", 14);
     c2pool::messages::message_version *firstMsg = new c2pool::messages::message_version(1, 2, addrs1, addrs2, 15, "16", 17, 18);
     firstMsg->send();
 
+    //expected data for firstMsg->send()
     char *expectedData = c2pool::str::from_bytes_to_strChar("118 101 114 115 105 111 110 0 0 0 0 0 111 0 0 0 80 249 219 9 1 0 0 0 2 0 0 0 0 0 0 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 255 255 4 5 6 7 0 8 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 255 255 10 11 12 13 0 14 15 0 0 0 0 0 0 0 2 49 54 17 0 0 0 18 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
 
     ASSERT_EQ(*firstMsg->data, *expectedData);
 
+    //RECEIVE
     c2pool::messages::message_version *secondMsg = new c2pool::messages::message_version();
     secondMsg->receive_from_data(firstMsg->data);
 
@@ -43,12 +46,22 @@ TEST(TestMessages, message_version)
     // std::cout << "mode " << secondMsg->mode << std::endl;
     // std::cout << "best_share_hash " << secondMsg->best_share_hash << std::endl;
 
-    ASSERT_EQ(firstMsg->version, secondMsg->version); 
-    ASSERT_EQ(firstMsg->services, secondMsg->services); 
+    ASSERT_EQ(firstMsg->version, secondMsg->version);
+    ASSERT_EQ(firstMsg->services, secondMsg->services);
     ASSERT_EQ(firstMsg->addr_to, secondMsg->addr_to);
     ASSERT_EQ(firstMsg->addr_from, secondMsg->addr_from);
     ASSERT_EQ(firstMsg->nonce, secondMsg->nonce);
     ASSERT_EQ(firstMsg->sub_version, secondMsg->sub_version);
     ASSERT_EQ(firstMsg->mode, secondMsg->mode);
     ASSERT_EQ(firstMsg->best_share_hash, secondMsg->best_share_hash);
+}
+
+TEST(TestMessages, message_addrme)
+{
+}
+TEST(TestMessages, message_getaddrs)
+{
+}
+TEST(TestMessages, message_addrs)
+{
 }
