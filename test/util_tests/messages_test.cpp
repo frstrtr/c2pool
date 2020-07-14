0d00cb24f7e2b620ba19000ff8d739f9d3fb1a54
Neels99 <vasile.danil1@gmail.com>
2020-07-14 15:24:25 +0300

    reorganization send/receive for message

--- test/util_tests/messages_test.cpp
+++ test/util_tests/messages_test.cpp
@@ -25,20 +25,14 @@ TEST(TestMessages, message_version)
     c2pool::messages::address_type addrs1(3, "4.5.6.7", 8);
     c2pool::messages::address_type addrs2(9, "10.11.12.13", 14);
     c2pool::messages::message_version *firstMsg = new c2pool::messages::message_version(1, 2, addrs1, addrs2, 15, "16", 17, 18);
+    firstMsg->send();
 
-    //packed bytes
-    char *data = c2pool::messages::python::pymessage::send(firstMsg);
     char *expectedData = c2pool::str::from_bytes_to_strChar("118 101 114 115 105 111 110 0 0 0 0 0 111 0 0 0 80 249 219 9 1 0 0 0 2 0 0 0 0 0 0 0 3 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 255 255 4 5 6 7 0 8 9 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 255 255 10 11 12 13 0 14 15 0 0 0 0 0 0 0 2 49 54 17 0 0 0 18 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0");
 
-    ASSERT_EQ(*data, *expectedData);
+    ASSERT_EQ(*firstMsg->data, *expectedData);
 
     c2pool::messages::message_version *secondMsg = new c2pool::messages::message_version();
-    secondMsg->get_data(data);
-    secondMsg->encode_data();
-
-    std::stringstream ss = c2pool::messages::python::pymessage::receive(secondMsg->command, secondMsg->checksum, secondMsg->payload, secondMsg->unpacked_length);
-
-    secondMsg->unpack(ss);
+    secondMsg->receive_from_data(firstMsg->data);
 
     // std::cout << "version " << secondMsg->version << std::endl;
     // std::cout << "services " << secondMsg->services << std::endl;
