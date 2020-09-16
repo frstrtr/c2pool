5aa617337a75e4d39a81a2aa3922cf68b82dd847
Neels99 <vasile.danil1@gmail.com>
2020-09-16 13:26:00 +0300

    CLI addrs param

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,9 @@
 #include <memory>
 #include <console.h>
 #include <fstream>
+#include <string>
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
     SetThreadUILanguage(MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US));//for test
     setlocale(LC_ALL, "rus");
@@ -26,12 +27,29 @@ int main(int argc, char *argv[])
     LOG_INFO << "Start c2pool...";
 
     c2pool::config::Network *net = new c2pool::config::DigibyteNetwork();
+
+    //TODO: [костыль]addrs_parse
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string str(argv[i]);
+
+        size_t pos_dd = str.find(":");
+        if (pos_dd != string::npos){
+            std::string ip = str.substr(0, pos_dd);
+            std::string port = str.substr(pos_dd+1, str.size());
+
+            auto addr = std::make_tuple(ip,port);
+            net->BOOTSTRAP_ADDRS.push_back(addr);
+        }
+    }
+    //--------------------
+
     boost::asio::io_context io;
     c2pool::p2p::AddrStore addr_store("data//digibyte//addrs", net); //TODO: path
 
     std::shared_ptr<c2pool::p2p::NodesManager> nodesManager = std::make_shared<c2pool::p2p::NodesManager>(io, net);
 
-    string port = "3035"; //TODO
+    std::string port = "3035"; //TODO
 
     //std::unique_ptr<c2pool::p2p::P2PNode> node = std::make_unique<c2pool::p2p::P2PNode>(nodesManager, port, addr_store);
     nodesManager->p2p_node = std::make_unique<c2pool::p2p::P2PNode>(nodesManager, port, addr_store);
