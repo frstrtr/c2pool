0c71a37cd32fb75261f68a65acdc4bcf6d2b8ec7
Neels99 <vasile.danil1@gmail.com>
2020-10-08 03:46:36 +0300

    HashLinkType template test

---
+++ test/share_tests/shareTypes_test.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <iostream>
+#include <memory>
+#include <shareTypes.h>
+#include <sstream>
+
+
+//TODO: create CMakeList.txt
+TEST(Py, HashLinkTypeTest)
+{
+    c2pool::shares::HashLinkType hashLinkType1("1", "2");
+    c2pool::shares::HashLinkType hashLinkType2;
+    std::stringstream ss;
+    ss << "1" << " " << "2";
+    ss >> hashLinkType2;
+    ASSERT_EQ(hashLinkType1, hashLinkType2);
+    c2pool::shares::HashLinkType hashLinkType3;
+    std::stringstream ss2;
+    ss2 << hashLinkType1;
+    ss2 >> hashLinkType3;
+    ASSERT_EQ(hashLinkType1, hashLinkType3);
+}
