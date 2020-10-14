2784e31491cb4d4c8ee17c7314961beb3eb0b431
Neels99 <vasile.danil1@gmail.com>
2020-10-15 00:11:34 +0300

    upd share

--- src/sharechain/share.h
+++ src/sharechain/share.h
@@ -1,31 +1,52 @@
 #include <string>
+#include <shareTypes.h>
+#include <memory>
 
+namespace c2pool::config
+{
+    class Network;
+}
 
+using std::shared_ptr, std::string;
+using namespace c2pool::shares;
 
 namespace c2pool::shares
 {
+    class Share
+    {
+    public:
+        int shareType;        //pack.VarIntType()
+        string shareContents; //pack.VarStrType()
+
+        void load_share(/*in parameters block*/ Share share, string net, /*addr*/ peerAddr, /*out parameters block*/ share_versions[share['type']](net, peer_addr, share_versions[share['type']].get_dynamic_types(net)['share_type'].unpack(share['contents']))){
+
+        };
+    };
 
     class BaseShare
     {
     public:
+        BaseShare(shared_ptr<c2pool::config::Network> _net, auto _peer_addr, auto _contents); //TODO: types
+    public:
         int VERSION = 0;
         int VOTING_VERSION = 0;
-        int *SUCCESSOR = nullptr; // None
-        SmallBlockHeaderType smallBlockHeader;
+        // auto SUCCESSOR = nullptr; // None //TODO
+        shared_ptr<SmallBlockHeaderType> small_block_header;
 
-        Share *shareInfoType = nullptr; // None
-        Share *shareType = nullptr;     // None
-        Share *refType = nullptr;       // None
+        shared_ptr<ShareInfoType> share_info_type; // None
+        shared_ptr<ShareType> share_type;          // None
+        shared_ptr<RefType> ref_type;              // None
 
-        auto gentxBeforeRefhash; //gentx_before_refhash = pack.VarStrType().pack(DONATION_SCRIPT) + pack.IntType(64).pack(0) + pack.VarStrType().pack('\x6a\x28' + pack.IntType(256).pack(0) + pack.IntType(64).pack(0))[:3]
+        auto gentxBeforeRefhash; //TODO: gentx_before_refhash = pack.VarStrType().pack(DONATION_SCRIPT) + pack.IntType(64).pack(0) + pack.VarStrType().pack('\x6a\x28' + pack.IntType(256).pack(0) + pack.IntType(64).pack(0))[:3]
 
         int gentxSize = 50000; // conservative estimate, will be overwritten during execution
         int gentxWeight = 200000;
-        int *cachedTypes = nullptr; // None
 
-        // @classmethod
-        void getDynamicTypes(/*in parameters block*/ /*this?*/ cls, auto net, /*out paarmeters block*/ auto cachedTypes, /*dict*/ t){};
-
+    public:
+        shared_ptr<c2pool::config::Network> net;
+        auto peer_addr; //TODO: type
+        auto contents;  //TODO: type
+    public:
         // @classmethod
         void generateTransaction(/*in parameters block*/
                                  /*cls, tracker, share_data, block_target, desired_timestamp, desired_target, ref_merkle_link, desired_other_transaction_hashes_and_fees, net, known_txs=None, last_txout_nonce=0, base_subsidy=None, segwit_data=None*/
@@ -130,24 +151,6 @@ namespace c2pool::shares
 
         /*type?*/ asBlock(/*type?*/ tracker, /*type?*/ knownTXs){
 
-        };
-    };
-
-    class Share
-    {
-    public:
-        int shareType;        //pack.VarIntType()
-        string shareContents; //pack.VarStrType()
-
-        void load_share(/*in parameters block*/ Share share, string net, /*addr*/ peerAddr, /*out parameters block*/ share_versions[share['type']](net, peer_addr, share_versions[share['type']].get_dynamic_types(net)['share_type'].unpack(share['contents']))){
-
-        };
-
-        bool is_segwit_activated(int version, string net)
-        {
-            //assert not(version is None or net is None)
-            // segwit_activation_version = getattr(net, 'SEGWIT_ACTIVATION_VERSION', 0)
-            return 0; //version >= segwit_activation_version and segwit_activation_version > 0
         };
     };
 
