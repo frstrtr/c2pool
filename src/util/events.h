b88d840375be7a9f5ca87bc749d7aaaa9e6c3a00
Neels99 <vasile.danil1@gmail.com>
2020-10-24 10:09:45 +0300

    Created all trackers, attrDelta; created Event system
    Created Tracker, OkayTracker, SubsetTracker;
    Created ProtoAttributeDelta, OkayProtoAttributeDelta, SubsetProtoAttributeDelta
    Created c2pool::util::events::Event

---
+++ src/util/events.h
@@ -0,0 +1,25 @@
+#include <boost/signals2.hpp>
+
+namespace c2pool::util::events
+{
+    template <typename... Args>
+    class Event
+    {
+        boost::signals2::signal<void(Args...)> sig;
+
+    public:
+        Event() {}
+
+        template <typename _Func, typename... _BoundArgs>
+        void subscribe(_Func &&__f, _BoundArgs &&... __args)
+        {
+            sig.connect(std::bind(__f, __args...));
+        }
+        
+        void unsubscribe(){
+            //TODO
+        }
+
+        void happened(Args... args) const { sig(args...); }
+    };
+} // namespace c2pool::util::events
