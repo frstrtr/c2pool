800bb158f46437f2e22ac5eb67f1e8fcf422ed13
Neels99 <vasile.danil1@gmail.com>
2020-11-12 14:28:10 +0300

    upd data.h

--- test/bitcoind_tests/data_test.cpp
+++ test/bitcoind_tests/data_test.cpp
@@ -38,3 +38,26 @@ TEST(BitcoindDataTest, target_to_averate_attempts_test){
     cout << fourth_res.GetHex() << endl;
     ASSERT_EQ(fourth_res.GetHex(), "00000000000000000000000000000000ffffffffffffffffffffffffffffffff");
 }
+
+TEST(BitcoindDataTest, average_attempts_to_target_test){
+    auto first = CreateUINT256("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff");
+    uint256 first_res = bitcoind::data::average_attempts_to_target(first);
+    cout << first_res.GetHex() << endl;
+    ASSERT_EQ(first_res.GetHex(), "0000000000000000000000000000000000000000000000000000000000000000");
+    
+    auto second = CreateUINT256("100000000000000000000000000000000");
+    uint256 second_res = bitcoind::data::target_to_average_attempts(second);
+    cout << second_res.GetHex() << endl;
+    //Note: in Python: '0x8000000000000000000000000000000000000000000000000000000000000000'
+    ASSERT_EQ(second_res.GetHex(), "0000000000000000000000000000000100000000000000000000000000000000");
+
+    auto third = CreateUINT256("100000000000000000000000000000000");
+    uint256 third_res = bitcoind::data::target_to_average_attempts(third);
+    cout << third_res.GetHex() << endl;
+    ASSERT_EQ(third_res.GetHex(), "00000000000000000000000000000000ffffffffffffffffffffffffffffffff");
+
+    auto fourth = CreateUINT256("ffffffffffffffffffffffffffffffff");
+    uint256 fourth_res = bitcoind::data::target_to_average_attempts(fourth);
+    cout << fourth_res.GetHex() << endl;
+    ASSERT_EQ(fourth_res.GetHex(), "00000000000000000000000000000000ffffffffffffffffffffffffffffffff");
+}
