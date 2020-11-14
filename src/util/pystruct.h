1095b53c083d6ecee709e48ef13de7d894e4064a
Neels99 <vasile.danil1@gmail.com>
2020-11-14 03:46:33 +0300

    reworked pystruct
    Added PyPackTypes

--- src/util/pystruct.h
+++ src/util/pystruct.h
@@ -3,6 +3,8 @@
 
 #include <string>
 #include <sstream>
+#include <univalue.h>
+
 using namespace std;
 
 namespace c2pool::messages
@@ -27,7 +29,7 @@ namespace c2pool::messages::python
     class other
     {
     public:
-        static void debug_log(char* data, unsigned int len);
+        static void debug_log(char *data, unsigned int len);
     };
 
     class pymessage
@@ -47,6 +49,36 @@ namespace c2pool::messages::python
         static int payload_length(char *command, char *payload2);
 
         static int payload_length(c2pool::messages::message *msg);
+    };
+
+    class PyPackTypes
+    {
+    private:
+        static auto GetMethodObject(const char *method_name, const char *filename = "packtypes");
+
+        template <typename PyObjectType>
+        static char *GetCallFunctionResult(PyObjectType *pyObj);
+
+    public:
+        //obj(c++) -> json -> bytes -> unsigned_char*
+        template <typename T>
+        static char *serialize(char *name_type, T &value);
+        //message_<command> -> json -> bytes -> unsigned_char*
+        static char *serialize(c2pool::messages::message *msg);
+
+        //unsigned char* -> bytes -> json -> obj(c++)
+        static UniValue deserialize(char *name_type, char *value, int length); //length = len(value)
+        //msg.[unsigned char*] -> bytes -> json -> obj(c++)
+        static UniValue deserialize(c2pool::messages::message *msg);
+
+        static int payload_length(c2pool::messages::message *msg);
+
+        //obj(c++) -> json -> bytes -> len(bytes)
+        template <typename T>
+        static unsigned int packed_size(char *name_type, T &value);
+
+        static unsigned int receive_length(char *length_data);
+
     };
 } // namespace c2pool::messages::python
 
