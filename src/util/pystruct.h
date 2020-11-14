d03eaa303ab9b9df8477813f45ffd1dd84d2d5e1
Neels99 <vasile.danil1@gmail.com>
2020-11-14 03:54:28 +0300

    Removed all python::for_test::pymessage methods
    because this wanna be reworked

--- src/util/pystruct.h
+++ src/util/pystruct.h
@@ -68,20 +68,7 @@ namespace c2pool::python::for_test
     class pymessage
     {
     public:
-        static char *get_packed_int(int num);
-
-        static char *data_for_test_receive();
-
-        static char *checksum_for_test_receive();
-
-        static unsigned int length_for_test_receive();
-
-        static char *data_for_test_send();
-
-        //called, when send message to p2pool [packed]
-        static std::stringstream emulate_protocol_get_data(char *comamnd, char *payload2);
-
-        static void test_get_bytes_from_cpp(char *data, int len);
+        
     };
 } // namespace c2pool::messages::python::for_test
 
