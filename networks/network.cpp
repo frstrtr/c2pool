b1fd5e2e77e892b37e986bd756c80012db5acbb8
neels99 <vasile.danil1@gmail.com>
2021-01-28 01:33:01 +0300

    reorganization of project; rewrite CMake; removed crypto

---
+++ networks/network.cpp
@@ -0,0 +1,18 @@
+#include "network.h"
+#include <vector>
+#include <string>
+#include <iostream>
+//#include <console.h>
+#include <tuple>
+using namespace std;
+
+namespace c2pool::config
+{
+
+    Network::Network()
+    {
+        //TODO
+        //LOG_INFO << "Created Network Config.";
+    }
+
+} // namespace c2pool::config
