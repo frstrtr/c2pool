614b577007425a45931c9c1b3e063a07a31988a2
neels99 <vasile.danil1@gmail.com>
2021-02-19 21:13:37 +0300

    rework p2p_socket, update p2p_protocol

--- libnet/p2p_protocol.h
+++ libnet/p2p_protocol.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "p2p_socket.h"
+
 #include "messages.h"
 #include "converter.h"
 using namespace c2pool::libnet::messages;
@@ -7,63 +7,94 @@ using namespace c2pool::libnet::messages;
 #include <lib/univalue/include/univalue.h>
 
 #include <memory>
-using std::shared_ptr, std::make_shared;
+using std::shared_ptr, std::weak_ptr, std::make_shared;
 
 namespace c2pool::p2p
 {
-    template <class converter_type>
+    class P2PSocket;
+}
+
+namespace c2pool::p2p
+{
     class Protocol
     {
+    protected:
+        shared_ptr<c2pool::p2p::P2PSocket> _socket;
+
+    protected:
+        Protocol(shared_ptr<c2pool::p2p::P2PSocket> _sct)
+        {
+            _socket = _sct;
+        }
+
     public:
-        Protocol(P2PSocket socket) : _socket(std::move(socket))
+        virtual void handle(shared_ptr<raw_message> RawMSG) = 0;
+
+        virtual shared_ptr<raw_message> make_raw_message() = 0;
+    };
+
+    template <class converter_type>
+    class P2P_Protocol : public Protocol
+    {
+    public:
+        P2P_Protocol(shared_ptr<c2pool::p2p::P2PSocket> socket) : Protocol(socket)
         {
         }
 
-        void handle(shared_ptr<raw_message> RawMSG)
+        void handle(shared_ptr<raw_message> RawMSG) override
         {
             //В Python скрипте, команда передается, как int, эквивалентный commands
+
+            UniValue json_value = RawMSG->value;
 
             switch (RawMSG->name_type) //todo: switch -> if (" " == cmd)
             {
             case commands::cmd_addrs:
-                handle(GenerateMsg<message_addrs>(RawMSG->value));
+                handle(GenerateMsg<message_addrs>(json_value));
                 break;
             case commands::cmd_version:
-                handle(GenerateMsg<message_version>(json));
+                handle(GenerateMsg<message_version>(json_value));
                 break;
             case commands::cmd_ping:
-                handle(GenerateMsg<message_ping>(json));
+                handle(GenerateMsg<message_ping>(json_value));
                 break;
             case commands::cmd_addrme:
-                handle(GenerateMsg<message_addrme>(json));
+                handle(GenerateMsg<message_addrme>(json_value));
                 break;
             case commands::cmd_getaddrs:
-                handle(GenerateMsg<message_getaddrs>(json));
+                handle(GenerateMsg<message_getaddrs>(json_value));
                 break;
             //new:
             case commands::cmd_shares:
-                handle(GenerateMsg<message_shares>(json));
+                handle(GenerateMsg<message_shares>(json_value));
                 break;
             case commands::cmd_sharereq:
-                handle(GenerateMsg<message_sharereq>(json));
+                handle(GenerateMsg<message_sharereq>(json_value));
                 break;
             case commands::cmd_sharereply:
-                handle(GenerateMsg<message_sharereply>(json));
+                handle(GenerateMsg<message_sharereply>(json_value));
                 break;
             //TODO:
             // case commands::cmd_best_block:
-            //     handle(GenerateMsg<message_best_block>(json));
+            //     handle(GenerateMsg<message_best_block>(json_value));
             //     break;
             case commands::cmd_have_tx:
-                handle(GenerateMsg<message_have_tx>(json));
+                handle(GenerateMsg<message_have_tx>(json_value));
                 break;
             case commands::cmd_losing_tx:
-                handle(GenerateMsg<message_losing_tx>(json));
+                handle(GenerateMsg<message_losing_tx>(json_value));
                 break;
             default:
-                handle(GenerateMsg<message_error>(json));
+                handle(GenerateMsg<message_error>(json_value));
                 break;
             }
+        }
+
+        shared_ptr<raw_message> make_raw_message() override
+        {
+            auto raw_msg = make_shared<raw_message>();
+            raw_msg->set_converter<converter_type>();
+            return raw_msg;
         }
 
     protected:
@@ -80,7 +111,7 @@ namespace c2pool::p2p
         {
             //TODO:
         }
-        void handle(shared_ptr<message_addrs<converter_type>> msg)
+        void handle(shared_ptr<message_addrs> msg)
         {
             //TODO:
         }
@@ -124,8 +155,9 @@ namespace c2pool::p2p
         {
             //TODO:
         }
-
-    private:
-        P2PSocket _socket;
     };
+
+    typedef P2P_Protocol<c2pool::libnet::messages::p2pool_converter> p2pool_protocol;
+    //TODO: typedef P2P_Protocol<c2pool::libnet::messages::c2pool_converter> c2pool_protocol;
+
 } // namespace c2pool::p2p
--- libnet/p2p_socket.cpp
+++ libnet/p2p_socket.cpp
@@ -1,16 +1,36 @@
 #include "p2p_socket.h"
 #include <util/messages.h>
+#include <devcore/logger.h>
 
 #include <memory>
 
 #include <boost/asio.hpp>
+#include <boost/function.hpp>
 namespace ip = boost::asio::ip;
 
 namespace c2pool::p2p
 {
-    P2PSocket::P2PSocket(ip::tcp::socket socket) : _socket(std::move(socket))
+
+    P2PSocket::P2PSocket(ip::tcp::socket socket, protocol_handle const &handle) : _socket(std::move(socket))
     {
         //TODO: check p2pool/c2pool node
+        shared_ptr<c2pool::p2p::Protocol> temp_protocol;
+        //if p2pool:
+        //create P2P_Protocol<c2pool::libnet::messages::p2pool_converter>
+        temp_protocol = make_shared<p2pool_protocol>(shared_from_this());
+
+        //if c2pool:
+        //create P2P_Protocol<c2pool::libnet::messages::c2pool_converter>
+        //TODO: temp_protocol = make_shared<c2pool_protocol>(shared_from_this());
+
+        //handle protocol for P2PNode
+        handle(temp_protocol);
+
+        //save protocol in P2PSocket like weak_ptr:
+        _protocol = temp_protocol;
+
+        //start reading in socket:
+        start_read();
     }
 
     void P2PSocket::write(std::shared_ptr<c2pool::messages::message> msg)
@@ -26,9 +46,110 @@ namespace c2pool::p2p
                                  });
     }
 
-    void read_prefix();
-    void read_command();
-    void read_length();
-    void read_checksum();
-    void read_payload();
+    void P2PSocket::start_read()
+    {
+        //make raw_message for reading data
+        shared_ptr<raw_message> tempRawMessage = _protocol.lock()->make_raw_message();
+        //Socket started for reading!
+        read_prefix(tempRawMessage);
+    }
+
+    void P2PSocket::read_prefix()
+    {
+        
+        tempMessage->prefix = new char[nodes->p2p_node->net()->PREFIX_LENGTH];
+
+        boost::asio::async_read(socket,
+                                boost::asio::buffer(tempMessage->prefix, nodes->p2p_node->net()->PREFIX_LENGTH),
+                                [this](boost::system::error_code ec, std::size_t /*length*/) {
+                                    if (!ec && c2pool::str::compare_str(tempMessage->prefix, nodes->p2p_node->net()->PREFIX, nodes->p2p_node->net()->PREFIX_LENGTH))
+                                    {
+                                        c2pool::python::other::debug_log(tempMessage->prefix, nodes->p2p_node->net()->PREFIX_LENGTH);
+                                        // LOG_INFO << "MSG: " << tempMessage->command;
+                                        read_command();
+                                    }
+                                    else
+                                    {
+                                        LOG_ERROR << ec << " " << ec.message();
+                                        disconnect();
+                                    }
+                                });
+    }
+
+    void P2PSocket::read_command(shared_ptr<raw_message> tempRawMessage)
+    {
+        boost::asio::async_read(socket,
+                                boost::asio::buffer(tempMessage->command, tempMessage->command_length),
+                                [this](boost::system::error_code ec, std::size_t /*length*/) {
+                                    if (!ec)
+                                    {
+                                        c2pool::python::other::debug_log(tempMessage->command, tempMessage->command_length);
+                                        //LOG_INFO << "read_command";
+                                        read_length();
+                                    }
+                                    else
+                                    {
+                                        LOG_ERROR << ec << " " << ec.message();
+                                        disconnect();
+                                    }
+                                });
+    }
+
+    void P2PSocket::read_length(shared_ptr<raw_message> tempRawMessage)
+    {
+        boost::asio::async_read(socket,
+                                boost::asio::buffer(tempMessage->length, tempMessage->payload_length),
+                                [this](boost::system::error_code ec, std::size_t /*length*/) {
+                                    if (!ec)
+                                    {
+                                        c2pool::python::other::debug_log(tempMessage->length, tempMessage->payload_length);
+                                        tempMessage->set_unpacked_length();
+                                        // LOG_INFO << "read_length";
+                                        read_checksum();
+                                    }
+                                    else
+                                    {
+                                        LOG_ERROR << ec << " " << ec.message();
+                                        disconnect();
+                                    }
+                                });
+    }
+
+    void P2PSocket::read_checksum(shared_ptr<raw_message> tempRawMessage)
+    {
+        boost::asio::async_read(socket,
+                                boost::asio::buffer(tempMessage->checksum, tempMessage->checksum_length),
+                                [this](boost::system::error_code ec, std::size_t /*length*/) {
+                                    if (!ec)
+                                    {
+                                        c2pool::python::other::debug_log(tempMessage->checksum, tempMessage->checksum_length);
+                                        // LOG_INFO << "read_checksum";
+                                        read_payload();
+                                    }
+                                    else
+                                    {
+                                        LOG_ERROR << ec << " " << ec.message();
+                                        disconnect();
+                                    }
+                                });
+    }
+    void P2PSocket::read_payload(shared_ptr<raw_message> tempRawMessage)
+    {
+        boost::asio::async_read(socket,
+                                boost::asio::buffer(tempMessage->payload, tempMessage->unpacked_length()),
+                                [this](boost::system::error_code ec, std::size_t /*length*/) {
+                                    if (!ec)
+                                    {
+                                        c2pool::python::other::debug_log(tempMessage->payload, tempMessage->unpacked_length());
+                                        // LOG_INFO << "read_payload";
+                                        //todo: move tempMesssage -> new message
+                                        read_prefix();
+                                    }
+                                    else
+                                    {
+                                        LOG_ERROR << ec << " " << ec.message();
+                                        disconnect();
+                                    }
+                                });
+    }
 } // namespace c2pool::p2p
