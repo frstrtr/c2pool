0e939e970f7176a28c49b09a7a892869263d5bd0
neels99 <vasile.danil1@gmail.com>
2021-03-29 18:27:41 +0300

    update coind_jsonrpc, network

--- networks/coind_networks/digibyte.cpp
+++ networks/coind_networks/digibyte.cpp
@@ -1,9 +1,12 @@
 #include <networks/network.h>
 #include <btclibs/uint256.h>
+#include <coind/jsonrpc/coind.h>
 
 #include <vector>
 #include <string>
 #include <tuple>
+#include <memory>
+using std::shared_ptr;
 
 namespace c2pool
 {
@@ -29,5 +32,29 @@ namespace c2pool
         REAL_CHAIN_LENGTH = 24 * 60 * 60 / 10;
         CHAIN_LENGTH = 24 * 60 * 60 / 10;
         SPREAD = 30;
+    }
+
+    bool DigibyteNetwork::jsonrpc_check(shared_ptr<c2pool::coind::jsonrpc::Coind> coind)
+    {
+        // defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+        //     (yield helper.check_block_header(bitcoind, '7497ea1b465eb39f1c8f507bc877078fe016d6fcb6dfad3a64c98dcc6e1e8496')) and # genesis block
+        //     (yield bitcoind.rpc_getblockchaininfo())['chain'] != 'test'
+        // ))
+        uint256 blockheader;
+        blockheader.SetHex("7497ea1b465eb39f1c8f507bc877078fe016d6fcb6dfad3a64c98dcc6e1e8496");
+
+        bool check_header = coind->check_block_header(blockheader);
+        auto chain_type = coind->GetBlockChainInfo()["chain"].get_str();
+        return check_header && (chain_type != "test");
+    }
+
+    bool DigibyteNetwork::version_check(int version)
+    {
+        //lambda v: None if 7170200 <= v else 'DigiByte version too old. Upgrade to 7.17.2 or newer!'
+        if (7170200 <= version){
+            return true;
+        } else {
+            return false;
+        }
     }
 } // namespace c2pool
