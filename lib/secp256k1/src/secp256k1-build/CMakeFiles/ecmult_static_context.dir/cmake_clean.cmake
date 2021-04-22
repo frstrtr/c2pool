3c3894d8e832a896101af1fbf60363d959b409ef
neels99 <vasile.danil1@gmail.com>
2021-04-22 14:55:09 +0300

    added worked CMake lib Secp256k1

---
+++ lib/secp256k1/src/secp256k1-build/CMakeFiles/ecmult_static_context.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ecmult_static_context"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ecmult_static_context.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lib/secp256k1/src/secp256k1-build/CMakeFiles/secp256k1.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sl33n/c2pool/lib/secp256k1/src/secp256k1/src/secp256k1.c" "/home/sl33n/c2pool/lib/secp256k1/src/secp256k1-build/CMakeFiles/secp256k1.dir/src/secp256k1.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "ENABLE_MODULE_ECDH"
+  "ENABLE_MODULE_RECOVERY"
+  "HAVE_BUILTIN_EXPECT"
+  "HAVE___INT128"
+  "USE_ECMULT_STATIC_PRECOMPUTATION"
+  "USE_FIELD_5X52"
+  "USE_FIELD_INV_BUILTIN"
+  "USE_NUM_NONE"
+  "USE_SCALAR_4X64"
+  "USE_SCALAR_INV_BUILTIN"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/sl33n/c2pool/lib/secp256k1/src/secp256k1"
+  "/home/sl33n/c2pool/lib/secp256k1/src/secp256k1/src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
