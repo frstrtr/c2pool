5d617fd669cd4bac9fcf3e2eea1a0e661c342bf2
neels99 <vasile.danil1@gmail.com>
2021-07-10 12:35:16 +0300

    [test] Added test for python module

--- test/util_tests/python_test.cpp
+++ test/util_tests/python_test.cpp
@@ -1,26 +1,10 @@
 #include <gtest/gtest.h>
 #include <iostream>
-#include <memory>
+#include <util/pystruct.h>
 using namespace std;
 
-TEST(Py, TestWitoutPy)
+TEST(Py, TestWorkPyPackTypes)
 {
-    cout << "Work" << endl;
+    ASSERT_EQ(c2pool::python::PyPackTypes::is_worked(), true);
+    cout << "PyPackTypes is worked" << endl;
 }
-
-// TEST(Py, TestSharedPtr)
-// {
-//     shared_ptr<int> p1 = make_shared<int>(100);
-//     vector<shared_ptr<int>> vp;
-//     vp.push_back(p1);
-//     *p1 = 101;
-//     cout << p1.use_count() << " " << *p1 << " | " << *vp[0] << endl;
-//     //p1 = make_shared<int>(200);
-//     p1.reset();
-//     cout << p1.use_count() << " " << *p1 << " | " << *vp[0] << endl;
-//     int* p2 = vp[0].get();
-//     vp.pop_back();
-//     cout << vp.size() << endl;
-//     cout << vp[0].use_count() << endl;
-//     cout << *p2 << " | " << *vp[0] << endl;
-// }
