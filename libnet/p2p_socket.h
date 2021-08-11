9119f049153a6de1400ab939b63fef72133d0242
neels99 <vasile.danil1@gmail.com>
2021-08-11 09:58:40 +0300

    [feature] p2p socket finally updated

--- libnet/p2p_socket.h
+++ libnet/p2p_socket.h
@@ -38,9 +38,6 @@ namespace c2pool::libnet::p2p
 
         void init(protocol_handle handle);
 
-        template <class protocol_type>
-        void set_protocol_type_and_version(protocol_handle handle, std::shared_ptr<raw_message> raw_message_version);
-
         bool isConnected() const { return _socket.is_open(); }
         ip::tcp::socket &get() { return _socket; }
         void disconnect() { _socket.close(); }
@@ -61,11 +58,12 @@ namespace c2pool::libnet::p2p
 
     private:
         void start_read();
-        void read_prefix(std::shared_ptr<raw_message> tempRawMessage);
-        void read_command(std::shared_ptr<raw_message> tempRawMessage);
-        void read_length(std::shared_ptr<raw_message> tempRawMessage);
-        void read_checksum(shared_ptr<raw_message> tempRawMessage);
-        void read_payload(std::shared_ptr<raw_message> tempRawMessage);
+        void read_prefix(std::shared_ptr<ReadPackedMsg> msg);
+        void read_command(std::shared_ptr<ReadPackedMsg> msg);
+        void read_length(std::shared_ptr<ReadPackedMsg> msg);
+        void read_checksum(std::shared_ptr<ReadPackedMsg> msg);
+        void read_payload(std::shared_ptr<ReadPackedMsg> msg);
+        void final_read_message(std::shared_ptr<ReadPackedMsg> msg)
 
         void write_prefix(std::shared_ptr<base_message> msg);
         void write_message_data(std::shared_ptr<base_message> msg);
@@ -75,58 +73,4 @@ namespace c2pool::libnet::p2p
 
         std::weak_ptr<c2pool::libnet::p2p::Protocol> _protocol;
     };
-
-    struct StreamMessageData
-    {
-        char *prefix;
-        int32_t prefix_len;
-        char *command;
-        char *length;
-        char *checksum;
-        char *payload;
-
-        StreamMessageData(shared_ptr<base_message> msg, shared_ptr<c2pool::Network> _net)
-        {
-            //prefix
-            prefix = new char[_net->PREFIX_LENGTH];
-            prefix_len = _net->PREFIX_LENGTH;
-            memcpy(prefix, _net->PREFIX_LENGTH, _net->PREFIX/_LENGTH);
-            
-            //command
-            const char* temp_cmd = c2pool::libnet::messages::string_commands(msg->cmd);
-            command = new char[12]{'\0'};
-            memcpy(command, temp_cmp, strlen(temp_cmd));
-
-            //payload
-            PackStream payload_stream;
-            payload_stream << *msg;
-            payload = new char[payload_stream.size()];
-            auto temp_payload = payload_stream.bytes();
-            memcpy(payload, temp_payload, payload_stream.size());
-
-            //len
-            PackStream stream2;
-            IntType(32) unpacked_len (payload_stream.size());
-            stream2 << IntType(32);
-            auto temp_len = stream2.bytes();
-            length = new char[4];
-            memcpy(length, temp_len, 4);
-
-            //checksum
-            //TODO: sha256(sha256(payload))
-            
-        }
-
-        PackStream &write(PackStream &stream) override
-        {
-            stream << command << error_text;
-            return stream;
-        }
-
-        PackStream &read(PackStream &stream) override
-        {
-            stream >> command >> error_text;
-            return stream;
-        }
-    }
 } // namespace c2pool::p2p
