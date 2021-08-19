4c34b2ed752f7fa7195d53feb3033699e32ccc10
neels99 <vasile.danil1@gmail.com>
2021-08-19 14:26:02 +0300

    [feature] Added c2pool::math

---
+++ util/math.h
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <string>
+#include <initializer_list>
+#include <btclibs/uint256.h>
+
+using namespace std;
+
+//TODO:
+// skipped:
+// - generator nth
+// - geometric, reversed, flatten_linked_list [useless, for skiplist]
+// - perfect_round [lol]
+//  For tests:
+//      - add_tuples
+//      - reversed
+//  For statistics:
+//      - format, format_dt
+//      - erf
+//      - find_root
+//      - ierf
+//      - binomial_conf_interval
+//      - format_binomial_conf
+//      - weighted_choice
+//      - RateMonitor
+
+namespace c2pool::math
+{
+    template <typename T>
+    auto median(vector<T> values)
+    {
+        size_t size = values.size();
+
+        if (size == 0)
+        {
+            //throw?
+            return 0;
+        }
+        else
+        {
+            sort(values.begin(), values.end());
+            if (size % 2 == 0)
+            {
+                return (values[size / 2 - 1] + values[size / 2]) / 2;
+            }
+            else
+            {
+                return values[size / 2];
+            }
+        }
+    }
+
+    template <typename T>
+    auto mean(vector<T> values)
+    {
+        T total;
+        int count = 0;
+
+        for (auto v : values)
+        {
+            total += v;
+            count += 1;
+        }
+
+        return total / count;
+    }
+
+    template <typename T>
+    T clip(T value, T low, T high)
+    {
+        return clamp(value, low, high);
+    }
+
+    //without alphabet arg for base58
+    string natural_to_string(uint256 n)
+    {
+        auto s = n.GetHex();
+        if (s.length() % 2)
+        {
+            s = string("0") + s;
+        }
+        return s;
+    }
+
+    // string_to_natural
+
+    //example: merge_dicts({map1, map2, map3});
+    template <typename KEY, typename VALUE>
+    map<KEY, VALUE> merge_dicts(std::initializer_list<map<KEY, VALUE>> dicts)
+    {
+        map<KEY, VALUE> result;
+        for (auto dict : dicts)
+        {
+            for (auto pair : dict)
+            {
+                result[pair.first] = pair.second;
+            }
+        }
+
+        return result;
+    }
+
+}
