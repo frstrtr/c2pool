1f92e38a42a5568e64eec1805dcac1b2f59dcba1
neels99 <vasile.danil1@gmail.com>
2021-09-25 12:43:40 +0300

    [chore] Update coind::ParentNetwork, POW_FUNC

--- networks/network.h
+++ networks/network.h
@@ -1,11 +1,13 @@
 #pragma once
 
-#include "btclibs/uint256.h"
-
 #include <vector>
 #include <string>
 #include <tuple>
 #include <memory>
+
+#include "btclibs/uint256.h"
+#include <util/stream.h>
+
 using std::shared_ptr;
 
 namespace coind::jsonrpc
@@ -101,7 +103,7 @@ namespace coind
 
         virtual bool version_check(int version) = 0;
 
-        virtual uint256 POW_FUNC(char* packed_block_header) = 0;
+        virtual uint256 POW_FUNC(PackStream packed_block_header) = 0;
         //TODO: virtual /*todo: type*/ POW_FUNC(/*todo: block_header_type.pack(uint256)*/);
     };
 
