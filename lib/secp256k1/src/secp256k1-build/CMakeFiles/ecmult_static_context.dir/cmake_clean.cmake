e0b38f35d5654f894afd2fc5805ac13e781c3521
neels99 <vasile.danil1@gmail.com>
2021-09-27 13:09:11 +0300

    [git] Update .gitignore for secp256k1 for cmake/ProjectSecp256k1.cmake

--- lib/secp256k1/src/secp256k1-build/CMakeFiles/ecmult_static_context.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ecmult_static_context"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ecmult_static_context.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- lib/secp256k1/src/secp256k1-build/CMakeFiles/secp256k1.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libsecp256k1.a"
-)
