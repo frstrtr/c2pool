3cac1c1008365ec3705333b9889b7907d514b7b0
neels99 <vasile.danil1@gmail.com>
2021-10-25 23:44:17 +0300

    [refactor] bug with txs

--- coind/transaction.cpp
+++ coind/transaction.cpp
@@ -75,6 +75,80 @@ namespace coind::data::stream
         stream >> version >> marker >> flag >> tx_ins >> tx_outs;
         return stream;
     }
+
+// #define WitnessType ListType<StrType>
+//     PackStream &TransactionType_stream::read(PackStream &stream)
+//     {
+//         IntType(32) version;
+//         stream >> version;
+
+//         VarIntType marker;
+//         stream >> marker;
+
+//         if (marker.value == 0)
+//         {
+//             WTXType next;
+//             stream >> next;
+
+//             vector<WitnessType> _witness;
+//             for (int i = 0; i < next.tx_ins.l.size(); i++)
+//             {
+//                 WitnessType _wit;
+//                 stream >> _wit;
+//                 _witness.push_back(_wit);
+//             }
+
+//             IntType(32) locktime;
+//             stream >> locktime;
+
+//             vector<TxInType> tx_ins;
+//             for (auto tx_in_stream : next.tx_ins.l)
+//             {
+//                 auto ptr_tx_in = make_shared<coind::data::stream::TxInType_stream>(tx_in_stream);
+//                 TxInType tx_in(ptr_tx_in);
+//                 tx_ins.push_back(tx_in);
+//             }
+
+//             vector<TxOutType> tx_outs;
+//             for (auto tx_out_stream : next.tx_outs.l)
+//             {
+//                 auto ptr_tx_out = make_shared<coind::data::stream::TxOutType_stream>(tx_out_stream);
+//                 TxOutType tx_out(ptr_tx_out);
+//                 tx_outs.push_back(tx_out);
+//             }
+
+//             tx = std::make_shared<coind::data::WitnessTransactionType>(version.get(), marker.value, next.flag.value, tx_ins, tx_outs, _witness, locktime.value);
+//         }
+//         else
+//         {
+//             vector<TxInType> tx_ins;
+//             for (int i = 0; i < marker.value; i++)
+//             {
+//                 TxInType_stream tx_in_stream;
+//                 stream >> tx_in_stream;
+
+//                 auto ptr_tx_in = make_shared<coind::data::stream::TxInType_stream>(tx_in_stream);
+//                 TxInType tx_in(ptr_tx_in);
+//                 tx_ins.push_back(tx_in);
+//             }
+
+//             NTXType next;
+//             stream >> next;
+
+//             vector<TxOutType> tx_outs;
+//             for (auto tx_out_stream : next.tx_outs.l)
+//             {
+//                 auto ptr_tx_out = make_shared<coind::data::stream::TxOutType_stream>(tx_out_stream);
+//                 TxOutType tx_out(ptr_tx_out);
+//                 tx_outs.push_back(tx_out);
+//             }
+
+//             tx = std::make_shared<coind::data::TransactionType>(version.get(), tx_ins, tx_outs, next.lock_time.value);
+//         }
+
+//         return stream;
+//     }
+// #undef WitnessType
 }
 
 coind::data::TxIDType::TxIDType(int32_t _version, vector<TxInType> _tx_ins, vector<TxOutType> _tx_outs,
@@ -92,13 +166,15 @@ coind::data::TxIDType::TxIDType(coind::data::stream::TxIDType_stream obj)
 
     for (auto v : obj.tx_ins.l)
     {
-        TxInType tx_in(v);
+        auto ptr_tx_in = make_shared<coind::data::stream::TxInType_stream>(v);
+        TxInType tx_in(ptr_tx_in);
         tx_ins.push_back(tx_in);
     }
 
     for (auto v : obj.tx_outs.l)
     {
-        TxOutType tx_out(v);
+        auto ptr_tx_out = make_shared<coind::data::stream::TxOutType_stream>(v);
+        TxOutType tx_out(ptr_tx_out);
         tx_outs.push_back(tx_out);
     }
 
@@ -111,10 +187,10 @@ coind::data::TxOutType::TxOutType(int64_t _value, string _script)
     script = _script;
 }
 
-coind::data::TxOutType::TxOutType(coind::data::stream::TxOutType_stream obj)
+coind::data::TxOutType::TxOutType(std::shared_ptr<stream::TxOutType_stream> obj)
 {
-    value = obj.value.value;
-    script = obj.script.get();
+    value = obj->value.value;
+    script = obj->script.get();
 }
 
 coind::data::TxInType::TxInType()
@@ -131,11 +207,11 @@ coind::data::TxInType::TxInType(coind::data::PreviousOutput _previous_output, ch
     sequence = _sequence;
 }
 
-coind::data::TxInType::TxInType(coind::data::stream::TxInType_stream obj)
+coind::data::TxInType::TxInType(std::shared_ptr<coind::data::stream::TxInType_stream> obj)
 {
-    previous_output = PreviousOutput(obj.previous_output.get());
-    script = obj.script.get();
-    sequence = obj.sequence.get().value;
+    previous_output = PreviousOutput(obj->previous_output.get());
+    script = obj->script.get();
+    sequence = obj->sequence.get().value;
 }
 
 coind::data::PreviousOutput::PreviousOutput(coind::data::stream::PreviousOutput_stream obj)
