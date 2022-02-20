2c05ffcf2fa904ef86f5d5e84be0eab7dd8b3e01
neels99 <vasile.danil1@gmail.com>
2022-02-20 18:05:50 +0300

    [feature/test] Added set method for VariableDict

--- test/devcore_tests/events_test.cpp
+++ test/devcore_tests/events_test.cpp
@@ -2,6 +2,19 @@
 #include <libdevcore/events.h>
 #include <iostream>
 
+template <typename Key, typename Value>
+bool eq_maps(const std::map<Key, Value> &m1, const std::map<Key, Value> &m2)
+{
+    for (auto val: m1)
+    {
+        if (m2.find(val.first) == m2.end())
+            return false;
+        if (val.second != m2.find(val.first)->second)
+            return false;
+    }
+    return true;
+}
+
 TEST(DevcoreEvents, event_lambda)
 {
 	int res = 0;
@@ -163,4 +176,18 @@ TEST(DevcoreEvents, variabledict_varinheritance)
     var.remove(empty_keys);
 
     var.add(5,6);
+}
+
+TEST(DevcoreEvents, variabledict_set)
+{
+    VariableDict<int, int> var({{1,2}, {3,5}});
+
+    var.changed->subscribe([](std::map<int,int> val){
+        std::cout << "changed" << std::endl;
+        std::map<int, int> new_var_value = {{0,2}, {2,3}};
+        ASSERT_TRUE(eq_maps(val, new_var_value));
+    });
+
+    std::map<int, int> new_var_value = {{0,1}, {2,3}};
+    var = new_var_value;
 }
