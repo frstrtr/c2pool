9a8abca3e1509026ba7efcaf628179a29a11e0e4
neels99 <vasile.danil1@gmail.com>
2022-03-09 19:07:07 +0300

    [future/refactor/test] ShareAdapter to StreamAdapter; Updated Adapter functional

--- sharechains/share_adapters.h
+++ sharechains/share_adapters.h
@@ -8,44 +8,16 @@
 #include "share_types.h"
 #include "share_streams.h"
 
-template<typename ValueType, StreamObjType StreamType>
-class BasicShareTypeAdapter
+struct SmallBlockHeaderType :
+        BasicShareTypeAdapter<shares::SmallBlockHeaderType, shares::stream::SmallBlockHeaderType_stream>
 {
-private:
-    std::shared_ptr<ValueType> _value;
-    std::shared_ptr<StreamType> _stream;
-
-public:
-    BasicShareTypeAdapter() = default;
-
-    std::shared_ptr<ValueType> operator->()
+    void to_stream() override
     {
-        return value;
+
     }
 
-    PackStream &write(PackStream &stream)
+    void to_value() override
     {
-        stream << *_stream;
-        return stream;
-    }
 
-    PackStream &read(PackStream &stream)
-    {
-        stream >> *_stream;
-        return stream;
     }
-
-    std::shared_ptr<ValueType> get()
-    {
-        return _value;
-    }
-
-    std::shared_ptr<StreamType> stream()
-    {
-        return _stream;
-    }
-
-private:
-    virtual void to_stream() = 0;
-    virtual void to_value() = 0;
 };
