0d1bedb6b02a6880874e7e7a812e04ee251c88c4
neels99 <vasile.danil1@gmail.com>
2022-03-12 06:38:00 +0300

    [refactor/feature] Updated ShareStore

--- libdevcore/db.h
+++ libdevcore/db.h
@@ -3,6 +3,7 @@
 #include <string>
 
 #include <leveldb/db.h>
+#include <leveldb/slice.h>
 #include <boost/filesystem.hpp>
 
 class DBObject;
--- sharechains/share_store.h
+++ sharechains/share_store.h
@@ -6,19 +6,37 @@
 
 class ShareStore
 {
-public:
+private:
 	unique_ptr<Database> shares;
 	unique_ptr<Database> verified_shares;
 public:
 	ShareStore()
 	{
-		shares = std::make_unique<Database>(boost::filesystem::);
+		shares = std::make_unique<Database>(c2pool::filesystem::getProjectPath(), "shares");
+		verified_shares = std::make_unique<Database>(c2pool::filesystem::getProjectPath(), "shares_verified");
 	}
 
 public:
-	void add_share(std::shared_ptr<Share> share)
+	void add_share(ShareType share)
 	{
+		PackStream packed_share;
+		packed_share << *share;
 
+		leveldb::Slice key(share->hash, sizeof(share->hash));
+		leveldb::Slice value((char*) packed_share.data.data(), packed_share.size());
+
+		shares->write(key, value);
+	}
+
+	void add_verified(ShareType share)
+	{
+		PackStream packed_share;
+		packed_share << *share;
+
+		leveldb::Slice key(share->hash, sizeof(share->hash));
+		leveldb::Slice value((char*) packed_share.data.data(), packed_share.size());
+
+		verified_shares->write(key, value);
 	}
 
 };
