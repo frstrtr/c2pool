acabf0c9ed897242059836d418c80c381e789e5c
neels99 <vasile.danil1@gmail.com>
2022-03-13 20:18:52 +0300

    [test] Worked test with leveldb

--- test/devcore_tests/database.cpp
+++ test/devcore_tests/database.cpp
@@ -19,22 +19,37 @@ TEST(Database, boost_database)
 	std::cout << "Opened, status: " << status_open.ToString() << std::endl;
 
 	int32_t key = 123;
-	std::vector<unsigned char> value_data = {0x12, 0xaf, 0x0, 0x21, 0xff};
+	char *key_packed = reinterpret_cast<char *>(&key);
+    int32_t len = sizeof(key) / sizeof(*key_packed);
+	std::vector<unsigned char> value = {0x12, 0xaf, 0x0, 0x21, 0xff};
+	char* value_data = (char*) value.data();
 	std::cout << "ok" << std::endl;
 
 	leveldb::WriteBatch batch;
-	leveldb::Slice slice_key((char*)key,  sizeof(key));
-	leveldb::Slice slice_value((char*)value_data.data(),  value_data.size());
+	leveldb::Slice slice_key(key_packed,  len);
+	leveldb::Slice slice_value(value_data,  value.size());
 	std::cout << "Sliced" << std::endl;
 	db->Put(leveldb::WriteOptions(), slice_key, slice_value);
 
-	std::cout << "Put: ok!" << std::endl;
-	std::string* readed_data;
+//	db->Write(leveldb::WriteOptions(),)
+
+	std::string* readed_data = new std::string();
 	db->Get(leveldb::ReadOptions(), slice_key, readed_data);
 	leveldb::Slice slice_read(*readed_data);
 
 	unsigned char* readed_unpack_data = (unsigned char*) slice_read.data();
+	std::cout << "Unpacked!" << std::endl;
 	std::vector<unsigned char> value_data2(readed_unpack_data, readed_unpack_data+5);
-	ASSERT_EQ(value_data, value_data2);
+	ASSERT_EQ(value, value_data2);
+
+	for(auto v : value){
+		std::cout << (unsigned int) v << " ";
+	}
+	std::cout << std::endl;
+
+	for(auto v : value_data2){
+		std::cout << (unsigned int) v << " ";
+	}
+	std::cout << std::endl;
 
 }
