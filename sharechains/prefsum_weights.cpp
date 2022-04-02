5bbd2c3d274240dcfb334bae3b91bcd9becd8501
neels99 <vasile.danil1@gmail.com>
2022-04-02 23:23:30 +0300

    [feature] Updated PrefsumShare for PrefsumWeight; Updated PrefsumWeight;

--- sharechains/prefsum_weights.cpp
+++ sharechains/prefsum_weights.cpp
@@ -3,40 +3,49 @@
 
 namespace shares::weight
 {
-	weight_element_ptr::weight_element_ptr(std::vector<unsigned char> _key, arith_uint256 _value)
-	{
-		value = std::make_shared<weight_element>(std::make_pair(_key, _value));
-	}
 
-	std::shared_ptr<weight_element> weight_element_ptr::operator->()
-	{
-		return value;
-	}
-
-	bool weight_element_ptr::is_null() const
-	{
-		return value ? true : false;
-	}
-
-	weight_element_ptr weight_element::operator+(weight_element_ptr element)
-	{
-		element->prev = shared_from_this();
-		return element;
-	}
+//	weight_element_ptr weight_element::operator+(weight_element_ptr element)
+//	{
+//		this->prev = element;
+//		return shared_from_this();
+//	}
 
 	weight_element_ptr weight_element::operator+=(const weight_element_ptr &element)
 	{
 		prev = element;
-		return ;
+		return shared_from_this();
+	}
+
+//	weight_element_ptr weight_element::operator-(weight_element_ptr element)
+//	{
+//		if (this->prev != element)
+//		{
+//			//TODO: assert/debug_error
+//		}
+//
+//		this->prev = nullptr;
+//		return element;
+//	}
+
+
+	weight_element_ptr weight_element::operator-=(const weight_element_ptr &element)
+	{
+		if (this->prev != element)
+		{
+			//TODO: assert/debug_error
+		}
+
+		this->prev = nullptr;
+		return shared_from_this();
 	}
 }
-
-namespace shares::weight
-{
-    PrefsumWeights::PrefsumWeights(std::shared_ptr<ShareTracker> tracker) : _tracker(tracker)
-    {
-        tracker->removed.subscribe([&](ShareType share){
-//TODO:            remove(share);
-        });
-    }
-}
+//
+//namespace shares::weight
+//{
+//    PrefsumWeights::PrefsumWeights(std::shared_ptr<ShareTracker> tracker) : _tracker(tracker)
+//    {
+//        tracker->removed.subscribe([&](ShareType share){
+////TODO:            remove(share);
+//        });
+//    }
+//}
