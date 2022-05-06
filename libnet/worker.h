7f659f9700c87dbc872fb921afc066b88725636f
neels99 <vasile.danil1@gmail.com>
2022-05-06 21:26:42 +0300

    [feature] Update worker for get_work(...)

--- libnet/worker.h
+++ libnet/worker.h
@@ -11,9 +11,15 @@
 
 using std::shared_ptr;
 
-namespace c2pool{
-    namespace libnet::p2p{
-        class P2PNode;
+namespace c2pool
+{
+    namespace libnet
+    {
+        class CoindNode;
+        namespace p2p
+        {
+            class P2PNode;
+        }
     }
 }
 
@@ -34,6 +40,24 @@ namespace c2pool::libnet
 		MerkleLink merkle_link;
 		int64_t subsidy;
 	};
+
+    struct NotifyData
+    {
+        int32_t version;
+        uint256 previous_block;
+        coind::data::MerkleLink merkle_link;
+        std::string coinb1;
+        std::string coinb2;
+        int32_t timestamp;
+        uint32_t bits;
+        uint256 share_target;
+    };
+
+    struct worker_get_work_result
+    {
+        NotifyData ba;
+        std::function<int()> get_response; //TODO: change arguments
+    };
 }
 
 namespace c2pool::libnet
@@ -41,16 +65,19 @@ namespace c2pool::libnet
 	class Worker
 	{
 	public:
-		Worker(std::shared_ptr<c2pool::Network> net, std::shared_ptr<c2pool::libnet::p2p::P2PNode> p2p_node, std::shared_ptr<ShareTracker> tracker);
-		//TODO: return type
-		void get_work(uint160 pubkey_hash, uint256 desired_share_target, uint256 desired_pseudoshare_target);
+		Worker(std::shared_ptr<c2pool::Network> net, std::shared_ptr<c2pool::libnet::p2p::P2PNode> p2p_node, std::shared_ptr<c2pool::libnet::CoindNode> coind_node, std::shared_ptr<ShareTracker> tracker);
+
+        worker_get_work_result get_work(uint160 pubkey_hash, uint256 desired_share_target, uint256 desired_pseudoshare_target);
 
 	private:
 		std::shared_ptr<c2pool::Network> _net;
 		std::shared_ptr<c2pool::libnet::p2p::P2PNode> _p2p_node;
+        std::shared_ptr<c2pool::libnet::CoindNode> _coind_node;
 		std::shared_ptr<ShareTracker> _tracker;
 	public:
 		Variable<Work> current_work;
+        Event<> new_work;
+
 		double donation_percentage; // TODO: init
 	};
 }
