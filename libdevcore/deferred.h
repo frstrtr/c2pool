7a9807389025a604d88a4ada01d00938e960748c
neels99 <vasile.danil1@gmail.com>
2022-05-25 10:56:21 +0300

    [feature/test] Updated deferred::ReplyMatcher

--- libdevcore/deferred.h
+++ libdevcore/deferred.h
@@ -67,15 +67,122 @@ namespace c2pool::deferred
         }
     };
 
+    template <typename ReturnType>
+    class result_reply
+    {
+    private:
+        std::promise<ReturnType> _promise;
+        std::shared_future<ReturnType> _future;
+
+        std::vector<std::function<void(ReturnType)>> callbacks;
+    public:
+        boost::asio::steady_timer await_timer;
+        boost::asio::steady_timer timeout_timer;
+    private:
+        void cancel()
+        {
+            await_timer.cancel();
+            timeout_timer.cancel();
+        }
+
+        void await_result(const boost::system::error_code &ec)
+        {
+            if (!ec.failed())
+            {
+                if (is_ready(_future))
+                {
+                    auto result = _future.get();
+                    for (auto v : callbacks)
+                    {
+                        v(result);
+                    }
+                    cancel();
+                    return;
+                }
+
+                await_timer.expires_from_now(100ms);
+                await_timer.async_wait(std::bind(&result_reply<ReturnType>::await_result, this, std::placeholders::_1));
+            }
+        }
+
+        void await_timeout(const boost::system::error_code &ec)
+        {
+            if (!ec)
+            {
+                _promise.set_exception(make_exception_ptr(std::runtime_error("ReplyMatcher timeout!")));
+                cancel();
+            }
+        }
+    public:
+        // + 10 milliseconds -- tip for call await_result before await_timeout if t == timeout
+        result_reply(std::shared_ptr<io::io_context> _context, time_t t) : timeout_timer(*_context, std::chrono::seconds(t) + std::chrono::milliseconds(10)),
+                                                                           await_timer(*_context, 1ms)
+        {
+            _future = _promise.get_future().share();
+        }
+
+        void add_callback(std::function<void(ReturnType)> _callback)
+        {
+            callbacks.push_back(_callback);
+        }
+
+        void await()
+        {
+            await_timer.async_wait(std::bind(&result_reply<ReturnType>::await_result, this, std::placeholders::_1));
+            timeout_timer.async_wait(std::bind(&result_reply<ReturnType>::await_timeout, this, std::placeholders::_1));
+        }
+
+        void set_value(ReturnType val)
+        {
+            _promise.set_value(val);
+            timeout_timer.cancel();
+        }
+
+
+    };
+
+    template <typename Key, typename ReturnType, typename... Args>
+    struct ReplyMatcher2
+    {
+        std::map<Key, std::shared_ptr<result_reply<ReturnType>>> result;
+        std::function<void(Args...)> func;
+        std::shared_ptr<io::io_context> context;
+        time_t timeout_t;
+
+        ReplyMatcher2(std::shared_ptr<io::io_context> _context, std::function<void(Args...)> _func, time_t _timeout_t = 5) : context(_context), func(_func), timeout_t(_timeout_t) {}
+
+        void operator()(Key key, Args... ARGS)
+        {
+            if (!result.count(key))
+            {
+                func(ARGS...);
+
+                result[key] = std::make_shared<result_reply<ReturnType>>(context, timeout_t);
+                result[key]->await();
+            }
+        }
+
+        void got_response(Key key, ReturnType val)
+        {
+            result[key]-> template set_value(val);
+        }
+
+        void yield(Key key, std::function<void(ReturnType)> __f, Args... ARGS)
+        {
+            this->operator()(key, ARGS...);
+            result[key]->add_callback(__f);
+        }
+    };
+
     template <typename Key, typename ReturnType, typename... Args>
     struct ReplyMatcher
     {
         std::map<Key, std::shared_ptr<result_obj_type<ReturnType>>> result;
-        std::function<ReturnType(Args...)> func;
+        std::function<void(Args...)> func;
         std::shared_ptr<io::io_context> _context;
         time_t timeout_t;
 
-        ReplyMatcher(std::shared_ptr<io::io_context> context, std::function<ReturnType(Args...)> _func, time_t _timeout_t = 5) : func(_func), timeout_t(_timeout_t)
+        ReplyMatcher(std::shared_ptr<io::io_context> context, std::function<void(Args...)> _func, time_t _timeout_t = 5) : func(_func), timeout_t(_timeout_t)
         {
             _context = context;
         }
