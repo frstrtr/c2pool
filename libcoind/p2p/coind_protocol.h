2df6361d0e566b081c6821b7f027588cfb076322
neels99 <vasile.danil1@gmail.com>
2022-07-18 13:04:26 +0300

    [feature/todo] Updated ProtocolEvents for ProtocolPinger (auto send_ping) ; Removed legacy todo

--- libcoind/p2p/coind_protocol.h
+++ libcoind/p2p/coind_protocol.h
@@ -5,6 +5,7 @@
 #include <memory>
 
 #include "coind_protocol_data.h"
+#include "coind_messages.h"
 #include <libp2p/protocol.h>
 #include <libp2p/handler.h>
 #include <libp2p/protocol_events.h>
@@ -19,12 +20,19 @@ public:
 
     CoindProtocol(std::shared_ptr<boost::asio::io_context> _context, std::shared_ptr<Socket> _socket,
                   HandlerManagerPtr<CoindProtocol> _handler_manager) : Protocol<CoindProtocol>(std::move(_socket), std::move(handler_manager)),
-                                                                       ProtocolPinger(_context, 30, std::bind(&CoindProtocol::out_time_ping, this))
+                                                                       ProtocolPinger(_context, 30, std::bind(&CoindProtocol::out_time_ping, this),
+																					  [](){return 20; /*TODO: return  random.expovariate(1/100)*/}, [&](){ send_ping(); })
     {
 
     }
 
 private:
+	void send_ping()
+	{
+		auto ping_msg = std::make_shared<coind::messages::message_ping>(1234);
+		socket->write(ping_msg);
+	}
+
     void out_time_ping()
     {
         //TODO: out of ping timer;
