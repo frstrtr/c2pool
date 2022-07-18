68a6ea0e23befa1901c7034d5c3c083ad8d7c085
neels99 <vasile.danil1@gmail.com>
2022-07-18 13:04:26 +0300

    [feature/todo] Updated ProtocolEvents for ProtocolPinger (auto send_ping) ; Removed legacy todo

--- libnet2/pool_protocol.h
+++ libnet2/pool_protocol.h
@@ -7,6 +7,7 @@
 #include <btclibs/uint256.h>
 
 #include "pool_protocol_data.h"
+#include "pool_messages.h"
 #include <libp2p/protocol.h>
 #include <libp2p/handler.h>
 #include <libcoind/transaction.h>
@@ -26,8 +27,15 @@ public:
 public:
 	PoolProtocol(std::shared_ptr<boost::asio::io_context> _context, std::shared_ptr<Socket> _socket,
                  HandlerManagerPtr<PoolProtocol> _handler_manager, std::shared_ptr<PoolProtocolData> _data) : Protocol<PoolProtocol>(_socket, _handler_manager), PoolProtocolData(*_data),
-                                                                                                              ProtocolPinger(_context, 100, std::bind(&PoolProtocol::out_time_ping, this))
+                                                                                                              ProtocolPinger(_context, 100, std::bind(&PoolProtocol::out_time_ping, this),
+																															 [](){return 20; /*TODO: return  random.expovariate(1/100)*/}, [&](){ send_ping(); })
 	{}
+
+	void send_ping()
+	{
+		auto ping_msg = std::make_shared<pool::messages::message_ping>();
+		socket->write(ping_msg);
+	}
 
 	void out_time_ping()
 	{
--- libp2p/protocol_events.h
+++ libp2p/protocol_events.h
@@ -12,7 +12,7 @@ class ProtocolEvents
 {
     bool stopped = false;
 protected:
-    Event<> event_handle_message;
+    Event<> event_handle_message; // Вызывается, когда мы получаем любое сообщение.
 
     bool is_stopped() const { return stopped;}
 
@@ -23,20 +23,25 @@ protected:
 class ProtocolPinger : virtual ProtocolEvents
 {
 private:
-    time_t ping_time;
-    boost::asio::steady_timer timer;
+    time_t outtime_ping_time;
+    boost::asio::steady_timer timer_outtime;
     std::function<void()> outtimeF;
 
-    void start_ping()
+	std::function<time_t()> send_ping_time;
+	boost::asio::steady_timer timer_send_ping;
+	std::function<void()> send_pingF;
+
+    void start_outtime_ping()
     {
-        timer.expires_from_now(boost::asio::chrono::seconds(ping_time));
-        timer.async_wait([&](const boost::system::error_code &ec)
+		timer_outtime.expires_from_now(boost::asio::chrono::seconds(outtime_ping_time));
+		timer_outtime.async_wait([&](const boost::system::error_code &ec)
                          {
                              if (ec)
                              {
                                  if (ec == errc::operation_canceled)
                                  {
-                                     start_ping();
+									 // Если таймер был canceled, то он запускается заново без вызова outtimeF()
+                                     start_outtime_ping();
                                  } else
                                  {
                                      //TODO: error
@@ -48,18 +53,36 @@ private:
                          });
     }
 
+	void start_send_ping()
+	{
+		timer_send_ping.expires_from_now(boost::asio::chrono::seconds(send_ping_time()));
+		timer_send_ping.async_wait([&](const boost::system::error_code &ec){
+			if (!ec)
+			{
+				send_pingF();
+				start_send_ping();
+			} else
+			{
+				// TODO: error
+			}
+		});
+	}
+
     void restart_ping()
     {
-        timer.cancel();
+		timer_outtime.cancel();
     }
 
 public:
 
-    ProtocolPinger(std::shared_ptr<boost::asio::io_context> _context, time_t t, std::function<void()> outtime_f)
-            : ping_time(t), timer(*_context), outtimeF(std::move(outtime_f))
+    ProtocolPinger(std::shared_ptr<boost::asio::io_context> _context, time_t outtime_t, std::function<void()> outtime_f, std::function<time_t()> send_ping_t, std::function<void()> send_ping_f)
+            : outtime_ping_time(outtime_t), timer_outtime(*_context), outtimeF(std::move(outtime_f)),
+			  send_ping_time(std::move(send_ping_t)), timer_send_ping(*_context), send_pingF(std::move(send_ping_f))
     {
-        start_ping();
+        start_outtime_ping();
         event_handle_message.subscribe(std::bind(&ProtocolPinger::restart_ping, this));
+
+		start_send_ping();
     }
 
     ~ProtocolPinger() = default;
