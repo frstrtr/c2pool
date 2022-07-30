b8b3f71968b3b203318eae746af3639f81dbacbc
neels99 <vasile.danil1@gmail.com>
2022-07-30 20:05:05 +0300

    [feature/fix] Update CoindNode; CoindProtocol for libp2p

--- libcoind/p2p/coind_protocol.h
+++ libcoind/p2p/coind_protocol.h
@@ -23,10 +23,31 @@ public:
                                                                        ProtocolPinger(_context, 30, std::bind(&CoindProtocol::out_time_ping, this),
 																					  [](){return 20; /*TODO: return  random.expovariate(1/100)*/}, [&](){ send_ping(); })
     {
-
+		send_version();
     }
 
 private:
+
+	void send_version()
+	{
+		address_type addr_to(1, std::get<0>(socket->get_addr()), std::get<1>(socket->get_addr()));
+
+		//TODO: get my global ip
+		address_type addr_from(1, "192.168.0.1", 12024);
+
+		auto msg = std::make_shared<coind::messages::message_version>(
+				70017,
+				1,
+				c2pool::dev::timestamp(),
+				addr_to,
+				addr_from,
+				c2pool::random::randomNonce(),
+				"C2Pool", //TODO: generate sub_version
+				0
+		);
+		write(msg);
+	}
+
 	void send_ping()
 	{
 		auto ping_msg = std::make_shared<coind::messages::message_ping>(1234);
