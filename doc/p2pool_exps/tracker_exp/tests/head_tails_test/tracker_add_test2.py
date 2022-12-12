5cc8ced2acec6d11702c6a9c6198ca2ff635ace9
neels99 <vasile.danil1@gmail.com>
2022-12-12 11:13:45 +0300

    [exp] Exp for head/tails

---
+++ doc/p2pool_exps/tracker_exp/tests/head_tails_test/tracker_add_test2.py
@@ -0,0 +1,27 @@
+import tracker
+
+class PresudoShare:
+    
+    def __init__(self, item):
+        # print(item[1])
+        self.hash = item[0]
+        self.previous_hash = item[1]
+
+def write_ht(track):
+    print('heads: {0}'.format(track.heads))
+    print('tails: {0}'.format(track.tails))
+
+track = tracker.Tracker()
+write_ht(track)
+
+first = PresudoShare([1,0])
+track.add(first)
+write_ht(track)
+
+second = PresudoShare([3,2])
+track.add(second)
+write_ht(track)
+
+third = PresudoShare([2,1])
+track.add(third)
+write_ht(track)
