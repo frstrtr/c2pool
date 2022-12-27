a791dfff12aee6b4edf4b1d987353a750fd80421
neels99 <vasile.danil1@gmail.com>
2022-12-27 21:57:57 +0300

    [feature] Generate default config for Network/ParentNetwork

--- networks/pool_networks/digibyte.cpp
+++ networks/pool_networks/digibyte.cpp
@@ -29,9 +29,9 @@ namespace c2pool
         PREFIX = new unsigned char[PREFIX_LENGTH]{0x83, 0xE6, 0x5D, 0x2C, 0x81, 0xBF, 0x6D, 0x68};
         IDENTIFIER_LENGHT = 8;
         IDENTIFIER = new unsigned char[IDENTIFIER_LENGHT]{0x83, 0xE6, 0x5D, 0x2C, 0x81, 0xBF, 0x6D, 0x66};
+
         MINIMUM_PROTOCOL_VERSION = 1600;
         SEGWIT_ACTIVATION_VERSION = 17;
-
         TARGET_LOOKBEHIND = 200;
         SHARE_PERIOD = 25;
         BLOCK_MAX_SIZE = 1000000;
