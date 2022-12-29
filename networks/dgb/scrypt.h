6fe0798704ee3e7bec8a36210acbe93d594e4710
neels99 <vasile.danil1@gmail.com>
2022-12-29 17:05:40 +0300

    [feature (error)] bug undefined reference to `scrypt_1024_1_1_256'

--- networks/dgb/scrypt.h
+++ networks/dgb/scrypt.h
@@ -1,3 +1,7 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #ifndef SCRYPT_H
 #define SCRYPT_H
 
@@ -5,4 +9,8 @@ void scrypt_1024_1_1_256(const char* input, char* output);
 void scrypt_1024_1_1_256_sp(const char* input, char* output, char* scratchpad);
 const int scrypt_scratchpad_size = 131583;
 
+#endif
+
+#ifdef __cplusplus
+}
 #endif
