c6d41eb64c1aafe75b25c5513fc444e6b16f61f9
neels99 <vasile.danil1@gmail.com>
2023-01-11 11:04:02 +0300

    [feature] Update for GenerateShareTransaction#1

---
+++ sharechains/generate_tx.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <string>
+#include <map>
+#include <vector>
+#include <tuple>
+
+#include <boost/function.hpp>
+
+#include <btclibs/uint256.h>
+#include <networks/network.h>
+#include <libcoind/transaction.h>
+#include "share_types.h"
+#include "share.h"
+
+class ShareTracker;
+
+//GenerateShareTransaction
+namespace shares
+{
+    typedef boost::function<ShareType(coind::data::types::BlockHeaderType, uint64_t)> get_share_method;
+
+    struct GeneratedShareTransactionResult
+    {
+        std::unique_ptr<shares::types::ShareInfo> share_info;
+        coind::data::tx_type gentx;
+        std::vector<uint256> other_transaction_hashes;
+        get_share_method get_share;
+
+        GeneratedShareTransactionResult(std::unique_ptr<shares::types::ShareInfo> _share_info, coind::data::tx_type _gentx, std::vector<uint256> _other_transaction_hashes, get_share_method &_get_share);
+    };
+
+#define type_desired_other_transaction_hashes_and_fees std::vector<std::tuple<uint256, std::optional<int32_t>>>
+#define type_known_txs std::map<uint256, coind::data::tx_type>
+
+#define SetProperty(type, name)          \
+    type _##name;                        \
+                                         \
+    GenerateShareTransaction &set_##name(const type &_value){ \
+        _##name = _value;                \
+		return *this; \
+    }
+
+    class GenerateShareTransaction
+    {
+    public:
+        std::shared_ptr<ShareTracker> tracker;
+        std::shared_ptr<c2pool::Network> net;
+
+        GenerateShareTransaction(std::shared_ptr<ShareTracker> _tracker);
+
+    public:
+        SetProperty(types::ShareData, share_data);
+        SetProperty(uint256, block_target);
+        SetProperty(uint32_t, desired_timestamp);
+        SetProperty(uint256, desired_target);
+        SetProperty(coind::data::MerkleLink, ref_merkle_link);
+        SetProperty(type_desired_other_transaction_hashes_and_fees, desired_other_transaction_hashes_and_fees);
+        SetProperty(type_known_txs, known_txs);
+        SetProperty(unsigned long long, last_txout_nonce);
+        SetProperty(unsigned long long, base_subsidy);
+
+        std::optional<shares::types::SegwitData> _segwit_data;
+
+        void set_segwit_data(const shares::types::SegwitData &_value)
+        {
+            _segwit_data = _value;
+        }
+
+    public:
+        std::shared_ptr<GeneratedShareTransactionResult> operator()(uint64_t version);
+
+        arith_uint256 pre_target_calculate(ShareType previous_share, const int32_t &height);
+        std::tuple<FloatingInteger, FloatingInteger> bits_calculate(const arith_uint256 &pre_target);
+        std::tuple<vector<uint256>, vector<tuple<uint64_t, uint64_t>>, vector<uint256>> new_tx_hashes_calculate(uint256 prev_share_hash, int32_t height);
+        std::tuple<std::map<std::vector<unsigned char>, arith_uint288>> weight_amount_calculate(uint256 prev_share_hash, int32_t height);
+
+        coind::data::tx_type gentx_generate(bool segwit_activated, uint256 witness_commitment_hash, std::map<std::vector<unsigned char>, arith_uint288> amounts, unique_ptr<shares::types::ShareInfo> &share_info, const char* witness_reserved_value_str)
+    };
+
+#undef SetProperty
+#undef type_desired_other_transaction_hashes_and_fees
+#undef type_known_txs
+}
