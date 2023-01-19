dfb276d0a988642b255b011a97c8b8f37b07014d
neels99 <vasile.danil1@gmail.com>
2023-01-19 11:51:38 +0300

    [test] Python test for Worker

---
+++ doc/p2pool_exps/tracker_exp/get_work_worker_test.py
@@ -0,0 +1,123 @@
+import OkayTracker, data
+import argparse
+import net as _net
+import random
+import height_tracker
+import coind_data
+import pack
+import share as SHARE
+import p2pool_math
+import time
+import script
+import work
+import variable
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-shares', action='store', dest='shares')
+args = parser.parse_args()
+
+share_store_path = args.shares
+net = _net
+
+
+shares = {}
+def share_cb(share):
+    share.time_seen = 0 # XXX
+    shares[share.hash] = share
+    if len(shares) % 1000 == 0 and shares:
+        print("    %i" % (len(shares),))
+
+known_verified = set()
+
+share_store = data.ShareStore(net, share_store_path, share_cb, known_verified.add)
+
+# print(len(shares))
+# print(len(known_verified))
+assert(int('14a875701d88f6421211b8ffb4ba5248295905edb3bdd47312e2022abdd97374',16) in shares)
+# print('0x14a875701d88f6421211b8ffb4ba5248295905edb3bdd47312e2022abdd97374L' in known_verified)
+
+tracker = OkayTracker.OkayTracker(net)
+
+for share in shares.values():
+    tracker.add(share)
+        
+for share_hash in known_verified:
+    if share_hash in tracker.items:
+        tracker.verified.add(tracker.items[share_hash])
+
+previous_block = None
+bits = None
+known_txs_var = {}
+get_height_rel_highest = height_tracker.get_height_rel_highest_func(lambda: previous_block)#self.bitcoind, self.factory, lambda: self.bitcoind_work.value['previous_block'], self.net)
+best, desired, decorated_heads, bad_peer_addresses = tracker.think(get_height_rel_highest, previous_block, bits, known_txs_var)
+print('best = {0}'.format(hex(best)))
+
+print('\n=====================\nGenerateShareTransaction\n=====================\n')
+current_work = {'height':0, 'coinbaseflags':'', 'subsidy':0, 'transaction_fees':[], 'bits':0}
+donation_percentage = 0.0
+pubkey_hash = int('78ecd67a8695aa4adc55b70f87c2fa3279cee6d0', 16)
+desired_share_target = int('00000000359dc900000000000000000000000000000000000000000000000000', 16)
+get_stale_counts = lambda : ((0,0), 0, (0,0))
+share_type = SHARE.Share
+
+# share_info, gentx, other_transaction_hashes, get_share = share_type.generate_transaction2(
+#                 tracker=tracker,
+#                 share_data=dict(
+#                     previous_share_hash=best,
+#                     coinbase=(script.create_push_script([current_work['height']] + []) 
+#                         + current_work['coinbaseflags'])[:100],
+#                     nonce=random.randrange(2**32),
+#                     pubkey_hash=pubkey_hash,
+#                     subsidy=current_work['subsidy'],
+#                     donation=p2pool_math.perfect_round(65535*donation_percentage/100),
+#                     stale_info=(lambda (orphans, doas), total, (orphans_recorded_in_chain, doas_recorded_in_chain):
+#                         'orphan' if orphans > orphans_recorded_in_chain else
+#                         'doa' if doas > doas_recorded_in_chain else
+#                         None
+#                     )(*get_stale_counts()),
+#                     desired_version=17#(share.Share.SUCCESSOR if share.Share.SUCCESSOR is not None else share.Share).VOTING_VERSION,
+#                 ),
+#                 block_target=pack.FloatingInteger(current_work['bits']).target,
+#                 desired_timestamp=int(time.time() + 0.5),
+#                 desired_target=desired_share_target,
+#                 ref_merkle_link=dict(branch=[], index=0),
+#                 desired_other_transaction_hashes_and_fees=[],
+#                 net=net,
+#                 known_txs=[],
+#                 base_subsidy=7200000000000#net.PARENT.SUBSIDY_FUNC(current_work['height']),
+#             )
+
+# #share_info, gentx, other_transaction_hashes, get_share
+# print("RESULT:")
+# print('\nshare_info = {0}\n'.format(share_info))
+# print('gentx = {0}'.format(gentx))
+# print('other_tx_hashes = {0}'.format(other_transaction_hashes))
+# print('coinbase: [{0}], first part = [{1}], second part = [{2}]'.format((script.create_push_script([current_work['height']] + []) 
+#                         + current_work['coinbaseflags'])[:100], script.create_push_script([current_work['height']] + []), current_work['coinbaseflags']))
+
+
+print('\n=====================\nWorker::get_work\n=====================\n')
+
+class FakeNode:
+    
+    def __init__(self, net, tracker):
+        self.net = net
+        self.tracker = tracker
+
+        self.bitcoind_work = variable.Variable({})
+    #     self.best_block_header = variable.Variable({
+    #         ('version', pack.IntType(32)),
+    # ('previous_block', pack.PossiblyNoneType(0, pack.IntType(256))),
+    # ('merkle_root', pack.IntType(256)),
+    # ('timestamp', pack.IntType(32)),
+    # ('bits', FloatingIntegerType()),
+    # ('nonce', pack.IntType(32)),
+    #     })
+
+node = FakeNode(net, tracker)
+
+address = 'DKCJJSrA9vAXwwbzTbD2PL3553iKX3oPv1'
+my_pubkey_hash = coind_data.address_to_pubkey_hash(address, net.PARENT)
+pubkeys = coind_data.address_to_pubkey_hash(address, net.PARENT)
+
+wb = work.WorkerBridge(node, my_pubkey_hash, donation_percentage, 0, pubkeys)
