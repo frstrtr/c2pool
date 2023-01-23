ccb7c518fb60b6a020a057440b43c61e63341573
neels99 <vasile.danil1@gmail.com>
2023-01-23 18:18:26 +0300

    [test] Upd for test Worker#2

--- doc/p2pool_exps/tracker_exp/work.py
+++ doc/p2pool_exps/tracker_exp/work.py
@@ -258,11 +258,11 @@ class WorkerBridge(coind_worker_interface.WorkerBridge):
         if self.node.best_share_var.value is None and self.node.net.PERSIST:
             raise BaseException('p2pool is downloading shares')
             # raise jsonrpc.Error_for_code(-12345)(u'p2pool is downloading shares')
-        unknown_rules = set(r[1:] if r.startswith('!') else r for r in self.node.bitcoind_work.value['rules']) - set(getattr(self.node.net, 'SOFTFORKS_REQUIRED', []))
-        if unknown_rules:
-            print "Unknown softforks found: ", unknown_rules
-            # raise jsonrpc.Error_for_code(-12345)(u'unknown rule activated')
-            raise BaseException("unknown rule activated")
+        # unknown_rules = set(r[1:] if r.startswith('!') else r for r in self.node.bitcoind_work.value['rules']) - set(getattr(self.node.net, 'SOFTFORKS_REQUIRED', []))
+        # if unknown_rules:
+        #     print "Unknown softforks found: ", unknown_rules
+        #     # raise jsonrpc.Error_for_code(-12345)(u'unknown rule activated')
+        #     raise BaseException("unknown rule activated")
         
         #2: NOW IGNORED FOR C2POOL!!!
         # if self.merged_work.value:
