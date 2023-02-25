38e9cf4e6c0174f053d8e4cae9d610ce97aa7d4e
neels99 <vasile.danil1@gmail.com>
2023-02-25 13:50:50 +0300

    [feature] Update Prefsum for Rules#2

--- libdevcore/prefsum.h
+++ libdevcore/prefsum.h
@@ -15,49 +15,122 @@ class Rule
 public:
     std::any value;
 private:
-    typedef std::function<void(Rule)> func_type;
+    typedef std::function<void(Rule&, const Rule&)> func_type;
     /// addition function
-    func_type _add;
+    func_type *_add;
     /// subtraction function
-    func_type _sub;
+    func_type *_sub;
 public:
-    template <typename ValueType>
-    Rule(ValueType &v, func_type additionF, func_type subtractionF) : _add(std::move(additionF)), _sub(std::move(subtractionF))
+    Rule() : _add(nullptr), _sub(nullptr)
+    {
+
+    }
+
+    Rule(std::any v, func_type *additionF, func_type *subtractionF) : _add(additionF), _sub(subtractionF)
     {
         value = v;
+    }
+
+    Rule operator+(const Rule& r) const
+    {
+        auto copy_rule = Rule(*this);
+        (*_add)(copy_rule, r);
+        return copy_rule;
+    }
+
+    Rule operator-(const Rule& r) const
+    {
+        auto copy_rule = Rule(*this);
+        (*_sub)(copy_rule, r);
+        return copy_rule;
+    }
+
+    Rule &operator+=(const Rule& r)
+    {
+        (*_add)(*this, r);
+        return *this;
+    }
+
+    Rule &operator-=(const Rule& r)
+    {
+        (*_sub)(*this, r);
+        return *this;
     }
 };
 
 class PrefsumRulesElement
 {
-    std::map<std::string, std::any> rules;
+    std::map<std::string, Rule> rules;
 public:
     PrefsumRulesElement() = default;
 
-    void add(std::string& key, std::any& value)
+    void add(const std::string& key, Rule& value)
     {
-        rules[key] = value;
+        rules[key] = std::move(value);
     }
 
     template <typename TypeValue>
-    TypeValue* get(std::string &key)
+    TypeValue* get(std::string key)
     {
         if (rules.find(key) == rules.end())
             throw std::invalid_argument((boost::format("PrefsumRulesElement[key = [%1%]]: key not exist in PrefsumRulesElement") % key).str());
 
-        if (rules[key].type() != typeid(TypeValue))
-            throw std::invalid_argument((boost::format("PrefsumRulesElement[key = [%1%]]: %2% != %3%  in PrefsumRulesElement") % key % typeid(TypeValue).name() % rules[key].type().name()).str());
+        if (rules.at(key).value.type() != typeid(TypeValue))
+            throw std::invalid_argument((boost::format("PrefsumRulesElement[key = [%1%]]: %2% != %3%  in PrefsumRulesElement") % key % typeid(TypeValue).name() % rules.at(key).value.type().name()).str());
 
-        return std::any_cast<TypeValue>(&rules[key]);
+        TypeValue* result = std::any_cast<TypeValue>(&rules[key].value);
+        return result;
     }
 
     PrefsumRulesElement &operator+=(const PrefsumRulesElement &r)
     {
-        for (const auto &[k, v] : r.rules)
+        for (const auto &[k, rule] : r.rules)
         {
-
+            rules[k] += rule;
         }
         return *this;
+    }
+
+    PrefsumRulesElement &operator-=(const PrefsumRulesElement &r)
+    {
+        for (const auto &[k, rule] : r.rules)
+        {
+            rules[k] -= rule;
+        }
+        return *this;
+    }
+};
+
+template <typename ValueType>
+class PrefsumRules
+{
+    struct RuleFunc
+    {
+        // Словарь с функциями для инициализации (расчёта) новых значений правил.
+        std::function<std::any(const ValueType&)> make;
+        //
+        std::function<void(Rule&, const Rule&)> add;
+        //
+        std::function<void(Rule&, const Rule&)> sub;
+    };
+
+    std::map<std::string, RuleFunc> funcs;
+
+public:
+    void add(std::string k, std::function<std::any(const ValueType&)> _make, std::function<void(Rule&, const Rule&)> _add, std::function<void(Rule&, const Rule&)> _sub)
+    {
+        funcs[k] = {std::move(_make), std::move(_add), std::move(_sub)};
+    }
+
+    PrefsumRulesElement make_rules(const ValueType &value)
+    {
+        PrefsumRulesElement rules;
+        for (auto &[k, f] : funcs)
+        {
+            Rule rule {f.make(value), &f.add, &f.sub};
+            rules.add(k, rule);
+        }
+        return rules;
     }
 };
 
@@ -126,23 +199,6 @@ public:
     BasePrefsumElement() {}
 };
 
-template <typename ValueType>
-class PrefsumRules
-{
-    // Словарь с функциями для инициализации (расчёта) новых значений правил.
-    std::map<std::string, std::function<std::any(ValueType)>> make_func;
-
-    PrefsumRulesElement make_rules(const ValueType &value)
-    {
-        PrefsumRulesElement rules;
-        for (const auto &[k, f] : make_func)
-        {
-            rules.add(k, f(value));
-        }
-        return rules;
-    }
-};
-
 template <typename PrefsumElementType>
 class Prefsum
 {
@@ -176,7 +232,7 @@ public:
     virtual element_type make_element(value_type value)
     {
         element_type element {value};
-        element.rules = rules.make_rules();
+        element.rules = rules.make_rules(value);
         element.pvalue = items.end();
         element.prev = sum.find(element.tail);
         element.height = 1;
