f92d52e8d4ecfbe4bf0494d2f3b2ca44f312b546
neels99 <vasile.danil1@gmail.com>
2023-02-26 08:58:11 +0300

    [refactor] Remove legacy prefsum share

--- sharechains/prefsum_doa.cpp
+++
@@ -1,8 +0,0 @@
-#include "prefsum_doa.h"
-
-namespace shares
-{
-    bool doa_element_type::rules = 0;
-
-    std::function<std::tuple<int32_t, int32_t, int32_t, int32_t>(ShareType)> doa_element_type::_rules_func;
-}
