9306a6ed2c9fd299d55b969cd3dc218ce1555b1f
Alec <frstrtr@gmail.com>
2023-02-27 04:27:36 +0400

    finalized

--- doc/optimize/perfsum_rules.cpp
+++ doc/optimize/perfsum_rules.cpp
@@ -1,66 +1,64 @@
-'''   
-To enhance, make safe, and optimize the provided code, you can consider the following suggestions:
+// To enhance, make safe, and optimize the provided code, you can consider the following suggestions:
 
-Replace raw pointers with smart pointers
-The current implementation uses raw pointers to store function pointers, which can cause memory leaks and undefined behavior if not managed properly. You can replace them with smart pointers such as std::unique_ptr or std::shared_ptr to ensure proper memory management.
+// Replace raw pointers with smart pointers
+// The current implementation uses raw pointers to store function pointers, which can cause memory leaks and undefined behavior if not managed properly. You can replace them with smart pointers such as std::unique_ptr or std::shared_ptr to ensure proper memory management.
 
-Use const references instead of pass-by-value for function arguments
-The add and sub functions in the RuleFunc struct currently take the Rule argument by value, which incurs unnecessary copying. You can change them to take const Rule& instead to avoid the copying.
+// Use const references instead of pass-by-value for function arguments
+// The add and sub functions in the RuleFunc struct currently take the Rule argument by value, which incurs unnecessary copying. You can change them to take const Rule& instead to avoid the copying.
 
-Avoid using std::any when possible
-The use of std::any in the Rule class can make the code harder to understand and maintain. If possible, you can consider using templates instead to provide type safety and avoid the overhead of type erasure.
+// Avoid using std::any when possible
+// The use of std::any in the Rule class can make the code harder to understand and maintain. If possible, you can consider using templates instead to provide type safety and avoid the overhead of type erasure.
 
-Use std::unordered_map instead of std::map for faster lookups
-The PrefsumRulesElement class uses std::map to store its rules, but std::unordered_map can provide faster lookups in most cases.
+// Use std::unordered_map instead of std::map for faster lookups
+// The PrefsumRulesElement class uses std::map to store its rules, but std::unordered_map can provide faster lookups in most cases.
 
-Add error handling for std::any_cast
-The get function in the PrefsumRulesElement class uses std::any_cast to retrieve the value of a rule, but it does not handle the std::bad_any_cast exception that can be thrown if the cast fails. You can add error handling to handle this case.
+// Add error handling for std::any_cast
+// The get function in the PrefsumRulesElement class uses std::any_cast to retrieve the value of a rule, but it does not handle the std::bad_any_cast exception that can be thrown if the cast fails. You can add error handling to handle this case.
 
-Use const iterators when iterating over containers
-The operator+= and operator-= functions in the PrefsumRulesElement class use non-const iterators to iterate over the rules, which can cause the rules to be modified unintentionally. You can use const iterators instead to avoid this.
+// Use const iterators when iterating over containers
+// The operator+= and operator-= functions in the PrefsumRulesElement class use non-const iterators to iterate over the rules, which can cause the rules to be modified unintentionally. You can use const iterators instead to avoid this.
 
-Use move semantics instead of copying
-The add function in the PrefsumRulesElement class currently takes a non-const reference to a Rule object, which can prevent move semantics from being used. You can change it to take a const reference instead to allow move semantics to be used.
+// Use move semantics instead of copying
+// The add function in the PrefsumRulesElement class currently takes a non-const reference to a Rule object, which can prevent move semantics from being used. You can change it to take a const reference instead to allow move semantics to be used.
 
-To enhance, make safe, and optimize this C++ code, here are some suggestions:
+// To enhance, make safe, and optimize this C++ code, here are some suggestions:
 
-Replace std::any with a more specific type
-Using std::any can be convenient, but it can also be less efficient and make it harder to reason about the code. It would be better to use a more specific type for value in the Rule class, such as a template parameter. This way, the type of the value would be known at compile time and the code could be optimized better.
+// Replace std::any with a more specific type
+// Using std::any can be convenient, but it can also be less efficient and make it harder to reason about the code. It would be better to use a more specific type for value in the Rule class, such as a template parameter. This way, the type of the value would be known at compile time and the code could be optimized better.
 
-Use smart pointers instead of raw pointers
-The Rule class currently uses raw function pointers for the addition and subtraction functions. This can lead to memory leaks and other problems. It would be better to use smart pointers, such as std::unique_ptr, to manage the lifetime of the functions.
+// Use smart pointers instead of raw pointers
+// The Rule class currently uses raw function pointers for the addition and subtraction functions. This can lead to memory leaks and other problems. It would be better to use smart pointers, such as std::unique_ptr, to manage the lifetime of the functions.
 
-Use const references where appropriate
-In some places, the code is copying objects unnecessarily. For example, in the operator+ and operator- functions of the Rule class, a copy of the current object is made before the addition or subtraction is performed. This can be inefficient if the object is large. It would be better to use const references instead of copying the object.
+// Use const references where appropriate
+// In some places, the code is copying objects unnecessarily. For example, in the operator+ and operator- functions of the Rule class, a copy of the current object is made before the addition or subtraction is performed. This can be inefficient if the object is large. It would be better to use const references instead of copying the object.
 
-Check for null pointers before dereferencing
-The Rule class allows the addition and subtraction functions to be null pointers, which can lead to undefined behavior if they are dereferenced. It would be better to check for null pointers before calling these functions.
+// Check for null pointers before dereferencing
+// The Rule class allows the addition and subtraction functions to be null pointers, which can lead to undefined behavior if they are dereferenced. It would be better to check for null pointers before calling these functions.
 
-Add error handling to the PrefsumRulesElement class
-The PrefsumRulesElement class currently throws exceptions when an invalid key is accessed or when the type of a value does not match the expected type. It would be better to add error handling to these functions so that they can return error codes or throw exceptions with more specific error messages.
+// Add error handling to the PrefsumRulesElement class
+// The PrefsumRulesElement class currently throws exceptions when an invalid key is accessed or when the type of a value does not match the expected type. It would be better to add error handling to these functions so that they can return error codes or throw exceptions with more specific error messages.
 
-Consider using emplace instead of insert for map insertion
-The add function of the PrefsumRulesElement class currently uses the [] operator to insert a new key-value pair into the rules map. This can be less efficient than using the emplace function, which constructs the object in place.
+// Consider using emplace instead of insert for map insertion
+// The add function of the PrefsumRulesElement class currently uses the [] operator to insert a new key-value pair into the rules map. This can be less efficient than using the emplace function, which constructs the object in place.
 
-Use const iterators where appropriate
-In the operator+= and operator-= functions of the PrefsumRulesElement class, the loop iterates over the key-value pairs in the r object using a non-const reference. It would be better to use const iterators instead to avoid accidental modification of the r object.
+// Use const iterators where appropriate
+// In the operator+= and operator-= functions of the PrefsumRulesElement class, the loop iterates over the key-value pairs in the r object using a non-const reference. It would be better to use const iterators instead to avoid accidental modification of the r object.
 
-Consider using move semantics
-In some places, the code is copying objects unnecessarily. For example, in the add function of the PrefsumRulesElement class, the value object is moved into the rules map. However, the std::move function is not used to take advantage of move semantics. It would be better to use move semantics where appropriate to avoid unnecessary copies.
+// Consider using move semantics
+// In some places, the code is copying objects unnecessarily. For example, in the add function of the PrefsumRulesElement class, the value object is moved into the rules map. However, the std::move function is not used to take advantage of move semantics. It would be better to use move semantics where appropriate to avoid unnecessary copies.
 
-Consider using static_assert for type checking
-The get function of the PrefsumRulesElement class currently throws an exception if the type of the value does not match the expected type. It would be better to use static_assert to perform this check at compile time and provide a more informative error message.
+// Consider using static_assert for type checking
+// The get function of the PrefsumRulesElement class currently throws an exception if the type of the value does not match the expected type. It would be better to use static_assert to perform this check at compile time and provide a more informative error message.
 
-There are a few potential issues with this code that we can address:
+// There are a few potential issues with this code that we can address:
 
-Safety issues:
-The get function in PrefsumRulesElement assumes that the passed key is always present in the map, which may not be true. A better approach would be to use the find function to check if the key exists in the map, and if it does not, return an error or throw an exception.
-The get function also performs a type check on the value associated with the key using std::any_cast, which can throw an exception if the type is incorrect. A better approach would be to use std::any_cast with a std::optional return type, which would allow for easier error handling.
-The Rule class uses raw function pointers (*_add and _sub) to store its addition and subtraction functions, which could result in null pointer dereferences. A better approach would be to use std::function objects instead, which can be safely checked for null.
-Optimization issues:
-The Rule class stores its value as an std::any object, which can result in unnecessary dynamic allocations and type checks. A better approach would be to use a templated value type instead, which would allow for better performance and type safety.
-The PrefsumRules class uses a std::map to store its rule functions, which can be slow for large numbers of functions. A better approach would be to use an unordered map (std::unordered_map) or a compile-time map implementation (such as boost::mp11::mp_map) to improve performance.
-'''
+// Safety issues:
+// The get function in PrefsumRulesElement assumes that the passed key is always present in the map, which may not be true. A better approach would be to use the find function to check if the key exists in the map, and if it does not, return an error or throw an exception.
+// The get function also performs a type check on the value associated with the key using std::any_cast, which can throw an exception if the type is incorrect. A better approach would be to use std::any_cast with a std::optional return type, which would allow for easier error handling.
+// The Rule class uses raw function pointers (*_add and _sub) to store its addition and subtraction functions, which could result in null pointer dereferences. A better approach would be to use std::function objects instead, which can be safely checked for null.
+// Optimization issues:
+// The Rule class stores its value as an std::any object, which can result in unnecessary dynamic allocations and type checks. A better approach would be to use a templated value type instead, which would allow for better performance and type safety.
+// The PrefsumRules class uses a std::map to store its rule functions, which can be slow for large numbers of functions. A better approach would be to use an unordered map (std::unordered_map) or a compile-time map implementation (such as boost::mp11::mp_map) to improve performance.
 
 //TODO: rewrite this code to be safe, efficient, and maintainable
 
@@ -155,36 +153,104 @@ The PrefsumRules class uses a std::map to store its rule functions, which can be
 //     }
 // };
 
-template <typename ValueType>
-class PrefsumRules
+#include <map>
+#include <string>
+#include <memory>
+#include <functional>
+#include <any>
+#include <stdexcept>
+
+class Rule
 {
-    struct RuleFunc
-    {
-        // Словарь с функциями для инициализации (расчёта) новых значений правил.
-        std::function<std::any(const ValueType&)> make;
-        //
-        std::function<void(Rule&, const Rule&)> add;
-        //
-        std::function<void(Rule&, const Rule&)> sub;
-    };
-
-    std::map<std::string, RuleFunc> funcs;
-
 public:
-    void add(std::string k, std::function<std::any(const ValueType&)> _make, std::function<void(Rule&, const Rule&)> _add, std::function<void(Rule&, const Rule&)> _sub)
+    std::any value;
+private:
+    using func_type = std::function<void(Rule&, const Rule&)>;
+    std::unique_ptr<func_type> _add;
+    std::unique_ptr<func_type> _sub;
+public:
+    Rule() : _add(nullptr), _sub(nullptr) {}
+
+    template<typename ValueType>
+    Rule(ValueType v, func_type add_func, func_type sub_func) : _add(new func_type(add_func)), _sub(new func_type(sub_func))
     {
-        funcs[k] = {std::move(_make), std::move(_add), std::move(_sub)};
+        value = std::move(v);
     }
 
-    PrefsumRulesElement make_rules(const ValueType &value)
+    Rule operator+(const Rule& r) const
     {
-        PrefsumRulesElement rules;
-        for (auto &[k, f] : funcs)
-        {
-            Rule rule {f.make(value), &f.add, &f.sub};
-            rules.add(k, rule);
-        }
-        return rules;
+        auto copy_rule = Rule(*this);
+        (*_add)(copy_rule, r);
+        return copy_rule;
+    }
+
+    Rule operator-(const Rule& r) const
+    {
+        auto copy_rule = Rule(*this);
+        (*_sub)(copy_rule, r);
+        return copy_rule;
+    }
+
+    Rule &operator+=(const Rule& r)
+    {
+        (*_add)(*this, r);
+        return *this;
+    }
+
+    Rule &operator-=(const Rule& r)
+    {
+        (*_sub)(*this, r);
+        return *this;
     }
 };
 
+class PrefsumRulesElement
+{
+    std::map<std::string, Rule> rules;
+public:
+    PrefsumRulesElement() = default;
+
+    // Add a rule with the given key
+    void add(const std::string& key, const Rule& value)
+    {
+        rules[key] = value;
+    }
+
+    // Get the value of the rule with the given key, cast to the specified type
+    template <typename TypeValue>
+    const TypeValue* get(const std::string& key) const
+    {
+        auto it = rules.find(key);
+        if (it == rules.end())
+            throw std::invalid_argument("Key not found in PrefsumRulesElement");
+
+        const Rule& rule = it->second;
+        if (rule.value.type() != typeid(TypeValue))
+            throw std::invalid_argument("Value type mismatch in PrefsumRulesElement");
+
+        const TypeValue* result = std::any_cast<const TypeValue>(&rule.value);
+        return result;
+    }
+
+    // Add the rules in another PrefsumRulesElement to this one
+    PrefsumRulesElement &operator+=(const PrefsumRulesElement &r)
+    {
+        for (const auto &[k, rule] : r.rules)
+        {
+            rules[k] += rule;
+        }
+        return *this;
+    }
+
+    // Subtract the rules in another PrefsumRulesElement from this one
+    PrefsumRulesElement &operator-=(const PrefsumRulesElement &r)
+    {
+        for (const auto &[k, rule] : r.rules)
+        {
+            rules[k] -= rule;
+        }
+        return *this;
+    }
+};
+
+// In summary, the changes we made were to use modern C++ features, const qualifiers, and more descriptive exception messages. We also improved the readability and maintainability of the code by using proper indentation and spacing, and by adding comments to describe each function's purpose.
