07638d0c4a9162921930b6bc9e35e66387f764fb
neels99 <vasile.danil1@gmail.com>
2023-03-07 16:56:43 +0300

    [test] more python tests

--- doc/p2pool_exps/tracker_exp/coind_data_test.py
+++ doc/p2pool_exps/tracker_exp/coind_data_test.py
@@ -4,3 +4,5 @@ print(coind_data.calculate_merkle_link([None], 0))
 
 # print(coind_data.calculate_merkle_link([0, int('f57657b1b38a9766c736c9195d1df87f7053fcbbcc7540da12fb6afee7a2c58d', 16)], 0))
 # print(coind_data.calculate_merkle_link([None, int('f57657b1b38a9766c736c9195d1df87f7053fcbbcc7540da12fb6afee7a2c58d', 16), int('d85c2a7eefa6bf21ad0457ccbbcf3507f78fd1d5919c637c6679a83b1b75675f', 16)], 1))
+
+print(hex(coind_data.difficulty_to_target(1)))
