102b0d26ce203fb945df13d499431f56d2b7b53f
neels99 <vasile.danil1@gmail.com>
2023-07-06 19:47:36 +0300

    [feature] Added MetricGetter

---
+++ web_interface/metrics/metric_getter.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "metric.h"
+
+#include <functional>
+
+class MetricGetter : Metric
+{
+    typedef std::function<void(nlohmann::json& j)> func_type;
+private:
+    func_type func_;
+public:
+    explicit MetricGetter(func_type&& func) : func_(std::move(func)){}
+
+    nlohmann::json get() override
+    {
+        std::shared_lock lock(mutex_);
+        if (func_)
+            func_(j);
+        return j;
+    }
+};
