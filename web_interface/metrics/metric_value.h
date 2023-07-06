2db2719888e778cdda0f19725d9e31ce31b61c4f
neels99 <vasile.danil1@gmail.com>
2023-07-06 22:12:27 +0300

    [feature] Update for MetricValue/MetricValues

--- web_interface/metrics/metric_value.h
+++ web_interface/metrics/metric_value.h
@@ -25,3 +25,28 @@ public:
         j = value;
     }
 };
+
+class MetricValues : public Metric
+{
+public:
+    MetricValues() = default;
+
+    template <typename T>
+    explicit MetricValues(const std::string& field_name, const T& value)
+    {
+        set(field_name, value);
+    }
+
+    template <typename T>
+    explicit MetricValues(const std::string& field_name, T&& value)
+    {
+        set(field_name, value);
+    }
+
+    template<typename T>
+    void set(const std::string& field_name, T value)
+    {
+        std::unique_lock lock(mutex_);
+        j[field_name] = value;
+    }
+};
