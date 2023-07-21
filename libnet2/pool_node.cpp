b3517a31de9ee034755df496beb33ea6373897cc
neels99 <vasile.danil1@gmail.com>
2023-07-21 16:09:57 +0300

    [feature] Removed TODO from current_payouts_metric

--- libnet2/pool_node.cpp
+++ libnet2/pool_node.cpp
@@ -745,8 +745,7 @@ void PoolNode::init_web_metrics()
     current_payouts_metric = net->web->add<current_payouts_metric_type>("current_payouts", [&](nlohmann::json &j){
         for (const auto &[script, value] : tracker->get_expected_payouts(best_share->value(), FloatingInteger(coind_node->coind_work->value().bits).target(), coind_node->coind_work->value().subsidy))
         {
-            //TODO: script = bitcoin_data.script2_to_address(script, node.net.PARENT)?
-            j[script] = value/1e8;
+            j[coind::data::script2_to_address(PackStream{script}, net)] = (value/1e8).getdouble();
         }
     });
 //    stale_counts_metric = net->web->add<stale_counts_metric_type>("stale_counts");
