e01956ce110ab2cf47518d5656315cbf62f9b97c
neels99 <vasile.danil1@gmail.com>
2023-12-28 09:10:53 +0300

    [feature] reconnect/disconnect system upd#5 -- libp2p

--- libp2p/node.h
+++ libp2p/node.h
@@ -10,24 +10,17 @@
 class Listener
 {
 public:
-	Listener()
-	{
+	Listener() = default;
 
-	}
-
-	virtual void operator()(std::function<void(std::shared_ptr<Socket>)> socket_handle, std::function<void()> finish) = 0;
+	virtual void tick(std::function<void(std::shared_ptr<Socket>)> socket_handle, std::function<void()> finish) = 0;
 };
 
 class Connector
 {
 public:
-	Connector()
-	{
+	Connector() = default;
 
-	}
-
-	virtual void operator()(std::function<void(std::shared_ptr<Socket>)> socket_handle,
-                            NetAddress _addr) = 0;
+	virtual void tick(std::function<void(std::shared_ptr<Socket>)> socket_handle, NetAddress _addr) = 0;
 };
 
 enum NodeRunState
