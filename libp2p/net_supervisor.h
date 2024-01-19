1542c42bb88ca57c9848daa18b3cfeb9f7f883aa
Neels99 <vasile.danil1@gmail.com>
2024-01-19 05:29:44 +0300

    [feature] Updated net_supervisor

--- libp2p/net_supervisor.h
+++ libp2p/net_supervisor.h
@@ -1,59 +1,145 @@
 #pragma once
 #include <map>
 #include <set>
+#include <queue>
 
 #include <libdevcore/logger.h>
 
 enum net_state
 {
     disconnected,
+    reconnecting,
     connected
 };
 
 class SupervisorElement
 {
-    net_state state;
+    typedef std::function<void(SupervisorElement*)> finish_type;
+    typedef std::function<void(SupervisorElement*, std::string)> restart_type;
+private:
+    // functor, called when finished reconnecting [NetSupervisor::finish_element]
+    finish_type finish_reconnecting;
+    // functor, called when need stop and reconnecting all network [NetSupervisor::restart]
+    restart_type restart_network;
+protected:
+    net_state state {net_state::disconnected};
+    uint8_t layer{};
 public:
-    const uint8_t layer;
+    SupervisorElement() = default;
 
-    SupervisorElement(uint8_t l) : layer(l) {}
+    // finished reconnecting
+    void reconnected()
+    {
+        finish_reconnecting(this);
+    }
+    
+    // stop and reconnect all network
+    void restart(const std::string& reason)
+    {
+        restart_network(this, reason);
+    }
 
     net_state get_state() const
     {
         return state;
     }
-    
+
+    uint8_t get_layer() const
+    {
+        return layer;
+    }
+
+    void set_state(net_state new_state)
+    {
+        state = new_state;
+    }
 public:
-    virtual void start() = 0;
+    // call once when initialize element
+    void init(uint8_t l, finish_type&& finish_func, restart_type&& restart_network_func) 
+    {
+        layer = l;
+        finish_reconnecting = finish_func;
+        restart_network = restart_network_func;
+    }
+
+    // called for disconnect
     virtual void stop() = 0;
-    // virtual void restart() = 0;
+    // 
     virtual void reconnect() = 0;
 };
 
 class NetSupervisor
 {
 protected:
-    std::map<uint8_t, std::set<SupervisorElement*>> layers; // Network layers [0; 2^8]
-public:
-    void add(SupervisorElement* element)
+    // Network layers [0; 2^8]
+    std::map<uint8_t, std::set<SupervisorElement*>> layers; 
+    // Container from which service reconnection occurs in the order of queue
+    std::queue<SupervisorElement*> reconnect_queue;
+    // Current element for reconnect
+    SupervisorElement* current;
+protected:
+    // iterate reconnect_queue for reconnect();
+    inline void next_queue()
     {
-        if (!layers[element->layer].count(element))
-            layers[element->layer].insert(element);
+        // reconnect queue empty
+        if (!reconnect_queue.size())
+            return;
+
+        current = reconnect_queue.front();
+        reconnect_queue.pop();
+
+        current->reconnect();
+    }
+public:
+    void add(SupervisorElement* element, uint8_t layer)
+    {
+        if (!layers[element->get_layer()].count(element))
+        {
+            layers[element->get_layer()].insert(element);
+            element->init(
+                layer, 
+                [this](SupervisorElement* element){ finish_element(element); },
+                [this](SupervisorElement* element, const std::string& reason){ restart(element, reason); }
+            );
+        }
         else
-            LOG_WARNING << "Element is already in " << element->layer << " layer.";
+        {
+            LOG_WARNING << "Element is already in " << element->get_layer() << " layer.";
+        }
     }
 
-    void restart(SupervisorElement* element)
+    // Called when finished reconnect for element
+    void finish_element(SupervisorElement* element)
     {
-        // Stop all supervisor elements upper
-        auto layer_it = layers.find(element->layer);
+        if (!element || element != current)
+        {
+            throw std::invalid_argument("NetSupervisor::finish_element called for incorrect element");
+        }
+        
+        element->set_state(connected);
+        next_queue();
+    }
+
+    void restart(SupervisorElement* element, const std::string& reason)
+    {
+        //TODO: REASON
+        std::queue<SupervisorElement*> new_reconnect_queue;
+
+        // Stop all supervisor elements upper.
+        auto layer_it = layers.find(element->get_layer());
         while (layer_it != layers.end())
         {
             for (auto& el : layer_it->second)
+            {
+                new_reconnect_queue.push(el);
                 el->stop();
+            }
             layer_it++;
         }
 
-        
+        // Start reconnect again
+        std::swap(reconnect_queue, new_reconnect_queue);
+
+        next_queue();
     }
 };
