2dfeeb1746904d0b35749a75fc78545de0b0603c
Neels99 <vasile.danil1@gmail.com>
2024-01-30 08:59:28 +0300

    [refactor] Updated Exception system

--- libdevcore/exceptions.h
+++ libdevcore/exceptions.h
@@ -2,76 +2,36 @@
 #include <string>
 #include <stdexcept>
 
-enum ExceptionCode
-{
-    node_errc,
-    net_errc
-};
-
-struct c2pool_except_data
-{
-   const ExceptionCode code;
-    
-    c2pool_except_data(const ExceptionCode& _code) : code(_code)       {
-
-   }
-};
-
-struct node_exception : public c2pool_except_data
-{
-    node_exception() : c2pool_except_data(node_errc)
-    {
-
-    }
-};
-
-struct net_exception : public c2pool_except_data
-{
-    std::string ip;
-
-    net_exception(std::string _ip) : c2pool_except_data(net_errc), ip(_ip)
-    {
-
-    }
-};
-
 namespace c2pool
 {
-    class NodeExceptionHandler 
+    enum ExceptionCode
     {
-    public:
-        void HandleException(const c2pool_except_data* data)
-        {
-            switch (data->code)
-            {
-            case node_errc:
-                HandleNodeException();
-                break;
-            case net_errc:
-                HandleNetException((net_exception*)data);
-                break;
-            default:
-                break;
-            }
-        }
+        node_errc,
+        net_errc
+    };
 
-    protected:
-        virtual void HandleNodeException() = 0;
-        virtual void HandleNetException(net_exception* data) = 0;
+    struct C2PoolExceptData
+    {
+       const ExceptionCode code;
+
+        C2PoolExceptData(const ExceptionCode& _code) : code(_code)
+        {
+
+        }
     };
 
     class base_c2pool_exception : public std::runtime_error
     {
-        c2pool_except_data* data;
+        C2PoolExceptData* data;
     public:
-        base_c2pool_exception(std::string reason, c2pool_except_data* _data) : std::runtime_error(reason), data(_data) {}
+        base_c2pool_exception(std::string reason, C2PoolExceptData* _data) : std::runtime_error(reason), data(_data) {}
 
         ExceptionCode where() const
         {
             return data->code;
         }
 
-        c2pool_except_data* get_data() const
+        C2PoolExceptData* get_data() const
         { 
             return data; 
         }
@@ -82,28 +42,68 @@ namespace c2pool
                 delete data;
         }
     };
-
-    class CoindExcept : public base_c2pool_exception
-    {
-    public:
-        CoindExcept(std::string reason, c2pool_except_data* data) : base_c2pool_exception(reason, data)
-        {
-
-        }
-    };
-
-    class PoolExcept : public base_c2pool_exception
-    {
-    public:
-        PoolExcept(std::string reason, c2pool_except_data* data) : base_c2pool_exception(reason, data)
-        {
-
-        }
-    };
 }
+
+struct NodeExcept : public c2pool::C2PoolExceptData
+{
+    NodeExcept() : c2pool::C2PoolExceptData(c2pool::node_errc)
+    {
+
+    }
+};
+
+struct NetExcept : public c2pool::C2PoolExceptData
+{
+    std::string ip;
+
+    NetExcept(std::string _ip) : c2pool::C2PoolExceptData(c2pool::net_errc), ip(_ip)
+    {
+
+    }
+};
+class coind_exception : public c2pool::base_c2pool_exception
+{
+public:
+    coind_exception(std::string reason, c2pool::C2PoolExceptData* data) : c2pool::base_c2pool_exception(reason, data)
+    {
+
+    }
+};
+
+class pool_exception : public c2pool::base_c2pool_exception
+{
+public:
+    pool_exception(std::string reason, c2pool::C2PoolExceptData* data) : c2pool::base_c2pool_exception(reason, data)
+    {
+
+    }
+};
 
 template <typename ExceptT, typename DataT, typename... Args>
 ExceptT make_except(std::string reason, Args... args)
 {
     return ExceptT(reason, new DataT(args...));
 }
+
+class NodeExceptionHandler 
+{
+public:
+    void HandleException(const c2pool::C2PoolExceptData* data)
+    {
+        switch (data->code)
+        {
+        case c2pool::node_errc:
+            HandleNodeException();
+            break;
+        case c2pool::net_errc:
+            HandleNetException((NetExcept*)data);
+            break;
+        default:
+            break;
+        }
+    }
+
+protected:
+    virtual void HandleNodeException() = 0;
+    virtual void HandleNetException(NetExcept* data) = 0;
+};
