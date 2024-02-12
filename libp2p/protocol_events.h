427b9f4e1d487e7cc2a10326a925d831294f78a3
Neels99 <vasile.danil1@gmail.com>
2024-02-12 21:05:27 +0300

    [feature] Rework protocol events; Pinger Component

--- libp2p/protocol_events.h
+++ libp2p/protocol_events.h
@@ -2,12 +2,11 @@
 
 #include <functional>
 #include <libdevcore/events.h>
+#include <libdevcore/timer.h>
 #include <boost/asio.hpp>
-//TODO: удалением объекта протокола (дисконнект) -- отключать все ивенты.
 
 class ProtocolEvents
 {
-    bool stopped = false;
 public:
     Event<> event_handle_message;   // Вызывается, когда мы получаем любое сообщение.
 
@@ -20,78 +19,48 @@ public:
     {
         delete event_handle_message;
     }
-
-    void stop() { stopped = true; }
-    bool is_stopped() const { return stopped;}
-
 };
 
-class ProtocolPinger : virtual ProtocolEvents
+class Pinger
 {
 private:
-    time_t outtime_ping_time;
-    boost::asio::steady_timer timer_outtime;
-    std::function<void()> outtimeF;
+    ProtocolEvents* events;
 
-	std::function<time_t()> send_ping_time;
-	boost::asio::steady_timer timer_send_ping;
-	std::function<void()> send_pingF;
+    time_t frequency_t; // частота отправки сообщения ping
+    time_t timeout_t;   // время, через которое pinger посчитает, что peer отключён.
 
-    void start_outtime_ping()
-    {
-		timer_outtime.expires_from_now(boost::asio::chrono::seconds(outtime_ping_time));
-		timer_outtime.async_wait([&](const boost::system::error_code &ec)
-                         {
-                             if (ec)
-                             {
-                                 if (ec == boost::system::errc::operation_canceled)
-                                 {
-									 // Если таймер был canceled, то он запускается заново без вызова outtimeF()
-                                     if (!is_stopped())
-                                        start_outtime_ping();
-                                 } else
-                                 {
-                                     //TODO: error
-                                 }
-                             } else
-                             {
-                                 outtimeF();
-                             }
-                         });
-    }
-
-	void start_send_ping()
-	{
-		timer_send_ping.expires_from_now(boost::asio::chrono::seconds(send_ping_time()));
-		timer_send_ping.async_wait([&](const boost::system::error_code &ec){
-			if (!ec)
-			{
-				send_pingF();
-				start_send_ping();
-			} else
-			{
-//                if (ec != boost::system::errc::operation_canceled && is_stopped())
-                    // TODO: error
-			}
-		});
-	}
-
-    void restart_ping()
-    {
-		timer_outtime.cancel();
-    }
+    c2pool::Timer timer_timeout;
+    c2pool::Timer timer_ping;
 
 public:
-
-    ProtocolPinger(boost::asio::io_context* _context, time_t outtime_t, std::function<void()> outtime_f, std::function<time_t()> send_ping_t, std::function<void()> send_ping_f)
-            : outtime_ping_time(outtime_t), timer_outtime(*_context), outtimeF(std::move(outtime_f)),
-			  send_ping_time(std::move(send_ping_t)), timer_send_ping(*_context), send_pingF(std::move(send_ping_f))
+    Pinger(boost::asio::io_context* context, auto frequency_, auto timeout_)
+        : timer_timeout(*context), timer_ping(*context), frequency_t(frequency_), timeout_t(timeout_)
     {
-        start_outtime_ping();
-        event_handle_message->subscribe([this] { restart_ping(); });
+        timer_timeout.start(
+            timeout_t,
+            [&]()
+            {
+                timeout();
+            }
+        );
 
-		start_send_ping();
+        timer_ping.start(
+            frequency_t,
+            [&]()
+            {
+                send_ping();
+            }
+        );
+
+        event_handle_message->subscribe(
+            [this]()
+            { 
+                timer_timeout->restart();
+                timer_ping->restart();
+            }
+        );
     }
 
-    ~ProtocolPinger() = default;
+    virtual void timeout() = 0;
+    virtual void send_ping() = 0;
 };
