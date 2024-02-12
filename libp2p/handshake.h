3f6fdb1a78f1f05abec49c3556fc6c147bf07524
Neels99 <vasile.danil1@gmail.com>
2024-02-12 21:05:58 +0300

    [feature] Update handshake

--- libp2p/handshake.h
+++ libp2p/handshake.h
@@ -6,15 +6,17 @@
 #include <libp2p/protocol_events.h>
 
 #include "socket.h"
-#include "protocol.h"
 #include "message.h"
 
-template <typename ProtocolType>
+template <typename SocketType>
 class Handshake : public virtual ProtocolEvents
 {
 protected:
-	Socket* socket;
+	typedef SocketType socket_type;
+	SocketType* socket;
 public:
+	Event<> event_handle_message; // Вызывается, когда мы получаем любое сообщение.
+
 	Handshake(auto socket_) : socket(socket_)
 	{
 		socket->set_message_handler
@@ -28,18 +30,14 @@ public:
 
     ~Handshake() = default;
 
-	auto get_socket() const
-	{
-		return socket;
-	}
-
-    auto get_addr() { return socket->get_addr(); }
+	auto get_socket() const { return socket; }
+    auto get_addr() const { return socket->get_addr(); }
 
 	virtual void handle_message(std::shared_ptr<RawMessage> raw_msg) = 0;
 
     virtual void close()
     {
-        socket->disconnect();
+        socket->close();
 		delete socket;
     }
 };
