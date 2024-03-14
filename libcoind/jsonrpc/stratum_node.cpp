4d4fdf60feed53a5af8853f00690eaf5343c3778
Neels99 <vasile.danil1@gmail.com>
2024-03-14 10:20:03 +0300

    [feature] Big update for StratumNode

--- libcoind/jsonrpc/stratum_node.cpp
+++ libcoind/jsonrpc/stratum_node.cpp
@@ -2,82 +2,82 @@
 
 StratumNode::StratumNode(boost::asio::io_context* context, Worker* worker) : _context(context), acceptor(*_context), _worker(worker)
 {
-    ip::tcp::endpoint listen_ep(ip::tcp::v4(), 1131);
-
-    acceptor.open(listen_ep.protocol());
-    acceptor.set_option(io::socket_base::reuse_address(true));
-    acceptor.bind(listen_ep);
-    acceptor.listen();
 }
 
+void StratumNode::miner_processing(ip::tcp::socket& _socket)
+{
+    auto addr = NetAddress(_socket.remote_endpoint());
+    LOG_INFO << "Stratum connected from: " << addr.to_string();
+
+    if (bans.find(addr.ip) != bans.end())
+    {
+        LOG_TRACE << "Miner banned!";
+        _socket.close();
+        return;
+    }
+
+    if (miners.find(addr) != miners.end())
+    {
+        LOG_WARNING << addr.to_string() << " already connected!";
+        _socket.close();
+        ban(addr);
+        return;
+    }
+
+    auto socket = std::make_shared<ip::tcp::socket>(std::move(_socket));
+    auto stratum = new Stratum(_context, std::move(socket), _worker,
+                                             [&](const addr_t &addr_)
+                                             {
+                                                 disconnect(addr_);
+                                             });
+    miners[addr] = stratum;
+}
 
 void StratumNode::listen()
 {
-    acceptor.async_accept([this](const boost::system::error_code &ec, ip::tcp::socket _socket)
-                          {
-                              if (!ec)
-                              {
-                                  auto _addr = std::make_tuple(_socket.remote_endpoint().address().to_string(),
-                                                               _socket.remote_endpoint().port());
-                                  LOG_INFO << "Stratum connected from: " << std::get<0>(_addr) << ":"
-                                           << std::get<1>(_addr);
-
-                                  if (bans.find(std::get<0>(_addr)) != bans.end())
-                                  {
-                                      LOG_TRACE << "BANNED!";
-                                      _socket.close();
-                                      listen();
-                                      return;
-                                  }
-
-                                  if (miners.find(_addr) != miners.end())
-                                  {
-                                      LOG_WARNING << std::get<0>(_addr) << ":" << std::get<1>(_addr)
-                                                  << " already connected!";
-                                      _socket.close();
-                                      ban(_addr);
-                                      listen();
-                                      return;
-                                  }
-
-                                  auto socket = std::make_shared<ip::tcp::socket>(std::move(_socket));
-                                  auto stratum = new Stratum(_context, std::move(socket), _worker,
-                                                                           [&](const addr_t &addr)
-                                                                           {
-                                                                               disconnect(addr);
-                                                                           });
-                                  miners[_addr] = stratum;
-                                  listen();
-                              } else
-                              {
-                                  LOG_ERROR << "Stratum node listen: " << ec << " " << ec.message() << std::endl;
-                              }
-
-                          });
+    acceptor.async_accept(
+        [this](const boost::system::error_code &ec, ip::tcp::socket socket)
+        {
+            if (ec)
+            {
+                if (ec == boost::system::errc::operation_canceled)
+                    return;
+                else
+                    throw libp2p::node_exception("Stratum node listen error: " + ec.message(), this);
+            }
+            miner_processing(socket);
+            listen();
+        }
+    );
 }
 
-void StratumNode::ban(const StratumNode::addr_t& _addr)
+void StratumNode::ban(const StratumNode::addr_t& addr)
 {
-    auto _ban_timer = std::make_shared<boost::asio::steady_timer>(*_context);
-    _ban_timer->expires_from_now(std::chrono::seconds(10));
-    _ban_timer->async_wait([&, addr = _addr](const auto& ec){
-        bans.erase(std::get<0>(addr));
-    });
-    bans[std::get<0>(_addr)] = std::move(_ban_timer);
-    LOG_INFO << std::get<0>(_addr) << " banned for StratumNode!";
+    auto ban_timer = std::make_unique<c2pool::Timer>(_context);
+    ban_timer->start(
+        10, // 10 second ban
+        [&, addr = addr]()
+        {
+            bans.erase(addr.ip);
+        }
+    );
+
+    bans[addr.ip] = std::move(ban_timer);
+    LOG_INFO << addr.ip << " banned in StratumNode!";
 }
 
-void StratumNode::disconnect(const addr_t& addr)
+void StratumNode::disconnect(const addr_t& addr, bool is_ban)
 {
     if (miners.find(addr) != miners.end())
     {
         auto stratum = miners[addr];
         miners.erase(addr);
-        ban(addr);
+        if (is_ban)
+            ban(addr);
         delete stratum;
     } else
     {
-        throw std::invalid_argument("StratumNode::disconnect received wrong addr");
+        throw libp2p::node_exception("StratumNode::disconnect received wrong addr = " + addr.to_string(), this);;
     }
 }
 
--- libcoind/jsonrpc/stratum_node.h
+++ libcoind/jsonrpc/stratum_node.h
@@ -3,11 +3,16 @@
 #include <memory>
 
 #include "stratum.h"
+#include <libdevcore/timer.h>
+#include <libdevcore/types.h>
+#include <libp2p/net_errors.h>
+#include <libp2p/network_tree_node.h>
+
 #include <boost/asio.hpp>
 
-class StratumNode
+class StratumNode : public NetworkTreeNode
 {
-    typedef std::tuple<std::string, unsigned short> addr_t;
+    typedef NetAddress addr_t;
     boost::asio::io_context* _context;
 
     ip::tcp::acceptor acceptor;
@@ -16,12 +21,44 @@ class StratumNode
 
     // connected miners
     std::map<addr_t, Stratum*> miners;
-    std::map<std::string, std::shared_ptr<boost::asio::steady_timer>> bans;
+    std::map<std::string, std::unique_ptr<c2pool::Timer>> bans;
 public:
     StratumNode(boost::asio::io_context* context, Worker* worker);
 
+    void run() override
+    {
+        // init + listen boost::asio::acceptor
+        ip::tcp::endpoint listen_ep(ip::tcp::v4(), 1131);
+
+        acceptor.open(listen_ep.protocol());
+        acceptor.set_option(io::socket_base::reuse_address(true));
+        acceptor.bind(listen_ep);
+        acceptor.listen();
+
+        // start processing stratum connections
+        listen();
+    }
+
+    void stop() override
+    {
+        // stop boost::asio::acceptor
+        acceptor.cancel();
+
+        // disconnect all miners
+        for (const auto& [addr_, stratum_] : miners)
+        {
+            disconnect(addr_, false);
+        }
+
+        // discard all bans
+        for (auto& [addr_, timer_] : bans)
+        {
+            timer_->happened();
+        }
+    }
+private:
     void listen();
     void ban(const addr_t& _addr);
-private:
-    void disconnect(const addr_t& addr);
+    void disconnect(const addr_t& addr, bool is_ban = true);
+    void miner_processing(ip::tcp::socket& _socket);
 };
