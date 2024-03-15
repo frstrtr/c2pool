73f602fe7e5f052a0c268fdf2ac978631d57a868
Neels99 <vasile.danil1@gmail.com>
2024-03-15 09:11:18 +0300

    [feature] Added deconstructor in expiring_dict

--- libdevcore/expiring_dict.h
+++ libdevcore/expiring_dict.h
@@ -13,15 +13,30 @@ private:
     boost::asio::io_context* context;
     std::map<Key, std::tuple<Value, std::shared_ptr<boost::asio::deadline_timer>>> values;
 public:
-    expiring_dict(boost::asio::io_context* _context, int32_t expiry_seconds) : context(_context), expiry_time(boost::posix_time::seconds(expiry_seconds)) { }
+    expiring_dict(boost::asio::io_context* _context, int32_t expiry_seconds) 
+        : context(_context), expiry_time(boost::posix_time::seconds(expiry_seconds)) 
+    {
+    }
+
+    ~expiring_dict()
+    {
+        for (auto& [key, value_] : values)
+        {
+            auto& [value, timer] = value_;
+            timer->cancel();
+        }
+    }
 
     void add(Key key, Value value)
     {
         auto _timer = std::make_shared<boost::asio::deadline_timer>(*context);
         _timer->expires_from_now(expiry_time);
-        _timer->async_wait([&, _key = key](const boost::system::error_code &ec) {
-            values.erase(_key);
-        });
+        _timer->async_wait(
+            [&, _key = key](const boost::system::error_code &ec) 
+            {
+                values.erase(_key);
+            }
+        );
 
         values[key] = {value, _timer};
     }
