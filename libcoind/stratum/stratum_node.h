e4be3cd8ea3913b94da6df5a5fedab3105d912be
Sleen <vasile.danil1@gmail.com>
2024-04-29 21:58:31 +0300

    [feature] stratum updated

--- libcoind/stratum/stratum_node.h
+++ libcoind/stratum/stratum_node.h
@@ -6,11 +6,11 @@
 #include <libdevcore/timer.h>
 #include <libdevcore/types.h>
 #include <libp2p/net_errors.h>
-#include <libp2p/network_tree_node.h>
+#include <libp2p/workflow_node.h>
 
 #include <boost/asio.hpp>
 
-class StratumNode : public NetworkTreeNode
+class StratumNode : public WorkflowNode
 {
     typedef NetAddress addr_t;
     boost::asio::io_context* _context;
@@ -22,53 +22,70 @@ class StratumNode : public NetworkTreeNode
     // connected miners
     std::map<addr_t, Stratum*> miners;
     std::map<std::string, std::unique_ptr<c2pool::Timer>> bans;
-public:
-    StratumNode(boost::asio::io_context* context, Worker* worker);
 
-    void run() override
+protected:
+    void run_node() override
     {
-        LOG_INFO << "StratumNode running...";
-        // init + listen boost::asio::acceptor
-        ip::tcp::endpoint listen_ep(ip::tcp::v4(), 1131);
+        boost::asio::dispatch(*_context,
+            [&, PROCESS_DUPLICATE]
+            {
+                LOG_INFO << "StratumNode running...";
+                // init + listen boost::asio::acceptor
+                ip::tcp::endpoint listen_ep(ip::tcp::v4(), 1131);
 
-        acceptor.open(listen_ep.protocol());
-        acceptor.set_option(io::socket_base::reuse_address(true));
-        acceptor.bind(listen_ep);
-        acceptor.listen();
+                acceptor.open(listen_ep.protocol());
+                acceptor.set_option(io::socket_base::reuse_address(true));
+                acceptor.bind(listen_ep);
+                acceptor.listen();
 
-        // start processing stratum connections
-        listen();
-        connected();
-        LOG_INFO << "...StratumNode connected!";
+                // start processing stratum connections
+                listen();
+                connected();
+                LOG_INFO << "...StratumNode connected!";
+            }
+        );
     }
 
-    void stop() override
+    void stop_node() override
     {
-        LOG_INFO << "StratumNode stopping...!";
-        // stop boost::asio::acceptor
-        acceptor.cancel();
-        acceptor.close();
+        boost::asio::dispatch(*_context,
+            [&, PROCESS_DUPLICATE]
+            {
+                LOG_INFO << "StratumNode stopping...!";
+                // stop boost::asio::acceptor
+                acceptor.cancel();
+                acceptor.close();
 
-        // disconnect all miners
-        std::vector<addr_t> disconnect_addrs;
-        for (const auto& [addr_, stratum_] : miners)
-        {
-            disconnect_addrs.push_back(addr_);
-        }
+                // disconnect all miners
+                std::vector<addr_t> disconnect_addrs;
+                for (const auto& [addr_, stratum_] : miners)
+                {
+                    disconnect_addrs.push_back(addr_);
+                }
 
-        for (const auto& addr_ : disconnect_addrs)
-        {
-            disconnect(addr_, "stop node");
-        }
+                for (const auto& addr_ : disconnect_addrs)
+                {
+                    disconnect(addr_, "stop node");
+                }
 
-        // discard all bans
-        for (auto& [addr_, timer_] : bans)
-        {
-            timer_->stop();
-        }
-        bans.clear();
+                // discard all bans
+                for (auto& [addr_, timer_] : bans)
+                {
+                    timer_->stop();
+                }
+                bans.clear();
+            }
+        );
+    }
+    
+    void disconnect_notify() override
+    {
         LOG_INFO << "...StratumNode stopped!";
     }
+
+public:
+    StratumNode(boost::asio::io_context* context, Worker* worker);
+    
 private:
     void listen();
     void ban(const addr_t& _addr, int sec);
