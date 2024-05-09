de3ef53e6638a5da4ee12102e3cf1b6d0c59ce65
Sleen <vasile.danil1@gmail.com>
2024-05-09 05:27:45 +0300

    [feature] c2pool.cpp update; CMake update

--- src/c2pool/c2pool.cpp
+++ src/c2pool/c2pool.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
-#include <core/a.hpp>
-// #include <core/a.hpp>
-#include <nlohmann/json.hpp>
+
+#include <core/filesystem.hpp>
+#include <core/log.hpp>
+
 #include <btclibs/uint256.h>
+#include <nlohmann/json.hpp>
+
 
 int main(int argc, char *argv[])
 {
-    std::cout << "HI: " << a::f() << std::endl;
+    c2pool::log::Logger::init();
+    c2pool::log::Logger::add_category("all");
+
     std::cout << nlohmann::json::meta() << std::endl;
     std::cout << uint256S("ff").GetHex() << std::endl;
+    std::cout << c2pool::filesystem::config_path() << std::endl;
+    std::cout << c2pool::filesystem::current_path() << std::endl;
+     
+    std::cout << c2pool::log::Logger::check_category(c2pool::log::COIND_RPC) << std::endl;
+    BOOST_LOG_TRIVIAL(info) << "hi";
+    LOG_DEBUG_POOL << "DEBUG DATA";
 }
