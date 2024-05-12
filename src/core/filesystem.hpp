59cbba99496777f6d0c28c9d72f89ab8d610744e
5133n <vasile.danil1@gmail.com>
2024-05-12 18:03:37 +0300

    [fix] Windows build fix

--- src/core/filesystem.hpp
+++ src/core/filesystem.hpp
@@ -12,7 +12,7 @@ namespace filesystem
 inline std::filesystem::path config_path()
 {
     #ifdef _WIN32
-        return std::filesystem::path(std::getenv("APPDATA")) + "/c2pool";
+        return std::filesystem::path(std::getenv("APPDATA")) / "c2pool";
     #elif __linux__
         return std::filesystem::path(std::getenv("HOME")) / ".c2pool";
     #else
