0739e4c4ac98bbde406f40e582b1194a41c23564
5133n <vasile.danil1@gmail.com>
2024-05-13 22:58:33 +0300

    [feature] Moved from legacy, debug_timestamp

---
+++ src/core/common.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <ctime>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+
+namespace c2pool
+{
+
+struct debug_timestamp
+{
+    std::chrono::duration<double> t;
+
+    debug_timestamp() : t(std::chrono::high_resolution_clock::now().time_since_epoch()) { }
+    debug_timestamp(std::chrono::duration<double> _t) : t(_t) {}
+
+    debug_timestamp operator-(const debug_timestamp& v) const
+    {
+        return {t - v.t};
+    }
+
+    debug_timestamp operator+(const debug_timestamp& v) const
+    {
+        return {t + v.t};
+    }
+
+    friend std::ostream& operator<<(std::ostream &stream, const debug_timestamp& v)
+    {
+        stream << std::fixed << std::setprecision(10) << v.t.count() << "s";
+        stream.unsetf(std::ios_base::fixed);
+        return stream;
+    }
+};
+
+}
