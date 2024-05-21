200abbe9c54d09521ecd466dcb1e21188ff5115c
Sleen <vasile.danil1@gmail.com>
2024-05-21 19:23:11 +0300

    [chore] pool_impl/ltc

---
+++ src/pool_impl/ltc/messages.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <pool/message.hpp>
+
+namespace ltc
+{
+    
+struct message_ping : public c2pool::pool::Message
+{
+    message_ping() : c2pool::pool::Message("ping") {}
+    message_ping() : c2pool::pool::Message(std::move(command)) {}
+};
+
+} // namespace ltc
