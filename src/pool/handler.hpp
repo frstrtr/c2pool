7bb53e548987d8ee09a98bbb36dfa07886d20c4a
Sleen <vasile.danil1@gmail.com>
2024-05-21 19:20:44 +0300

    [feature] Updated MessageHandler + macro

--- src/pool/handler.hpp
+++ src/pool/handler.hpp
@@ -1,21 +1,36 @@
 #pragma once
 
+#include <memory>
+
+#include <pool/message.hpp>
+#include <core/pack.hpp>
+
 namespace c2pool
 {
 
 namespace pool
 {
 
-template <typename ProtocolType>
+template <typename PROTOCOL_TYPE>
 class MessageHandler
 {
 protected:
-    using protocol_type = ProtocolType;
+    using protocol_type = PROTOCOL_TYPE;
+    using callback_type = std::function<void(protocol_type*, PackStream&)>;
+
+    template <typename MESSAGE_TYPE>
+    static std::unique_ptr<MESSAGE_TYPE> parse_message(const std::string& cmd, PackStream &stream)
+    {
+        static_assert(std::is_base_of<Message, MESSAGE_TYPE>());
+        auto msg = std::make_unique<MESSAGE_TYPE>(cmd);
+        stream >> *msg;
+        return std::move(msg);
+    }
 
 public:
-    virtual const std::map<std::string, std::function<void(protocol_type*)>>& getFunctionMap() const = 0;
+    virtual const std::map<std::string, callback_type>& getFunctionMap() const = 0;
 
-    void invoke(std::string cmd) const
+    void invoke(std::string cmd, PackStream& msg) const
     {
         protocol_type* p = const_cast<protocol_type*>(static_cast<const protocol_type*>(this));
         
@@ -23,31 +38,35 @@ public:
         try
         {
             auto& func = m.at(cmd);
-            func(p);
+            func(p, msg);
         } catch (const std::out_of_range& ex)
         {
             std::cout << "out range" << std::endl;
         }
     }
-}
+};
 
 } // namespace pool
 
-}
+} // namespace c2pool
 
-#define INIT_MESSAGE_HANDLER_BEGIN                                                                     \
-    static std::map<std::string, std::function<void(protocol_type *)>> &getFunctionMapStatic() \
-    {                                                                                          \
-        static std::map<std::string, std::function<void(protocol_type *)>> functionMap = {
+#define INIT_MESSAGE_HANDLER_BEGIN                                                                      \
+    static std::map<std::string, callback_type> &getFunctionMapStatic()                                 \
+    {                                                                                                   \
+        static std::map<std::string, callback_type> functionMap = {
 
-#define INIT_MESSAGE_HANDLER_FINISH                                                                            \
+#define INIT_MESSAGE_HANDLER_FINISH                                                                    \
         };                                                                                             \
         return functionMap;                                                                            \
     }                                                                                                  \
                                                                                                        \
-    const std::map<std::string, std::function<void(protocol_type *)>> &getFunctionMap() const override \
+    const std::map<std::string, callback_type> &getFunctionMap() const override                        \
     {                                                                                                  \
         return getFunctionMapStatic();                                                                 \
     }
 
-#define MESSAGE_CALLBACK(name) {#name, [&](protocol_type *protocol) { protocol->name(); }},
+#define ADD_MESSAGE_CALLBACK_CUSTOM(cmd, msg_type) {#cmd, [&](protocol_type *protocol, PackStream& stream) { auto msg = parse_message<msg_type>(#cmd, stream); protocol->cmd(std::move(msg)); }},
+#define ADD_MESSAGE_CALLBACK(cmd) ADD_MESSAGE_CALLBACK_CUSTOM(cmd, message_##cmd)
+
+#define MESSAGE_CALLBACK_CUSTOM(cmd, msg_type) void cmd(std::unique_ptr<msg_type> message)
+#define MESSAGE_CALLBACK(cmd) MESSAGE_CALLBACK_CUSTOM(cmd, message_##cmd)
