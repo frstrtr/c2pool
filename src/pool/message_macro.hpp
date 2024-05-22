8baa760bd1c71112fc0d64f1065f3cf475eedbd8
Sleen <vasile.danil1@gmail.com>
2024-05-22 21:55:20 +0300

    [feature] macros added

--- src/pool/message_macro.hpp
+++ src/pool/message_macro.hpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#pragma once
 
 #define MESSAGE_DATA_EXPAND( x ) x
 #define MESSAGE_DATA_GET_MACRO(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, _33, _34, _35, _36, _37, _38, _39, _40, _41, _42, _43, _44, _45, _46, _47, _48, _49, _50, _51, _52, _53, _54, _55, _56, _57, _58, _59, _60, _61, _62, _63, _64, NAME,...) NAME
@@ -88,22 +88,27 @@
 #define MESSAGE_DATA_PASTE19(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18) MESSAGE_DATA_PASTE2(func, v1) MESSAGE_DATA_PASTE18(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18)
 #define MESSAGE_DATA_PASTE20(func, v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19) MESSAGE_DATA_PASTE2(func, v1) MESSAGE_DATA_PASTE19(func, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19)
 
-#define _MESSAGE_DATA_FIELD(first, second) std::cout << first + second << std::endl;
-#define MESSAGE_DATA_FIELD(tup) _MESSAGE_DATA_FIELD tup
+#define _MESSAGE_DATA_FIELD(pack_type, field_name) pack_type::get_type field_name;
+#define MESSAGE_DATA_FIELD(data) _MESSAGE_DATA_FIELD data
+
+#define _MESSAGE_DATA_PACK_FIELD(pack_type, field_name) pack_type field_name;
+#define MESSAGE_DATA_PACK_FIELD(data) _MESSAGE_DATA_PACK_FIELD data
+
+#define BEGIN_MESSAGE(cmd)\
+    struct message_##cmd : public c2pool::pool::Message {\
+        message_##cmd() : c2pool::pool::Message(#cmd) {}\
+        \
+
+        // WRITE
+
+        // READ TYPE
 
 #define MESSAGE_FIELDS(...)\
     MESSAGE_DATA_EXPAND(MESSAGE_DATA_PASTE(MESSAGE_DATA_FIELD, __VA_ARGS__))\
+    \
+    struct packed_type\
+    {\
+        MESSAGE_DATA_EXPAND(MESSAGE_DATA_PASTE(MESSAGE_DATA_PACK_FIELD, __VA_ARGS__))\
+    };
 
-#define test_f(a, b) a + b
-#define test_def(x) test_f x
-
-int main()
-{
-    MESSAGE_FIELDS(
-        (10, 5),
-        (55, 7)
-    );
-
-    // std::cout << test_def((10, 5)) << std::endl;
-
-}
+#define END_MESSAGE() };
