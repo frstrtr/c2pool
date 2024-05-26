fdbf41fcce97e1426c49e474de430f06a7a8c223
Sleen <vasile.danil1@gmail.com>
2024-05-26 22:59:18 +0300

    [concept] new_pack upd

--- temp/new_pack/new_pack.cpp
+++ temp/new_pack/new_pack.cpp
@@ -4,6 +4,7 @@
 #include <memory>
 #include <cstring>
 #include <iomanip>
+#include <string>
 
 struct PackStream
 {
@@ -22,14 +23,14 @@ struct PackStream
         std::cout << std::dec << "}\n";
     }
 
-    void read(std::span<std::byte>& data)
+    void read(const std::span<std::byte>& data)
     {
         memcpy(data.data(), &bytes[0], data.size());
     }
 };
 
 template <typename int_type>
-struct IntType
+struct _IntType
 {
     static void serialize(PackStream& os, int_type& value)
     {
@@ -38,26 +39,35 @@ struct IntType
 
     static void unserialize(PackStream& is, int_type& value)
     {
-        auto _span = std::as_writable_bytes(std::span{&value, 1});
-        std::cout << _span.size() << std::endl;
-        is.read(_span);
-        // value = *reinterpret_cast<const int32_t*>(dataSpan.data());
+
+        is.read(std::as_writable_bytes(std::span{&value, 1}));
     }
 };
 
-struct IntType32
+template <int N>
+struct IntTypeSpecificator;
+
+#define INT_TYPE_SPEC(N, TYPE)\
+    template<> struct IntTypeSpecificator<N> { using type = TYPE; }
+
+INT_TYPE_SPEC(16, int16_t);
+INT_TYPE_SPEC(32, int32_t);
+INT_TYPE_SPEC(64, int64_t);
+
+template <int N>
+using IntType = _IntType<typename IntTypeSpecificator<N>::type>;
+
+struct StrType
 {
-    static void serialize(PackStream& os, int32_t& value)
+    static void serialize(PackStream& os, std::string& value)
     {
         os.write(std::as_bytes(std::span{&value, 1}));
     }
 
-    static void unserialize(PackStream& is, int32_t& value)
+    static void unserialize(PackStream& is, std::string& value)
     {
-        auto _span = std::as_writable_bytes(std::span{&value, 1});
-        std::cout << _span.size() << std::endl;
-        is.read(_span);
-        // value = *reinterpret_cast<const int32_t*>(dataSpan.data());
+
+        is.read(std::as_writable_bytes(std::span{&value, 1}));
     }
 };
 
@@ -66,17 +76,29 @@ int main()
     int32_t old_value = 322;
 
     PackStream stream;
-    IntType32::serialize(stream, old_value);
+    IntType<32>::serialize(stream, old_value);
 
     stream.print();
     int32_t new_value;
-    IntType32::unserialize(stream, new_value);
+    IntType<32>::unserialize(stream, new_value);
 
     std::cout << "Old value: " << old_value << std::endl;
     std::cout << "New value: " << new_value << std::endl;
 
     int64_t v1;
-    IntType<int64_t>::unserialize(stream, v1);
+    IntType<64>::unserialize(stream, v1);
+    std::cout << v1 << std::endl;
+
+    // string
+    PackStream str_stream;
+
+    std::string str1("Hello world!");
+    StrType::serialize(str_stream, str1);
+
+    std::string str2;
+    StrType::unserialize(str_stream, str2);
+
+    std::cout << str2 << std::endl;
 
     return 0;
 }
