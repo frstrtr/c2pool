cfea716b11a1042c7c3dd04a18fe4f049d9862e0
Sleen <vasile.danil1@gmail.com>
2024-06-03 19:52:42 +0300

    [feature] impl/ltc BlockHeaderType added

--- src/impl/ltc/types.hpp
+++ src/impl/ltc/types.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <btclibs/uint256.h>
+
 #include <core/pack_types.hpp>
 #include <core/netaddress.hpp>
 
@@ -12,6 +14,37 @@ struct addr_type
     NetService m_endpoint;
 
     SERIALIZE_METHODS(ltc::addr_type) { READWRITE(obj.m_services, obj.m_endpoint); }
+};
+
+struct DefaultPrevBlock
+{ static uint256 get() { return uint256{}; }};
+
+struct BlockHeaderType
+{
+    int32_t m_version;
+    std::optional<uint256> m_previous_block;
+    uint256 m_merkle_root;
+    uint32_t m_timestamp;
+    uint32_t m_bits;
+    uint32_t m_nonce;
+
+    SERIALIZE_METHODS(BlockHeaderType) { READWRITE(obj.m_version, Optional(obj.m_previous_block, DefaultPrevBlock), obj.m_merkle_root, obj.m_timestamp, obj.m_bits, obj.m_bits); }
+
+    void SetNull()
+    {
+        m_version = 0;
+        m_previous_block.emplace(); m_previous_block->SetNull();
+        m_merkle_root.SetNull();
+        m_timestamp = 0;
+        m_bits = 0;
+        m_nonce = 0;
+    }
+
+    bool IsNull() const
+    {
+        return (m_bits == 0);
+    }
+
 };
 
 } // namespace ltc
