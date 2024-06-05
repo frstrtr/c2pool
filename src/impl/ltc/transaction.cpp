cdd26cf7ece092bdcdd4234efbc5a73dbc792b0e
Sleen <vasile.danil1@gmail.com>
2024-06-05 23:27:46 +0300

    [fix] Transaction constructor update

--- src/impl/ltc/transaction.cpp
+++ src/impl/ltc/transaction.cpp
@@ -3,10 +3,17 @@
 namespace ltc
 {
 
+Transaction::Transaction(const MutableTransaction& tx) : vin(tx.vin), vout(tx.vout), version(tx.version), locktime(tx.locktime), m_has_witness{ComputeHasWitness()} {} //, hash{ComputeHash()}, m_witness_hash{ComputeWitnessHash()} {}
+Transaction::Transaction(MutableTransaction&& tx) : vin(std::move(tx.vin)), vout(std::move(tx.vout)), version(tx.version), locktime(tx.locktime), m_has_witness{ComputeHasWitness()} {} // , hash{ComputeHash()}, m_witness_hash{ComputeWitnessHash()} {}
+
+bool Transaction::ComputeHasWitness() const
+{
+    return std::any_of(vin.begin(), vin.end(), [](const auto& input) {
+        return !input.scriptWitness.IsNull();
+    });
+}
+
 MutableTransaction::MutableTransaction() : version(Transaction::CURRENT_VERSION), locktime(0) {}
 MutableTransaction::MutableTransaction(const Transaction& tx) : version(tx.version), vin(tx.vin), vout(tx.vout), locktime(tx.locktime) {}
-
-// Transaction::Transaction(const MutableTransaction& tx) : vin(tx.vin), vout(tx.vout), version(tx.version), locktime(tx.locktime), m_has_witness{true} { } //, m_has_witness{ComputeHasWitness()}, hash{ComputeHash()}, m_witness_hash{ComputeWitnessHash()} {}
-// Transaction(MutableTransaction&& tx) : vin(std::move(tx.vin)), vout(std::move(tx.vout)), nVersion(tx.version), nLockTime(tx.locktime), m_has_witness{true} { }//m_has_witness{ComputeHasWitness()}, hash{ComputeHash()}, m_witness_hash{ComputeWitnessHash()} {}
 
 } // namespace ltc
