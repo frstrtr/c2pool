5e24e1bd5af7967723a249025a95a327143c3c88
Sleen <vasile.danil1@gmail.com>
2024-06-06 00:38:24 +0300

    [feature] Test for pack_test update

--- src/core/test/pack_test.cpp
+++ src/core/test/pack_test.cpp
@@ -11,6 +11,7 @@
 
 #include <core/pack.hpp>
 #include <core/pack_types.hpp>
+#include <core/opscript.hpp>
 
 #include <core/legacy/packv1.hpp>
 #include <core/legacy/pack_typesv1.hpp>
@@ -21,26 +22,17 @@ TEST(Pack, INT)
 
     PackStream stream;
     stream << old_value;
-    // Serialize(stream, old_value);
-
     stream.print();
+
     int32_t new_value;
     stream >> new_value;
-    // Unserialize(stream, new_value);
+    ASSERT_EQ(old_value, new_value);
 
     std::cout << "Old value: " << old_value << std::endl;
     std::cout << "New value: " << new_value << std::endl;
 
     int64_t v1;
-    try
-    {
-        stream >> v1;
-        // Unserialize(stream, v1);
-        std::cout << v1 << std::endl;
-    } catch (const std::ios_base::failure& fail)
-    {
-        std::cout << fail.what() << std::endl;
-    }
+    ASSERT_THROW({ stream >> v1; }, std::ios_base::failure);
 }
 
 TEST(Pack, STRING)
@@ -56,6 +48,7 @@ TEST(Pack, STRING)
     stream >> str2;
 
     std::cout << "str2: [" << str2 << "]" << std::endl;
+    ASSERT_EQ(str1, str2);
 }
 
 struct custom
@@ -77,6 +70,16 @@ struct custom
         std::cout << m_i << " <" << m_str << "> " << m_h << std::endl;
     }
 
+    bool operator==(const custom& c) const
+    {
+        return std::make_tuple(m_i, m_str, m_h) == std::make_tuple(c.m_i, c.m_str, c.m_h);
+    }
+
+    bool operator!=(const custom& c)
+    {
+        return !(*this == c);
+    }
+
     SERIALIZE_METHODS(custom) { READWRITE(obj.m_i, obj.m_str, obj.m_h); }
 };
 
@@ -92,6 +95,8 @@ TEST(Pack, CUSTOM_TYPE)
     custom value2;
     stream >> value2;
     value2.print();
+
+    ASSERT_EQ(value1, value2);
 }
 
 TEST(Pack, VECTOR)
@@ -109,6 +114,8 @@ TEST(Pack, VECTOR)
     for (const auto& n : nums2) std::cout << n << " ";
     std::cout << std::endl;
     stream.print();
+
+    ASSERT_EQ(nums1, nums2);
 }
 
 
@@ -118,7 +125,8 @@ TEST(Pack, INT_TYPES)
         PackStream  i_pack##N; i_pack##N << Using<IntType<N>>(i##N); \
         i_pack##N .print();\
         IntType<N>::num_type i_unpacked##N; i_pack##N >> i_unpacked##N ;\
-        std::cout << i##N << " -> " << i_unpacked##N << std::endl; 
+        std::cout << i##N << " -> " << i_unpacked##N << std::endl;\
+        ASSERT_EQ(i##N , i_unpacked##N);
     
     uint8_t     i8{std::numeric_limits<uint8_t>::max()};
     DEBUG_INT_TYPES(8);
@@ -153,7 +161,8 @@ TEST(Pack, INT_TYPES_BIG_ENDIAN)
         PackStream  i_pack##N; i_pack##N << Using<IntType<N, true>>(i##N); \
         i_pack##N .print();\
         IntType<N, true>::num_type i_unpacked##N(0); i_pack##N >> Using<IntType<N, true>>(i_unpacked##N);\
-        std::cout << i##N << " -> " << i_unpacked##N << std::endl; 
+        std::cout << i##N << " -> " << i_unpacked##N << std::endl;\
+        ASSERT_EQ(i##N , i_unpacked##N);
     
     uint8_t     i8{2};
     DEBUG_INT_TYPES(8);
@@ -201,6 +210,7 @@ TEST(Pack, ENUM)
     stream >> Using<EnumType<IntType<16 >>>(e2);
 
     std::cout << e1 << " -> " << e2 << std::endl;
+    ASSERT_EQ(e1, e2);
 }
 
 TEST(Pack, FIXED_STR)
@@ -219,6 +229,7 @@ TEST(Pack, FIXED_STR)
     std::string str2;
     stream >> FixedString(str2, 6);
     std::cout << str1 << " -> " <<  str2 << std::endl;
+    ASSERT_EQ(str1, str2);
 }
 
 TEST(Pack, VAR_INT)
@@ -227,7 +238,8 @@ TEST(Pack, VAR_INT)
         PackStream  i_pack##N; i_pack##N << VarInt(i##N); \
         i_pack##N .print();\
         uint64_t i_unpacked##N; i_pack##N >> VarInt(i_unpacked##N) ;\
-        std::cout << (uint64_t)i##N << " -> " << i_unpacked##N << std::endl; 
+        std::cout << (uint64_t)i##N << " -> " << i_unpacked##N << std::endl;\
+        ASSERT_EQ(i##N , i_unpacked##N);
     
     uint8_t     i8{250};
     DEBUG_INT_TYPES(8);
@@ -257,6 +269,7 @@ TEST(Pack, ENUM_VAR_INT)
     stream >> Using<EnumType<CompactFormat>>(e2);
 
     std::cout << e1 << " -> " << e2 << std::endl;
+    ASSERT_EQ(e1, e2);
 }
 
 struct DefaultUint32
@@ -275,6 +288,7 @@ TEST(Pack, OPTIONAL_TYPE)
     std::optional<uint32_t> num2;
     stream >> Using<OptionalType<DefaultUint32>>(num2);
     std::cout << num2.value();
+    ASSERT_EQ(DefaultUint32::get(), num2.value());
 }
 
 struct DefaultCustom
@@ -293,6 +307,8 @@ TEST(Pack, OPTIONAL_TYPE_FOR_CUSTOM)
     std::optional<custom> c2;
     stream >> Optional(c2, DefaultCustom);
     c2->print();
+
+    ASSERT_EQ(DefaultCustom::get(), c2.value());
 }
 
 struct opt_custom
@@ -315,6 +331,16 @@ struct opt_custom
             std::cout << "opt_custom is none!" << std::endl;
     }
 
+    bool operator==(const opt_custom& c) const
+    {
+        return std::make_tuple(m_c.has_value(), m_c.value_or(custom{})) == std::make_tuple(c.m_c.has_value(), c.m_c.value_or(custom{}));
+    }
+
+    bool operator!=(const opt_custom& c)
+    {
+        return !(*this == c);
+    }
+
     SERIALIZE_METHODS(opt_custom) 
     { READWRITE(Optional(obj.m_c, DefaultCustom)); }
     // { formatter.action(stream, Using<OptionalType<DefaultCustom>>(obj.m_c)); }
@@ -333,7 +359,7 @@ TEST(Pack, OPTIONAL_FIELD)
     opt_custom v2;
     stream >> v2;
     v2.print();
-
+    ASSERT_EQ(DefaultCustom::get(), v2.m_c);
 }
 
 TEST(Pack, OPTIONAL_FIELD2)
@@ -348,7 +374,7 @@ TEST(Pack, OPTIONAL_FIELD2)
     opt_custom v2;
     stream >> v2;
     v2.print();
-
+    ASSERT_EQ(v1, v2);
 }
 
 TEST(Pack, UINT_CHECK1)
@@ -363,7 +389,7 @@ TEST(Pack, UINT_CHECK1)
     stream >> n2;
 
     std::cout << n1 << " -> " << n2 << std::endl;
-
+    ASSERT_EQ(n1, n2);
 }
 
 TEST(Pack, UINT_CHECK2)
@@ -378,5 +404,26 @@ TEST(Pack, UINT_CHECK2)
     stream >> n2;
 
     std::cout << n1 << " -> " << n2 << std::endl;
+    ASSERT_EQ(n1, n2);
+}
 
+TEST(Pack, OP_SCRIPT)
+{
+    std::vector<unsigned char> vch{'a', 'c', 'o'};
+    OPScript script(vch.begin(), vch.end());
+
+    PackStream stream;
+    stream << script;
+
+    std::for_each(script.begin(), script.end(), [&](const auto& ch) { std::cout << ch << " "; });
+    std::cout << std::endl;
+    stream.print();
+
+    OPScript script2;
+    stream >> script2;
+    std::for_each(script.begin(), script.end(), [&](const auto& ch) { std::cout << ch << " "; });
+    std::cout << std::endl;
+
+    ASSERT_EQ(script, script2);
+    ASSERT_TRUE(script == vch && script2 == vch);
 }
