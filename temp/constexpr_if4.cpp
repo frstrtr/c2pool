7406827ab20d8255f338f07829650646237236f6
5133n <vasile.danil1@gmail.com>
2024-06-06 20:30:16 +0300

    [concept] check concept

--- temp/constexpr_if4.cpp
+++ temp/constexpr_if4.cpp
@@ -1,60 +1,85 @@
 #include <iostream>
 
-struct _Base {};
 
-template <typename SUB_TYPE, int VERSION>
-struct Base : public _Base
+// struct Base
+// {
+//     const int version;
+// };
+
+// struct Base2 : public Base
+// {
+
+// };
+
+// constexpr static Base2 A {Base::version = 1};
+
+struct Base
 {
-    using version = std::integral_constant<int, VERSION>();
-    using sub_type = SUB_TYPE;
+    const int m_version;
+
+    constexpr Base(int version) : m_version{version} { }
 };
 
-struct A : public Base<A, 0>
+template <int VERSION>
+struct BaseVersion : public Base
+{
+    BaseVersion() : Base(VERSION) {}
+};
+
+struct A : public BaseVersion<1>
 {
     int i;
-
-    A(int _i) : i(_i) {}
 };
 
-struct B : public Base<B, 1>
+struct B : public BaseVersion<2>
 {
-    int b;
-
-    B(int _b) : b(_b) {}
+    int h;
 };
 
+template <int I>
+struct Version
+{
+};
 
+template <>
+struct Version<1> { using type = A; };
+
+template <>
+struct Version<2> { using type = B; };
+
+// template <typename From, typename To>
+// constexpr To* f(From* v) 
+// { 
+//     return (To*) v; 
+// }
 
 template <typename T>
-int f(_Base* _v)
+typename Version<T::version>::type f(Base* v)
 {
-    // using type = std::remove_pointer_t<T>;
-    // static_assert(std::is_base_of<Base, T>::value);
-    Base* v = static_cast<Base*>(_v);
-
-    if constexpr (std::is_same<T::version, std::integral_constant<int, 1>>())
-        return 1;
-
-    return 0;
+    using type = Version<Base::m_version>;
 }
 
 int main()
 {
-    int i;
-    std::cin >> i;
 
-    Base* value;
 
-    switch (i)
-    {
-    case 0:
-        value = new A(111);
-        break;
-    case 1:
-        value = new B(50);
-        break;
-    default:
-        break;
-    }
-    std::cout << f(value) << std::endl;
+    // uint64_t* v = new uint64_t; std::cin >> *v;
+    // auto v2 = f<uint64_t, uint32_t>(v);
+
+    // std::cout << *v2 << std::endl;
+
+    // Base* value;
+
+    // switch (i)
+    // {
+    // case 0:
+    //     value = new A(111);
+    //     break;
+    // case 1:
+    //     value = new B(50);
+    //     break;
+    // default:
+    //     break;
+    // }
+    // std::cout << f(value) << std::endl;
 }
