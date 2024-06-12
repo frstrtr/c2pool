0a8c69bc981fe0768482706e72048c0696c9d24d
5133n <vasile.danil1@gmail.com>
2024-06-12 14:23:17 +0300

    [chore/test] upd

---
+++ src/core/chain/fork.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace c2pool
+{
+    
+namespace core
+{
+
+
+    
+} // namespace core
+
+
+} // namespace c2pool
---
+++ temp/get_chain_test.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <ranges>
+
+
+struct Chain
+{
+    std::vector<int> values;
+
+    auto get_chain(size_t n)
+    {
+        return
+    }
+};
+
+
+int main()
+{
+
+}
