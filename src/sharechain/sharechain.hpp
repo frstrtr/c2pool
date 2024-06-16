a3f02cd6498f5a6cbe20d47b9de7ddd06638f037
Sleen <vasile.danil1@gmail.com>
2024-06-16 07:02:18 +0300

    [feature] finished calculate head/tails

--- src/sharechain/sharechain.hpp
+++ src/sharechain/sharechain.hpp
@@ -70,7 +70,7 @@ private:
     std::unordered_map<hash_t, chain_data, hasher_t> m_shares;
 
     std::unordered_map<hash_t, hash_t, hasher_t> m_heads;
-    std::unordered_map<hash_t, std::set<index_t*>, hasher_t> m_tails;
+    std::unordered_map<hash_t, std::set<hash_t>, hasher_t> m_tails;
     
     void calculate_head_tail(hash_t hash, hash_t prev, index_t* index)
     {
@@ -80,8 +80,6 @@ private:
             only_heads = 1,
             only_tails = 1 << 1,
             merge = only_heads | only_tails,
-
-            split = 1 << 2
         };
 
         int state = new_fork;
@@ -96,7 +94,7 @@ private:
             // создание нового форка
             {
                 m_heads[hash] = prev;    
-                m_tails[prev].insert(index);
+                m_tails[prev].insert(hash);
             }
             break;
         case merge:
@@ -108,27 +106,24 @@ private:
                 auto& r_tail = right.key(); auto& r_heads = right.mapped();
 
                 m_tails[l_tail].insert(r_heads.begin(), r_heads.end());
-                m_tails[l_tail].erase(m_shares[prev].index);
+                m_tails[l_tail].erase(prev);
 
                 for (auto& i : m_tails[l_tail])
-                    m_heads[i->hash] = l_tail;
+                    m_heads[i] = l_tail;
 
                 index->prev = m_shares[prev].index;
                 index->calculate_index(index->prev);
 
-                // // right
-                std::unordered_set<index_t*> dirty_indexs;
-                
-                // right_parts.key() = left_part.key();
+                std::unordered_set<hash_t> dirty_indexs;
                 for (auto& part : right.mapped())
                 {
-                    index_t* cur = part;
+                    index_t* cur = m_shares[part].index;
                     while(cur)
                     {
-                        if (dirty_indexs.contains(cur))
+                        if (dirty_indexs.contains(cur->hash))
                             break;
                         
-                        dirty_indexs.insert(cur);
+                        dirty_indexs.insert(cur->hash);
                         cur->calculate_index(index);
 
                         if (!cur->prev)
@@ -140,29 +135,22 @@ private:
                         cur = cur->prev;
                     }
                 }
-                // for (auto& head : right_parts.mapped())
-                // {
-                //     m_heads[head->hash] = left_part.mapped();
-                //     m_tails[left_part.mapped()].insert(head);
-                // }
-                // m_heads.insert(std::move(left_part));
-                // m_tails.insert(std::move(right_parts));
             }
             break;
         case only_tails:
             // элемент слева
             {
-                std::unordered_set<index_t*> dirty_indexs;
-                auto right_parts = m_tails.extract(hash);
-                for (auto& part : right_parts.mapped())
+                std::unordered_set<hash_t> dirty_indexs;
+                auto right = m_tails.extract(hash);
+                for (auto& part : right.mapped())
                 {
-                    index_t* cur = part;
+                    index_t* cur = m_shares[part].index;
                     while(cur)
                     {
-                        if (dirty_indexs.contains(cur))
+                        if (dirty_indexs.contains(cur->hash))
                             break;
                         
-                        dirty_indexs.insert(cur);
+                        dirty_indexs.insert(cur->hash);
                         cur->calculate_index(index);
 
                         if (!cur->prev)
@@ -174,20 +162,24 @@ private:
                         cur = cur->prev;
                     }
                 }
+
+                for (auto& v : right.mapped())
+                    m_heads[v] = prev;
+                right.key() = prev;
+                m_tails.insert(std::move(right));
             }
             break;
         case only_heads:
             // элемент справа
             {
                 auto left_part = m_heads.extract(prev);
-                // left_part.key() = hash;
 
                 index->prev = m_shares[prev].index;
                 index->calculate_index(index->prev);
+
                 m_heads[hash] = left_part.mapped();
-                m_tails[left_part.mapped()].erase(m_shares[left_part.key()].index);
-                m_tails[left_part.mapped()].insert(index);
-                // m_heads.insert(std::move(left_part));
+                m_tails[left_part.mapped()].erase(left_part.key());
+                m_tails[left_part.mapped()].insert(hash);
             }
             break;
         }
@@ -201,15 +193,10 @@ public:
 
         // index
         auto index = new index_t(share);
-        // if (m_shares.contains(share->m_prev_hash))
-        //     index->prev = m_shares[share->m_prev_hash].index;
-        // index->calculate_index(index->prev); 
-        
         // share_variants
         share_t share_var; share_var = share;
 
         calculate_head_tail(share->m_hash, share->m_prev_hash, index);
-        
         m_shares[share->m_hash] = chain_data{index, share_var};
     }
 
@@ -219,6 +206,11 @@ public:
             return m_shares[hash];
         else
             throw std::out_of_range("Hash out of chain!");
+    }
+
+    index_t& get_index(hash_t&& hash)
+    {
+        return get(std::move(hash)).index;
     }
 
     share_t& get_share(hash_t&& hash)
@@ -244,7 +236,7 @@ public:
         {
             std::cout << " " << hash << ": [ ";
             for (auto& value : values)
-                std::cout << value->hash << " ";
+                std::cout << value << " ";
             std::cout << "]; ";
         }
         std::cout << "}\n";
