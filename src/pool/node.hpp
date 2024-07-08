bf97bd8bf6eb013be7b05e2fccfa68d1e518041e
Sleen <vasile.danil1@gmail.com>
2024-07-08 20:38:55 +0300

    [feature] update pool/node.hpp

--- src/pool/node.hpp
+++ src/pool/node.hpp
@@ -4,7 +4,7 @@
 
 #include <pool/factory.hpp>
 #include <pool/peer.hpp>
-#include <core/node_interface.hpp>
+#include <core/socket.hpp>
 
 namespace c2pool
 {
@@ -12,10 +12,15 @@ namespace c2pool
 namespace pool
 {
 
+class NodeInterface : public Communicator, public INetwork
+{
+    
+};
+
 template <typename NodeType>
 class IProtocol : public virtual NodeType
 {
-    static_assert(std::is_base_of_v<INode, NodeType>);
+    static_assert(std::is_base_of_v<NodeInterface, NodeType>);
 protected:
     virtual void handle_message() = 0;
 };
@@ -26,19 +31,56 @@ class BaseNode : public Legacy, public Actual, public Factory
 {
     static_assert(std::is_base_of_v<IProtocol<Base>, Legacy> && std::is_base_of_v<IProtocol<Base>, Actual>);
 
-    std::map<NetService, PeerType> peers;
+    // For implementation override:
+    //  Communicator:
+    //      void error(const message_error_type& err)
+    //      void handle_version(std::unique_ptr<RawMessage> rmsg, const NetService& service)
+    //  INetwork:
+    //      void connected(std::shared_ptr<Socket> socket) = 0;
+    //      void disconnect() = 0;
+
+    std::map<NetService, PeerType*> peers;
 
 public:
     template <typename... Args>
     BaseNode(boost::asio::io_context* ctx, Args... args) : Base(ctx, args...), Factory(ctx, this) { }
 
-    void handle(std::unique_ptr<RawMessage> msg, const NetService& peer) override
+    void handle(std::unique_ptr<RawMessage> rmsg, const NetService& service) override
     {
-        
+        auto peer = peers[service];
+
+        if (peer->type() == PeerConnectionType::unknown)
+        {
+            if (rmsg->m_command != "version")
+                //TODO: error, message wanna for be version 
+                {}
+
+
+
+            handle_version(std::move(rmsg), service);
+        }
+
+        IProtocol* protocol;
+        switch (peer->type())
+        {
+        case PeerConnectionType::unknown:
+            return;
+        case PeerConnectionType::legacy:
+            protocol = static_cast<Legacy>(this);
+            break;
+        case PeerConnectionType::actual:
+            protocol = static_cast<Actual>(this);
+            break;
+        default:
+            // TODO: error
+            return;
+        }
+
+        protocol->handle_message(rmsg, peer);
     }
 };
 
-#define ADD_HANDLER(name, msg_type)\
+// #define ADD_HANDLER(name, msg_type)\
     
 
 } // namespace pool
