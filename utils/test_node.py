46f832d755123b568158c2bbb32121b031071ba0
5133n <vasile.danil1@gmail.com>
2024-07-15 09:28:22 +0300

    [util] test_node update

--- utils/test_node.py
+++ utils/test_node.py
@@ -5,9 +5,8 @@ from twisted.python import log
 
 import hashlib
 import struct
+import time
 
-command = b"version"
-payload = b"1111"
 prefix = b"\01\02\03\04"
 
 class Protocol(protocol.Protocol):
@@ -20,21 +19,27 @@ class Protocol(protocol.Protocol):
         if len(command) >= 12:
             raise ValueError('command too long')
         print(self.prefix)
-        # data = self.prefix + struct.pack('<12sI', command, len(payload)) + hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4] + payload
-        data = self.prefix 
-        print('f:', data)
-        data += struct.pack('<12sI', command, len(payload))
-        print('f:', data)
-        data += hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4] 
-        print('f:', data)
-        data += payload
+        checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()
+        print("out1: ", hashlib.sha256(payload).hexdigest())
+        print("checksum_full: ", checksum.hex())
+        print("chesksum_part: ", struct.unpack('<I', checksum[:4]))
+        data = self.prefix + struct.pack('<12sI', command, len(payload)) + checksum[:4] + payload
+        # data = self.prefix 
+        # print('f:', data)
+        # data += struct.pack('<12sI', command, len(payload))
+        # print('f:', data)
+        # data += hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4] 
+        # print('f:', data)
+        # data += payload
         print('f:', data)
         self.transport.write(data)
         
 
 def gotProtocol(p):
-    global command, payload
-    p.sendPacket(command, payload)
+    p.sendPacket(b"version", b"11112")
+    p.sendPacket(b"test", b"2111111111312312")
+    # reactor.stop()
+    reactor.callLater(2, reactor.stop)
     # reactor.callLater(1, p.sendMessage, "This is sent in a second")
     # reactor.callLater(2, p.transport.loseConnection)
 
