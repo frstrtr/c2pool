aea67f10ff91f032264e262ec9a0add4795dbedf
Sleen <vasile.danil1@gmail.com>
2024-07-22 20:35:25 +0300

    [feature] Finished AddrStore

--- src/core/addr_store.cpp
+++ src/core/addr_store.cpp
@@ -1,7 +1,17 @@
 #include "addr_store.hpp"
 
+#include <core/common.hpp>
 namespace core
 {
+
+void AddrStore::save() const
+{
+    std::fstream file(m_path);
+    file << to_json();
+
+    file.close();
+    LOG_DEBUG_OTHER << "Addrs [" << m_path << "] saved in file!";
+}
 
 void AddrStore::from_json(std::string j_str)
 {
@@ -14,7 +24,7 @@ void AddrStore::from_json(std::string j_str)
         LOG_WARNING << "AddrStore::FromJSON " << e.what() << ", j_str = " << j_str;
         return;
     }
-    data = j.get<std::map<NetService, AddrValue>>();
+    m_data = j.get<std::map<NetService, AddrValue>>();
 }
 
 void AddrStore::add(const NetService& addr, AddrValue value)
@@ -22,7 +32,7 @@ void AddrStore::add(const NetService& addr, AddrValue value)
     if (check(addr)) 
         return;
     
-    data[addr] = value;
+    m_data[addr] = value;
     save();
 }
 
@@ -31,9 +41,16 @@ void AddrStore::remove(const NetService& addr)
     if (check(addr)) 
         return;
     
-    data.erase(addr);
+    m_data.erase(addr);
     save();
 }
 
+void AddrStore::load(const std::vector<NetService>& addrs)
+{
+    for (const NetService& addr : addrs)
+    {
+        m_data[addr] = {0, core::timestamp(), core::timestamp()};
+    }
+}
 
 } // namespace core
