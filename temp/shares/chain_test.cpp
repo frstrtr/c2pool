ab111ca333ca24522eecf153d0360c286729a906
Sleen <vasile.danil1@gmail.com>
2024-07-27 09:55:25 +0300

    [test] update chain_test

--- temp/shares/chain_test.cpp
+++ temp/shares/chain_test.cpp
@@ -9,8 +9,6 @@ struct BaseFakeShare : chain::BaseShare<int, Version>
 {
     BaseFakeShare() { }
     BaseFakeShare(int hash, int prev_hash) : chain::BaseShare<int, Version>(hash, prev_hash) { }
-    
-    SERIALIZE_METHODS(BaseFakeShare<Version>) { READWRITE(obj.m_hash, obj.m_prev_hash); }
 };
 
 struct FakeShareA : BaseFakeShare<10>
@@ -19,21 +17,31 @@ struct FakeShareA : BaseFakeShare<10>
 
     FakeShareA() {}
     FakeShareA(int hash, int prev_hash, int data1) : BaseFakeShare<10>(hash, prev_hash), m_data1{data1} {}
-    
-    SERIALIZE_METHODS(FakeShareA) { READWRITE(AsBase<BaseFakeShare<10>>(obj), obj.m_data1); }
 };
 
 struct FakeShareB : BaseFakeShare<20>
 {
-    double m_data2;
+    std::string m_data2;
 
     FakeShareB() {}
-    FakeShareB(int hash, int prev_hash, double data2) : BaseFakeShare<20>(hash, prev_hash), m_data2{data2} {}
-
-    SERIALIZE_METHODS(FakeShareB) { READWRITE(AsBase<BaseFakeShare<20>>(obj)/*, obj.m_data2*/); }
+    FakeShareB(int hash, int prev_hash, std::string data2) : BaseFakeShare<20>(hash, prev_hash), m_data2{data2} {}
 };
 
-using ShareType = chain::ShareVariants<FakeShareA, FakeShareB>;
+struct FakeShareFormatter
+{
+    SHARE_FORMATTER()
+    {
+        READWRITE(obj->m_hash, obj->m_prev_hash);
+
+        if constexpr (version == 10)
+            READWRITE(obj->m_data1);
+        
+        if constexpr (version == 20)
+            READWRITE(obj->m_data2);
+    }
+};
+
+using ShareType = chain::ShareVariants<FakeShareFormatter, FakeShareA, FakeShareB>;
 
 class FakeIndex : public chain::ShareIndex<int, ShareType, std::hash<int>, FakeIndex>
 {
@@ -42,28 +50,23 @@ private:
 public:
 
     int data1{0};
-    double data2{0};
 
     FakeIndex() : base_index() {}
     template <typename ShareT> FakeIndex(ShareT* share) : base_index(share)
     {
         if constexpr(ShareT::version == 10)
             data1 = share->m_data1;
-        if constexpr(ShareT::version == 20)
-            data2 = share->m_data2;
     }
 
 protected:
     void add(FakeIndex* index) override
     {
         data1 += index->data1;
-        data2 += index->data2;
     }
 
     void sub(FakeIndex* index) override
     {
         data1 -= index->data1;
-        data2 -= index->data2;
     }
 };
 
@@ -107,7 +110,7 @@ void debug_print(FakeChain& chain, FakeIndex::hash_t hash)
     std::cout << "Print for hash [" << hash << "]" << std::endl;
     chain.debug();
     auto& [index, data] = chain.get(std::move(hash));
-    std::cout << "data1 = " << index->data1 << "; data2 = " << index->data2 << "; height = " << index->height << std::endl;
+    std::cout << "data1 = " << index->data1 << "; height = " << index->height << std::endl;
     std::cout << "====================" << std::endl;
 }
 
@@ -134,18 +137,18 @@ int main()
 
     chain.add(new FakeShareA(11, 10, 100));
     debug_print(chain, 11);
-    chain.add(new FakeShareB(13, 12, 10));
+    chain.add(new FakeShareB(13, 12, "10"));
     debug_print(chain, 13);
-    chain.add(new FakeShareB(12, 11, 200.222));
+    chain.add(new FakeShareB(12, 11, "200.222"));
     debug_print(chain, 13);
-    chain.add(new FakeShareB(14, 13, 10));
+    chain.add(new FakeShareB(14, 13, "30"));
     debug_print(chain, 14);
     chain.add(new FakeShareA(10, 9, 1000.20));
     debug_print(chain, 14);
 
 
     auto interval = chain.get_interval(14, 11); // [14, 13, 12]
-    std::cout << "interval: data1 = " << interval.data1 << "; data2 = " << interval.data2 << "; height = " << interval.height << std::endl;
+    std::cout << "interval: data1 = " << interval.data1 << "; height = " << interval.height << std::endl;
     debug_print(chain, 14);
 
     PackStream stream;
@@ -153,7 +156,7 @@ int main()
     auto share = ShareType::load(FakeShareA::version, stream);
     auto share_copy = share;
 
-    share.INVOKE(test_f2, 10, 2.2);
+    share.CALL(test_f2, 10, 2.2);
 
     share_copy.ACTION(
         {
@@ -168,4 +171,6 @@ int main()
     share.USE(test_f);
 
     std::cout << share.hash() << " <--- " << share.prev_hash() << std::endl;
+
+    
 }
