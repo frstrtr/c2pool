3f9f52a6c3549559c009bcf8081d5183dde9b61c
Sleen <vasile.danil1@gmail.com>
2024-07-29 19:38:46 +0300

    [feature][LTC] Update BlockHeaderType; Added SmallBlockHeaderType

--- src/impl/ltc/types.hpp
+++ src/impl/ltc/types.hpp
@@ -8,25 +8,46 @@
 namespace ltc
 {
 
-struct BlockHeaderType
+struct SmallBlockHeaderType
 {
-    int32_t m_version;
-    uint256 m_previous_block;
-    uint256 m_merkle_root;
-    uint32_t m_timestamp;
-    uint32_t m_bits;
-    uint32_t m_nonce;
+    uint64_t m_version {};
+    uint256 m_previous_block{};
+    uint32_t m_timestamp{};
+    uint32_t m_bits{};
+    uint32_t m_nonce{};
 
-    SERIALIZE_METHODS(BlockHeaderType) { READWRITE(obj.m_version, obj.m_previous_block, obj.m_merkle_root, obj.m_timestamp, obj.m_bits, obj.m_bits); }
+    SERIALIZE_METHODS(SmallBlockHeaderType) { READWRITE(VarInt(obj.m_version), obj.m_previous_block, obj.m_timestamp, obj.m_bits, obj.m_nonce); }
+
+    SmallBlockHeaderType() {}
 
     void SetNull()
     {
         m_version = 0;
         m_previous_block.SetNull();
-        m_merkle_root.SetNull();
         m_timestamp = 0;
         m_bits = 0;
         m_nonce = 0;
+    }
+
+    bool IsNull() const
+    {
+        return (m_bits == 0);
+    }
+};
+
+struct BlockHeaderType : SmallBlockHeaderType
+{
+    uint256 m_merkle_root;
+
+    //TODO: correct merkle_root position?
+    SERIALIZE_METHODS(BlockHeaderType) { READWRITE(VarInt(obj.m_version), obj.m_previous_block, obj.m_merkle_root, obj.m_timestamp, obj.m_bits, obj.m_bits); }
+
+    BlockHeaderType() : SmallBlockHeaderType() { }
+
+    void SetNull()
+    {
+        SmallBlockHeaderType::SetNull();
+        m_merkle_root.SetNull();
     }
 
     bool IsNull() const
