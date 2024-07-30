76c2e89d7a03a7a7592e022e022ae384bf769c52
Sleen <vasile.danil1@gmail.com>
2024-07-30 23:38:51 +0300

    [feature][LTC] update

--- src/impl/ltc/peer.hpp
+++ src/impl/ltc/peer.hpp
@@ -3,6 +3,7 @@
 #include "transaction.hpp"
 
 #include <set>
+#include <optional>
 #include <core/uint256.hpp>
 
 namespace ltc
@@ -10,6 +11,11 @@ namespace ltc
 
 struct Peer
 {
+    std::optional<uint32_t> m_other_version;
+    std::string m_other_subversion;
+    uint64_t m_other_services;
+    uint64_t m_nonce;
+    
     std::set<uint256> m_remote_txs; // hashes
     // int32_t remote_remembered_txs_size = 0;
 
