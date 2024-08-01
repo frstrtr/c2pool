e9fcf550ed9e02723d0dac69942266a844310090
Sleen <vasile.danil1@gmail.com>
2024-08-01 19:34:56 +0300

    [test][LTC] coin/pool test

---
+++ src/c2pool/ltc/ltc_coin_test.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <impl/ltc/coin/rpc.hpp>
+
+int main()
+{
+    // ltc::coin::RPC* rpc = new ltc::coin::RPC();
+    // std::cout << rpc->Send("asdasd") << std::endl;
+}
