1dcfecfd62066643afa1ec4f416eac93602002f3
Sleen <vasile.danil1@gmail.com>
2024-08-04 14:06:18 +0300

    [test] message_parser

--- temp/message_parser.cpp
+++ temp/message_parser.cpp
@@ -3,11 +3,15 @@
 #include <variant>
 #include <map>
 #include <string>
+#include <memory>
+#include <chrono>
+#include <iomanip>
 
 template <typename...T>
-struct MessageHandler
+class MessageHandler
 {
-    using var_t = std::variant<T...>;
+private:
+    using var_t = std::variant<std::unique_ptr<T>...>;
     using handlers_t = std::map<std::string, std::function<var_t()>>;
     static handlers_t m_handlers;
 
@@ -15,7 +19,7 @@ struct MessageHandler
     static void add_handlers()
     {
         Ty msg;
-        m_handlers[msg.m_command] = [cmd = msg.m_command]() { std::cout << cmd << " type" << std::endl; var_t res = Ty(); return res; };
+        m_handlers[msg.m_command] = []() { auto msg = std::make_unique<Ty>(); std::cout << msg->m_command << std::endl; return msg; };
     }
 
     static handlers_t init_handlers()
@@ -25,11 +29,20 @@ struct MessageHandler
         return handlers;
     }
 
-    template <typename Caller>
-    void call(std::string cmd, Caller& cl)
+public:
+    var_t parse(std::string cmd)
     {
-        std::visit([&](auto v){cl.handle(v);}, m_handlers[cmd]());
+        if (m_handlers.contains(cmd))
+            return m_handlers[cmd]();
+        else
+            throw std::out_of_range("MessageHandler not contain " + cmd);
     }
+
+    // template <typename Caller>
+    // void call(std::string cmd, Caller& cl)
+    // {
+    //     std::visit([&](auto v){cl.handle(v);}, m_handlers[cmd]());
+    // }
 };
 
 template <typename...T>
@@ -57,26 +70,64 @@ struct C : Base
     C() : Base("C") { }
 };
 
-struct CallObj
+struct debug_timestamp
 {
-    void handle(A a) { std::cout << "handle A1!" << std::endl; }
-    void handle(B a) { std::cout << "handle B2!" << std::endl; }
+    std::chrono::duration<double> t;
+
+    debug_timestamp() : t(std::chrono::high_resolution_clock::now().time_since_epoch()) { }
+    debug_timestamp(std::chrono::duration<double> _t) : t(_t) {}
+
+    debug_timestamp operator-(const debug_timestamp& v) const
+    {
+        return {t - v.t};
+    }
+
+    debug_timestamp operator+(const debug_timestamp& v) const
+    {
+        return {t + v.t};
+    }
+
+    friend std::ostream& operator<<(std::ostream &stream, const debug_timestamp& v)
+    {
+        stream << std::fixed << std::setprecision(10) << v.t.count() << "s";
+        stream.unsetf(std::ios_base::fixed);
+        return stream;
+    }
 };
 
-template <typename...T>
-void f()
+struct CallObj
 {
-    // ((prt(T)), ...);
-}
+    MessageHandler<A, B> handler;
+    int count = 1;
+
+    void handle_message(std::string cmd)
+    {
+        auto result = handler.parse(cmd);
+        std::visit(
+            [&](auto& msg)
+            {
+                handle(std::move(msg), 2);
+            }, result);
+    }
+
+    void handle(std::unique_ptr<A> a, int i) { count++;/*std::cout << "handle A" << i << "!\n";*/}
+    void handle(std::unique_ptr<B> b, int i) { count--;/*std::cout << "handle B" << i << "!\n";*/}
+};
 
 int main()
 {
-    MessageHandler<A, B> handler;
+    
 
     std::string cmd;
     std::cin >> cmd;
 
     // handler.m_handlers[cmd]();
     CallObj cl;
-    handler.call(cmd, cl);
+    debug_timestamp t0;
+    
+    cl.handle_message(cmd);
+
+    debug_timestamp t1;
+
+    std::cout << (t1 - t0) << std::endl;
 }
