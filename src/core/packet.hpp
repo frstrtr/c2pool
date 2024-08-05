c4b87ad2f5b641e6383d13f9bf4058f32c07723d
Sleen <vasile.danil1@gmail.com>
2024-08-05 13:27:29 +0300

    [fix] Packet fixed

--- src/core/packet.hpp
+++ src/core/packet.hpp
@@ -28,12 +28,12 @@ public:
         ArrayType<DefaultFormat, 12>::Write(result, msg->m_command);
 
         // message_length
-        result << msg->m_data.size();
+        uint32_t length = msg->m_data.size();
+        result << length;
 
         // checksum
-        //TODO: check
         uint256 hash_checksum = Hash(std::span<std::byte>(msg->m_data.data(), msg->m_data.size()));
-        result << hash_checksum.pn[7];
+        result << hash_checksum.pn[0];
 
         // payload
         result << msg->m_data;
