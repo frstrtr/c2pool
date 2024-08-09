19af373715d2314f4b6a89b69c7fa628625502ce
Sleen <vasile.danil1@gmail.com>
2024-08-09 20:35:53 +0300

    [feature] txidcache update;

--- src/impl/ltc/coin/node_interface.hpp
+++ src/impl/ltc/coin/node_interface.hpp
@@ -4,13 +4,14 @@
 
 #include "block.hpp"
 #include "transaction.hpp"
+#include "txidcache.hpp"
 
 #include <core/uint256.hpp>
 #include <core/events.hpp>
 
 #include <nlohmann/json.hpp>
 
-namespace ltc::coin::p2p
+namespace ltc
 {
 
 namespace interfaces
@@ -18,15 +19,16 @@ namespace interfaces
 
 struct Node
 {
-	Event<nlohmann::json> m_work;								//get_work result
-    Event<uint256> m_new_block;									//block_hash
-	Event<coin::Transaction> m_new_tx;							//bitcoin_data.tx_type
-	Event<std::vector<coin::BlockHeaderType>> m_new_headers;	//bitcoin_data.block_header_type
+	Variable<nlohmann::json> work;								//get_work result
+    Event<uint256> new_block;								//block_hash
+	Event<coin::Transaction> new_tx;						//bitcoin_data.tx_type
+	Event<std::vector<coin::BlockHeaderType>> new_headers;	//bitcoin_data.block_header_type
 
+	coin::TXIDCache txid_cache;
 };
 
 } // namespace interfaces
 
-} // namespace ltc::coin::p2p
+} // namespace ltc::coin
 
 
