31ad5f8bdac7929e80e85709dd30cb55bae01d38
frstrtr <frstrtr@gmail.com>
2025-07-05 00:07:52 +0400

    Fixed Boost library linkage issues Fixed namespace issues

--- src/c2pool/c2pool.cpp
+++ src/c2pool/c2pool.cpp
@@ -13,20 +13,20 @@
 
 int main(int argc, char *argv[])
 {
-    c2pool::log::Logger::init();
-    // c2pool::log::Logger::add_category("all");
+    core::log::Logger::init();
+    // core::log::Logger::add_category("all");
 
-    // std::ofstream fout(c2pool::filesystem::config_path() / "config.yaml");
-    // std::cout << c2pool::pool::config::get_default() << std::endl;
-    // fout << c2pool::pool::config::get_default();
+    // std::ofstream fout(core::filesystem::config_path() / "config.yaml");
+    // std::cout << core::pool::config::get_default() << std::endl;
+    // fout << core::pool::config::get_default();
     // fout.close();
 
-    auto settings = c2pool::Fileconfig::load_file<c2pool::Settings>();
+    auto settings = core::Fileconfig::load_file<core::Settings>();
 
-    // std::map<std::string, c2pool::fileconfig*> configs;
+    // std::map<std::string, core::fileconfig*> configs;
     // for (const auto& net : settings->m_networks)
     // {
-    //     c2pool::config* config = c2pool::config::load(net);
+    //     core::config* config = core::config::load(net);
     //     configs[net] = config;
     // }
     
